f39e5119bf020fcbe4cbffcfddbade28e99eaa9f
Francois Bojarski <francois.bojarski@consensys.net>
2023-08-25 16:44:12 +0530

    feat: wip

--- rlp_txrcpt/constraints.lisp
+++ rlp_txrcpt/constraints.lisp
@@ -1,4 +1,4 @@
-(module txRlp)
+(module rlpTxRcpt)
 
 (defconst
   int_short               128  ;;RLP prefix of a short integer (<56 bytes), defined in the EYP.
@@ -7,6 +7,8 @@
   list_long               247  ;;RLP prefix of a long list (>55 bytes), defined in the EYP.
   LLARGE                  16
   LLARGEMO                15)
+
+(defpurefun (if-not-eq x y then) (if-not-zero (- x y) then))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;                              ;;
@@ -22,7 +24,7 @@
 
 ;; Def transaction-constant
 (defun (transaction-constant C)
-  (if-not-eq (ABS_TX_NUM (+ (prev ABS_TX_NUM 1)))
+  (if-not-eq ABS_TX_NUM (+ (prev ABS_TX_NUM) 1)
     (remained-constant! C)))
 
 ;; Def counter-constant
@@ -92,8 +94,8 @@
 
 (defconstraint phase-exclusion ()
   (if-zero ABS_TX_NUM
-           (vanishes! (reduce + (for i [0 : 44] [PHASE i])))
-           (eq! 1 (reduce + (for i [0 : 14] [PHASE i])))))
+           (vanishes! (reduce + (for i [0 : 4] [PHASE i])))
+           (eq! 1 (reduce + (for i [0 : 4] [PHASE i])))))
 
 (defconstraint ABS_TX_NUM-evolution()
   (eq! ABS_TX_NUM
@@ -102,22 +104,22 @@
 
 (defconstraint no-done-no-end ()
   (if-zero DONE
-           (vanishes! end_phase)))
+           (vanishes! PHASE_END)))
 
 (defconstraint no-end-no-changephase ()
-  (if-zero end_phase
+  (if-zero PHASE_END
            (vanishes! (reduce + (for i [0 : 4]
                                      (* i
                                         (- (next [PHASE i]) [PHASE i])))))))
 
 (defconstraint phase-transition ()
-  (if-eq end_phase 1
+  (if-eq PHASE_END 1
          (begin
-          (eq! 1 (+ (* [PHASE 0 (next [Phase 1])])
-                    (* [PHASE 1 (next [Phase 2])])
-                    (* [PHASE 2 (next [Phase 3])])
-                    (* [PHASE 3 (next [Phase 4])])
-                    (* [PHASE 4 (next [Phase 0])])))
+          (eq! 1 (+ (* [PHASE 0] (next [PHASE 1]))
+                    (* [PHASE 1] (next [PHASE 2]))
+                    (* [PHASE 2] (next [PHASE 3]))
+                    (* [PHASE 3] (next [PHASE 4]))
+                    (* [PHASE 4] (next [PHASE 0]))))
           (if-eq [PHASE 4] 1
             (begin
               (vanishes! TXRCPT_SIZE)
@@ -126,7 +128,7 @@
 ;;    4.1.3 Byte decomposition's loop heartbeat  ;;
 
 (defconstraint ct-imply-done (:guard ABS_TX_NUM)
-  (if-eq-else CT (- number_step 1)
+  (if-eq-else CT (- nSTEP 1)
               (eq! DONE 1)
               (vanishes! DONE)))
 
@@ -168,14 +170,13 @@
 
 (defconstraint phasesize-update ()
   (if-eq 1 (+ (* [PHASE 3] (- 1 IS_PREFIX))
-              (* [PHASE 4] DEPTH_1)))
-    (eq! PHASE_SIZE (- (prev PHASE_SIZE) (* LC nBYTES))))
+              (* [PHASE 4] DEPTH_1))
+    (eq! PHASE_SIZE (- (prev PHASE_SIZE) (* LC nBYTES)))))
 
 ;;    LC correction nullity    ;;
 (defconstraint lccorrection-nullity ()
-  (if-zero (+ ([PHASE 4] IS_DATA)
-              (- 1 [PHASE 0])))
-    (vanishes! LC_CORRECTION))
+  (if-zero (+ [PHASE 0] (* [PHASE 4] IS_DATA))
+    (vanishes! LC_CORRECTION)))
 
 ;;    4.1.8 Finalisation Constraints    ;;
 
@@ -226,8 +227,8 @@
     (begin
       (eq! nSTEP 1)
       (if-zero [INPUT 1]
-        (eq! LIMB (* int_short (^256 LLARGEMO)))
-        (eq! LIMB (* [INPUT 1] (^256 LLARGEMO))))
+        (eq! LIMB (* int_short (^ 256 LLARGEMO)))
+        (eq! LIMB (* [INPUT 1] (^ 256 LLARGEMO))))
       (eq! nBYTES 1)
       (eq! PHASE_END 1))))
 
@@ -271,6 +272,181 @@
               (eq! (shift (- k 4) nBYTES) LLARGE)))
           (eq! (+ (shift LC -4) (shift LC -3)) 1)
           (if-zero PHASE_SIZE
-            (eq! PHASE_END)
+            (eq! PHASE_END 1)
             (vanishes! PHASE_END))))
       (eq! INDEX_LOCAL (+ (prev INDEX_LOCAL) (* (prev LC) (- 1 (prev IS_PREFIX))))))))
+
+;;  Phase 4: log series Rl    ;;
+(defconstraint phase4-ifPhaseSize-noEnd (:guard [PHASE 4])
+  (if-not-zero PHASE_SIZE (vanishes! PHASE_END)))
+
+(defconstraint phase4-init (:guard [PHASE 4])
+  (if-zero (prev [PHASE 4])
+    (vanishes! (+ DEPTH_1 (- 1 IS_PREFIX) IS_TOPIC IS_DATA))))
+
+(defconstraint phase4-phaseRlpPrefix (:guard [PHASE 4])
+  (if-zero DEPTH_1
+    (begin
+      (vanishes! LOG_NUM)
+      (eq! [INPUT 2] LOG_NUM_MAX)
+      (eq! [INPUT 1] PHASE_SIZE)
+      (if-zero [INPUT 1]
+        (begin
+          (eq! nSTEP 1)
+          (eq! LIMB (* list_short (^ 256 LLARGEMO)))
+          (eq! nBYTES 1)
+          (eq! PHASE_END 1))
+        (begin
+          (eq! nSTEP 8)
+          (rlpPrefixOfByteString [INPUT 1] CT nSTEP DONE [PHASE 4]
+                                 ACC_SIZE POWER BIT [ACC 1] [ACC 2]
+                                 LC LIMB nBYTES)
+          (if-eq DONE 1
+            (vanishes! (+ (- 1 (next DEPTH_1))
+                          (- 1 (next IS_PREFIX))
+                          (next IS_TOPIC)
+                          (next IS_DATA)))))))))
+
+(defconstraint phase4-logentryRlpPrefix (:guard [PHASE 4])
+  (if-eq 1 (* DEPTH_1 IS_PREFIX (- 1 IS_TOPIC) (- 1 IS_PREFIX))
+    (begin
+      (eq! [INPUT 1] LOG_ENTRY_SIZE)
+      (eq! nSTEP 8)
+      (rlpPrefixOfByteString [INPUT 1] CT nSTEP DONE [PHASE 4]
+                             ACC_SIZE POWER BIT [ACC 1] [ACC 2]
+                             LC LIMB nBYTES)
+      (if-eq DONE 1
+        (vanishes! (+ (next IS_PREFIX) (next IS_TOPIC) (next IS_DATA)))))))
+
+(defconstraint phase4-rlpAddress (:guard [PHASE 4])
+  (if-zero (+ IS_PREFIX IS_TOPIC IS_DATA)
+    (begin
+      (eq! nSTEP 3)
+      (eq! LC 1)
+      (if-eq DONE 1
+        (begin 
+          (eq! (shift LIMB -2) (* (+ int_short 20) (^ 256 LLARGEMO)))
+          (eq! (shift nBYTES -2) 1)
+          (eq! (prev LIMB) (* [INPUT 1] (^ 256 12)))
+          (eq! (prev LIMB) 4)
+          (eq! LIMB [INPUT 2])
+          (eq! nBYTES LLARGE)
+          (vanishes! (+ (- 1 IS_PREFIX) (- 1 IS_TOPIC) IS_DATA)))))))
+
+(defconstraint phase4-topic-prefix (:guard [PHASE 4])
+  (if-eq (* IS_PREFIX IS_TOPIC) 1
+    (begin
+      (vanishes! INDEX_LOCAL)
+      (eq! nSTEP 1)
+      (if-zero LOCAL_SIZE
+        (begin 
+          (eq! LIMB (* list_short (^ 256 LLARGEMO)))
+          (eq! nBYTES 1)
+          (eq! (next [INPUT 2]) INDEX_LOCAL)
+          (vanishes! (+ (- 1 (next IS_PREFIX))
+                        (next IS_TOPIC)
+                        (- 1 (next IS_DATA)))))
+        (begin
+          (if-eq-else LOCAL_SIZE 33
+            (begin
+              (eq! LIMB (* (+ list_short LOCAL_SIZE) (^ 256 LLARGEMO)))
+              (eq! nBYTES 1))
+            (begin
+              (eq! LIMB (+ (* (+ list_long 1) (^ 256 LLARGEMO))
+                           (* LOCAL_SIZE (^ 256 14))))
+              (eq! nBYTES 2)))
+          (vanishes! (+ (next IS_PREFIX)
+                        (- 1 (next IS_TOPIC))
+                        (next IS_DATA))))))))
+
+(defconstraint phase4-topic (:guard [PHASE 4])
+  (if-zero (+ IS_PREFIX (- 1 IS_TOPIC))
+    (begin
+      (eq! nSTEP 3)
+      (eq! LC 1)
+      (if-eq DONE 1
+        (begin
+          (eq! (+ INDEX_LOCAL (shift INDEX_LOCAL -2))
+               (* 2 (+ (shift INDEX_LOCAL -3) 1)))
+          (eq! (shift LIMB -2) (* (+ int_short 32) (^ 256 LLARGEMO)))
+          (eq! (shift nBYTES -2) 1)
+          (eq! (prev LIMB) [INPUT 1])
+          (eq! (prev nBYTES) LLARGE)
+          (eq! LIMB [INPUT 2])
+          (eq! nBYTES LLARGE)
+          (if-zero LOCAL_SIZE
+            (begin
+              (eq! (next [INPUT 2]) INDEX_LOCAL)
+              (vanishes! (+ (- 1 (next IS_PREFIX)) 
+                            (next IS_TOPIC) 
+                            (- 1 (next IS_DATA)))))
+            (vanishes! (+ (next IS_PREFIX)
+                          (- 1 (next IS_TOPIC))
+                          (next IS_DATA)))))))))
+                      
+(defconstraint phase4-dataprefix (:guard [PHASE 4])
+  (if-eq (* IS_PREFIX IS_DATA) 1
+    (begin
+      (eq! [INPUT 1] LOCAL_SIZE)
+      (if-zero LOCAL_SIZE
+        (begin 
+          (eq! nSTEP 1)
+          (vanishes! LC_CORRECTION)
+          (eq! LIMB (* int_short (^ 256 LLARGEMO)))
+          (eq! nBYTES 1)
+          (vanishes! LOG_ENTRY_SIZE)
+          (if-zero PHASE_SIZE
+            (eq! PHASE_END 1)
+            (vanishes! (+ (- 1 (next IS_PREFIX))
+                          (next IS_TOPIC)
+                          (next IS_DATA)))))
+        (begin
+          (eq! nSTEP 8)
+          (if-eq LOCAL_SIZE 1
+            (begin
+              (rlpPrefixInt [INPUT 3] CT nSTEP DONE
+                            [BYTE 1] [ACC 1] ACC_SIZE POWER BIT BIT_ACC
+                            LIMB LC nBYTES)
+              (if-eq DONE 1
+                (begin
+                  (eq! (+ (prev LC_CORRECTION) LC_CORRECTION) 1)
+                  (eq! (* [INPUT 3] (^ 256 LLARGEMO)) (next [INPUT 1])))))
+            (begin
+              (vanishes! LC_CORRECTION)
+              (rlpPrefixOfByteString [INPUT 1] CT nSTEP DONE [PHASE 0]
+                                     ACC_SIZE POWER BIT [ACC 1] [ACC 2]
+                                     LC LIMB nBYTES)))
+            (if-eq DONE 1
+              (vanishes! (+ (next IS_PREFIX)
+                            (next IS_TOPIC)
+                            (- 1 (next IS_DATA))))))))))
+
+(defconstraint phase4-data (:guard [PHASE 4])
+  (if-zero (+ IS_PREFIX (- 1 IS_DATA))
+    (begin
+      (eq! INDEX_LOCAL CT)
+      (eq! LC 1)
+      (eq! LIMB [INPUT 1])
+      (if-zero DONE
+        (eq! nBYTES LLARGE)
+        (begin
+          (vanishes! LOCAL_SIZE)
+          (vanishes! LOG_ENTRY_SIZE)
+          (if-zero PHASE_SIZE
+            (eq! PHASE_END 1)
+            (vanishes! (+ (- 1 (next IS_PREFIX))
+                          (next IS_TOPIC)
+                          (next IS_DATA)))))))))
+
+(defconstraint phase4-logNum-update (:guard [PHASE 4])
+  (if-zero (+ CT (- 1 DEPTH_1) (- 1 IS_PREFIX) IS_TOPIC IS_DATA)
+    (did-inc! LOG_NUM 1)
+    (remained-constant! LOG_NUM)))
+
+(defconstraint phase4-logEntrySize-update (:guard [PHASE 4])
+  (if-zero (+ (- 1 DEPTH_1) (* IS_PREFIX (- 1 IS_TOPIC) (- 1 IS_DATA)))
+    (eq! LOG_ENTRY_SIZE (- (prev LOG_ENTRY_SIZE) (* LC nBYTES)))))
+
+(defconstraint phase4-localsize-update (:guard [PHASE 4])
+  (if-zero (+ IS_PREFIX (- 1 (+ IS_TOPIC IS_DATA)))
+    (eq! LOCAL_SIZE (- (prev LOCAL_SIZE) (* LC nBYTES)))))
