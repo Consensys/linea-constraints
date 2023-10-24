ad68927b9600840c6589aa5786a7fc85ce7948bf
Lorenzo Gentile <lorenzogentile404@gmail.com>
2023-10-24 15:04:11 +0200

    OOB added columns

---
+++ oob/columns.lisp
@@ -0,0 +1,24 @@
+(module oob)
+
+(defcolumns 
+  STAMP
+  CT
+  CT_MAX
+  (INCOMING_DATA :array [6])
+  (OOB_EVENT :boolean :array [2])
+  (INCOMING_INST :display :opcode)
+  (IS_JUMP :boolean)
+  (IS_JUMPI :boolean)
+  (IS_RDC :boolean)
+  (IS_CDL :boolean)
+  (IS_CALL :boolean)
+  (IS_CREATE :boolean)
+  (IS_SSTORE :boolean)
+  (IS_RETURN :boolean)
+  (WCP :boolean)
+  (ADD :boolean)
+  (OUTGOING_INST :display :opcode)
+  (OUTGOING_DATA :array [4])
+  OUTGOING_RES_LO)
+
+
