573102f7c5f5d759a37f994f12e0caa5707a3c0d
Lorenzo Gentile <lorenzogentile404@gmail.com>
2023-10-25 11:47:01 +0200

    OOB added constants and support functions

--- oob/constraints.lisp
+++ oob/constraints.lisp
@@ -1,5 +1,58 @@
 (module oob)
 
+(defconst 
+  C_JUMP        0x56
+  C_JUMPI       0x57
+  C_RDC         0x3E
+  C_CDL         0x35
+  C_CALL        0xCA
+  C_CREATE      0xCE
+  C_SSTORE      0x55
+  C_RETURN      0xF3
+  CT^INF_JUMP   0
+  CT^INF_JUMPI  1
+  CT^INF_RDC    2
+  CT^INF_CDL    0
+  CT^INF_CALL   1
+  CT^INF_CREATE 2
+  CT^INF_SSTORE 0
+  CT^INF_RETURN 0)
+
+(defun (flag_sum)
+  (IS_JUMP + IS_JUMPI + IS_RDC + IS_CDL + IS_CALL + IS_CREATE + IS_SSTORE + IS_RETURN))
+
+(defun (wght_sum)
+  (C_JUMP*IS_JUMP +
+                  C_JUMPI*IS_JUMPI
+                  +
+                  C_RDC*IS_RDC
+                  +
+                  C_CDL*IS_CDL
+                  +
+                  C_CALL*IS_CALL
+                  +
+                  C_CREATE*IS_CREATE
+                  +
+                  C_SSTORE*IS_SSTORE
+                  +
+                  C_RETURN*IS_RETURN))
+
+(defun (maxct_sum)
+  (CT^INF_JUMP*IS_JUMP +
+                       CT^INF_JUMPI*IS_JUMPI
+                       +
+                       CT^INF_RDC*IS_RDC
+                       +
+                       CT^INF_CDL*IS_CDL
+                       +
+                       CT^INF_CALL*IS_CALL
+                       +
+                       CT^INF_CREATE*IS_CREATE
+                       +
+                       CT^INF_SSTORE*IS_SSTORE
+                       +
+                       CT^INF_RETURN*IS_RETURN))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;                             ;;
 ;;    2.3 counter constancy    ;;
