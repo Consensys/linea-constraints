1c9c4ebaf51059ed4e96abb63adf8bff5e87cd5c
Francois Bojarski <francois.bojarski@consensys.net>
2023-11-17 15:05:10 +0400

    fix: add selector for logInfo -> logData lookup

--- logInfo/lookups/loginfo-to-logdata.lisp
+++ logInfo/lookups/loginfo-to-logdata.lisp
@@ -1,3 +1,6 @@
+(defpurefun (sel_logInfo_to_logData)
+  logInfo.TXN_EMITS_LOGS)
+
 (deflookup 
   logInfo-into-logdata
   ;reference columns
@@ -8,9 +11,9 @@
   )
   ;source columns
   (
-    logInfo.ABS_LOG_NUM_MAX
-    logInfo.ABS_LOG_NUM
-    logInfo.DATA_SIZE
+    (* logInfo.ABS_LOG_NUM_MAX (sel_logInfo_to_logData))
+    (* logInfo.ABS_LOG_NUM (sel_logInfo_to_logData))
+    (* logInfo.DATA_SIZE (sel_logInfo_to_logData))
   ))
 
 
