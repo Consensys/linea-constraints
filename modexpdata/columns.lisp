12a2516b11eaee22c1166b95fc0da21df8d1aa30
Tsvetan Dimitrov <tsvetan.dimitrov@consensys.net>
2024-01-18 11:39:01 +0200

    feat: rename to modexpdata and add constants

---
+++ modexpdata/columns.lisp
@@ -0,0 +1,11 @@
+(module modexpdata)
+
+(defcolumns 
+  (STAMP :byte)
+  (CT :byte)
+  (RESULT_DATA_CONTEXT :i32)
+  (BEMR :byte)
+  (INDEX :byte :display :dec)
+  (LIMB :i16 :display :bytes)
+  (BYTES :byte))
+
