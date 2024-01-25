f32474d7fb2dae3d80586c6d7dc9e03ed7df55e0
Francois Bojarski <francois.bojarski@consensys.net>
2024-01-25 13:08:18 +0100

    feat(mmu): mmu inst const + colums

--- mmu/columns.lisp
+++ mmu/columns.lisp
@@ -2,19 +2,19 @@
 
 (defcolumns 
   ;;shared columns
-  (STAMP :i16)
-  (MMIO_STAMP :i24)
+  (STAMP :i32)
+  (MMIO_STAMP :i32)
   ;; PERSPECTIVE SELECTOR
   (MACRO :binary@prove)
   (PRPRC :binary@prove)
   (MICRO :binary@prove)
   ;; OUTPUT OF THE PREPROCESSING
-  TOT
-  TOTLZ
-  TOTNT
-  TOTRZ
-  (OUT :array [5])               ;;TODO put right limit
-  (BIN :binary@prove :array [5]) ;;TODO put right limit
+  (TOT :i16)
+  (TOTLZ :i16)
+  (TOTNT :i16)
+  (TOTRZ :i16)
+  (OUT :i32 :array [5])
+  (BIN :binary :array [5])
   ;; MMU INSTRUCTION FLAG
   (IS_MLOAD :binary@prove)
   (IS_MSTORE :binary@prove)
@@ -22,31 +22,32 @@
   (IS_INVALID_CODE_PREFIX :binary@prove)
   (IS_RIGHT_PADDED_WORD_EXTRACTION :binary@prove)
   (IS_RAM_TO_EXO_WITH_PADDING :binary@prove)
-  (IS_EXO_TO-RAM_TRANSPLANTS :binary@prove)
+  (IS_EXO_TO_RAM_TRANSPLANTS :binary@prove)
   (IS_RAM_TO_RAM_SANS_PADDING :binary@prove)
-  (IS_ANY_TO_RAM_WITH_PADDING :binary@prove)
+  (IS_ANY_TO_RAM_WITH_PADDING_SOME_DATA :binary@prove)
+  (IS_ANY_TO_RAM_WITH_PADDING_PURE_PADDING :binary@prove)
   (IS_MODEXP_ZERO :binary@prove)
   (IS_MODEXP_DATA :binary@prove)
   (IS_BLAKE_PARAM :binary@prove)
   ;; USED ONLY IN MICRO ROW BUT ARE SHARED 
-  LZRO
-  NT_ONLY
-  NT_FIRST
-  NT_MDDL
-  NT_LAST
-  RZ_ONLY
-  RZ_FIRST
-  RZ_MDDL
-  RZ_LAST)
+  (LZRO :binary@prove)
+  (NT_ONLY :binary@prove)
+  (NT_FIRST :binary@prove)
+  (NT_MDDL :binary@prove)
+  (NT_LAST :binary@prove)
+  (RZ_ONLY :binary@prove)
+  (RZ_FIRST :binary@prove)
+  (RZ_MDDL :binary@prove)
+  (RZ_LAST :binary@prove))
 
 (defperspective macro
 
   ;; selector
   MACRO
   ((INST :i16)
-   SRC_ID
-   TGT_ID
-   AUX_ID
+   (SRC_ID :i32)
+   (TGT_ID :i32)
+   (AUX_ID :i32)
    (SRC_OFFSET_HI :i128)
    (SRC_OFFSET_LO :i128)
    (TGT_OFFSET_LO :i64)
@@ -56,14 +57,14 @@
    (SUCCESS_BIT :binary)
    (LIMB_1 :i128)
    (LIMB_2 :i128)
-   PHASE
-   EXO_SUM))
+   (PHASE :i32)
+   (EXO_SUM :i32)))
 
 (defperspective prprc
 
   ;; selector
   PRPRC
-  (CT
+  ((CT :i16)
    (EUC_FLAG :binary)
    (EUC_A :i64)
    (EUC_B :i64)
@@ -82,20 +83,20 @@
 
   ;; selector
   MICRO
-  (INST
+  ((INST :i16)
    (SIZE :byte)
-   (SLO :i56)
-   (SBO :i5)
-   (TLO :i56)
-   (TBO :i5)
+   (SLO :i32)
+   (SBO :byte)
+   (TLO :i32)
+   (TBO :byte)
    (LIMB_1 :i128)
    (LIMB_2 :i128)
-   CN_S
-   CN_T
+   (CN_S :i32)
+   (CN_T :i32)
    (SUCCESS_BIT :binary)
-   EXO_SUM
-   PHASE
-   ID_1
-   ID_2))
+   (EXO_SUM :i32)
+   (PHASE :i32)
+   (ID_1 :i32)
+   (ID_2 :i32)))
 
 
