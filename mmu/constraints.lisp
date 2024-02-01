e570886a14d1768328a409f15ef57373713a2995
Francois Bojarski <francois.bojarski@consensys.net>
2024-02-01 12:03:08 +0100

    feat(mmu): heartbeat & constancies

--- mmu/constraints.lisp
+++ mmu/constraints.lisp
@@ -1,2 +1,92 @@
+(module mmu)
+
+(defun (flag-sum)
+  (+ MACRO PRPRC MICRO))
+
+(defconstraint perspective-flag ()
+  (begin (debug (is-binary (flag-sum)))
+         (if-zero STAMP
+                  (vanishes! (flag-sum))
+                  (eq! (flag-sum) 1))))
+
+;; Heartbeat
+(defconstraint first-row (:domain {0})
+  (vanishes! STAMP))
+
+(defconstraint no-macrostamp-no-microstamp ()
+  (if-zero STAMP
+           (vanishes! MMIO_STAMP)))
+
+(defconstraint mmu-stamp-evolution ()
+  (did-inc! STAMP MACRO))
+
+(defconstraint mmio-stamp-evolution ()
+  (did-inc! MMIO_STAMP MICRO))
+
+(defconstraint prprc-after-macro (:guard MACRO)
+  (eq! (next PRPRC) 1))
+
+(defconstraint after-prprc (:guard PRPRC)
+  (begin (debug (eq! (+ (next PRPRC) (next MICRO))
+                     1))
+         (if-zero prprc/CT
+                  (will-eq! MICRO 1)
+                  (begin (will-dec! prprc/CT 1)
+                         (will-eq! PRPRC 1)))))
+
+(defconstraint tot-nb-of-micro-inst ()
+  (eq! TOT (+ TOTLZ TOTNT TOTRZ)))
+
+(defconstraint after-micro (:guard MICRO)
+  (begin (debug (eq! (+ (next MICRO) (next MACRO))
+                     1))
+         (did-dec! TOT 1)
+         (if-zero TOT
+                  (begin (will-eq! MACRO 1)
+                         (debug (vanishes! TOTLZ))
+                         (debug (vanishes! TOTNT))
+                         (debug (vanishes! TOTRZ)))
+                  (will-eq! MICRO 1))
+         (if-zero (prev TOTLZ)
+                  (vanishes! TOTLZ)
+                  (did-dec! TOTLZ 1))
+         (if-zero (prev TOTNT)
+                  (vanishes! TOTNT)
+                  (did-dec! (+ TOTLZ TOTNT) 1))))
+
+(defconstraint last-row (:domain {-1})
+  (if-not-zero STAMP
+               (begin (eq! MICRO 1)
+                      (vanishes! TOT))))
+
+;; Constancies
+(defun (prprc-constant X)
+  (if-eq PRPRC 1 (remained-constant! X)))
+
+(defconstraint prprc-constancies ()
+  (begin (prprc-constant TOT)
+         (debug (prprc-constant TOTLZ))
+         (debug (prprc-constant TOTNT))
+         (debug (prprc-constant TOTRZ))))
+
+(defun (stamp-decrementing X)
+  (if-not-zero (- STAMP
+                  (+ (prev STAMP) 1))
+               (any! (remained-constant! X) (did-inc! X 1))))
+
+(defconstraint stamp-decrementings ()
+  (begin (stamp-decrementing TOT)
+         (stamp-decrementing TOTLZ)
+         (stamp-decrementing TOTNT)
+         (stamp-decrementing TOTRZ)))
+
+(defun (stamp-constant X)
+  (if-not-zero (- STAMP
+                  (+ (prev STAMP) 1))
+               (remained-constant! X)))
+
+(defconstraint stamp-constancies ()
+  (begin (for i [5] (stamp-constant [OUT 1]))
+         (for i [5] (stamp-constant [BIN 1]))))
 
 
