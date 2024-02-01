99f65da33ca6b6e11c800c2cb52214dcee41a393
Francois Bojarski <francois.bojarski@consensys.net>
2024-02-01 13:31:44 +0100

    feat(mmu): inst decoding wip

--- mmu/constraints.lisp
+++ mmu/constraints.lisp
@@ -9,7 +9,9 @@
                   (vanishes! (flag-sum))
                   (eq! (flag-sum) 1))))
 
+;;
 ;; Heartbeat
+;;
 (defconstraint first-row (:domain {0})
   (vanishes! STAMP))
 
@@ -59,7 +61,9 @@
                (begin (eq! MICRO 1)
                       (vanishes! TOT))))
 
+;;
 ;; Constancies
+;;
 (defun (prprc-constant X)
   (if-eq PRPRC 1 (remained-constant! X)))
 
@@ -87,6 +91,42 @@
 
 (defconstraint stamp-constancies ()
   (begin (for i [5] (stamp-constant [OUT 1]))
-         (for i [5] (stamp-constant [BIN 1]))))
+         (for i [5] (stamp-constant [BIN 1]))
+         (stamp-constant (weight-flag-sum))
+         (stamp-constant IS_ANY_TO_RAM_WITH_PADDING_SOME_DATA)))
+
+;;
+;; Instruction Decoding
+;;
+(defun (is-any-to-ram-with-padding)
+  (+ IS_ANY_TO_RAM_WITH_PADDING_SOME_DATA IS_ANY_TO_RAM_WITH_PADDING_PURE_PADDING))
+
+(defun (inst-flag-sum)
+  (+ IS_MLOAD
+     IS_MSTORE
+     IS_MSTORE8
+     IS_INVALID_CODE_PREFIX
+     IS_RIGHT_PADDED_WORD_EXTRACTION
+     IS_RAM_TO_EXO_WITH_PADDING
+     IS_EXO_TO_RAM_TRANSPLANTS
+     IS_RAM_TO_RAM_SANS_PADDING
+     (is-any-to-ram-with-padding)
+     IS_MODEXP_ZERO
+     IS_MODEXP_DATA
+     IS_BLAKE_PARAM))
+
+(defun (weight-flag-sum)
+  (+ (* MMU_INST_MLOAD IS_MLOAD)
+     (* MMU_INST_MSTORE IS_MSTORE)
+     (* MMU_INST_MSTORE8 IS_MSTORE8)
+     (* MMU_INST_INVALID_CODE_PREFIX IS_INVALID_CODE_PREFIX)
+     (* MMU_INST_RIGHT_PADDED_WORD_EXTRACTION IS_RIGHT_PADDED_WORD_EXTRACTION)
+     (* MMU_INST_RAM_TO_EXO_WITH_PADDING IS_RAM_TO_EXO_WITH_PADDING)
+     (* MMU_INST_EXO_TO_RAM_TRANSPLANTS IS_EXO_TO_RAM_TRANSPLANTS)
+     (* MMU_INST_RAM_TO_RAM_SANS_PADDING IS_RAM_TO_RAM_SANS_PADDING)
+     (* MMU_INST_ANY_TO_RAM_WITH_PADDING (is-any-to-ram-with-padding))
+     (* MMU_INST_MODEXP_ZERO IS_MODEXP_ZERO)
+     (* MMU_INST_MODEXP_DATA IS_MODEXP_DATA)
+     (* MMU_INST_BLAKE_PARAM IS_BLAKE_PARAM)))
 
 
