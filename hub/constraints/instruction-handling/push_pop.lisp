4f5cbc0f965883e54faddf0c3340d976368ecf2e
OlivierBBB <olivier.begassat.cours@gmail.com>
2024-03-07 00:46:31 +0100

    make corset compile + PUSH_POP wip
    removed (nth ...)
    updated Makefile to contain instruction_handling
    lispX for several deprecated files
    PUSH_POP wip

--- hub/constraints/instruction-handling/push_pop.lisp
+++ hub/constraints/instruction-handling/push_pop.lisp
@@ -2,28 +2,29 @@
 
 (defun (push-pop-no-stack-exceptions)
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-  (* PEEK_AT_STACK
-     stack/PUSHPOP_FLAG
-     (- 1 stack/SUX stack/SOX)))
+  (* PEEK_AT_STACK stack/PUSHPOP_FLAG (- 1 stack/SUX stack/SOX)))
 
 (defconstraint push-pop-stack-pattern (:guard (push-pop-no-stack-exceptions))
-               (begin
-                 (if-not-zero (- 1 [ stack/DEC_FLAG 1 ]) (stack-pattern-1-0))
-                 (if-not-zero [ stack/DEC_FLAG 1 ]       (stack-pattern-0-1))))
+  (begin (if-not-zero (- 1 [stack/DEC_FLAG 1])
+                      (stack-pattern-1-0))
+         (if-not-zero [stack/DEC_FLAG 1]
+                      (stack-pattern-0-1))))
 
 (defconstraint push-pop-setting-NSR (:guard (push-pop-no-stack-exceptions))
-               (eq! NSR CMC))
+  (eq! NSR CMC))
 
 ;; TODO: make debug
 (defconstraint push-pop-setting-peeking-flags (:guard (push-pop-no-stack-exceptions))
-               ;; (debug (eq! NSR (* CMC (next PEEK_AT_CONTEXT)))))
-               (eq! NSR (* CMC (next PEEK_AT_CONTEXT))))
+  ;; (debug (eq! NSR (* CMC (next PEEK_AT_CONTEXT)))))
+  (eq! NSR
+       (* CMC (next PEEK_AT_CONTEXT))))
 
 (defconstraint push-pop-setting-gas-costs (:guard (push-pop-no-stack-exceptions))
-               (eq! GAS_COST stack/STATIC_GAS))
+  (eq! GAS_COST stack/STATIC_GAS))
 
 (defconstraint push-pop-setting-stack-values (:guard (push-pop-no-stack-exceptions))
-               (if-not-zero [ stack/DEC_FLAG 1 ]
-                            (begin
-                              (eq! [ stack/STACK_ITEM_VALUE_HI 4 ] stack/PUSH_VALUE_HI )
-                              (eq! [ stack/STACK_ITEM_VALUE_LO 4 ] stack/PUSH_VALUE_LO ))))
+  (if-not-zero [stack/DEC_FLAG 1]
+               (begin (eq! [stack/STACK_ITEM_VALUE_HI 4] stack/PUSH_VALUE_HI)
+                      (eq! [stack/STACK_ITEM_VALUE_LO 4] stack/PUSH_VALUE_LO))))
+
+
