c07ccdbe46cea21f58cf721a64d3e7c67382aef0
Francois Bojarski <francois.bojarski@consensys.net>
2024-03-29 13:13:33 +0530

    fix: delete doublon constraints

--- hub_v2/constants.lisp
+++ hub_v2/constants.lisp
@@ -2,17 +2,15 @@
 
 ;; PHASE numbers for lookups
 (defconst 
-  PHASE_ECRECOVER_DATA    iota  ;; TODO must be mooved to constants/constants.lisp
-  PHASE_ECRECOVER_RESULT  iota  ;; TODO must be mooved to constants/constants.lisp
-  PHASE_SHA2-256_DATA     iota  ;; TODO must be mooved to constants/constants.lisp
-  PHASE_SHA2-256_RESULT   iota  ;; TODO must be mooved to constants/constants.lisp
-  PHASE_RIPEMD-160_DATA   iota  ;; TODO must be mooved to constants/constants.lisp
-  PHASE_RIPEMD-160_RESULT iota  ;; TODO must be mooved to constants/constants.lisp
-  PHASE_ECADD_DATA        iota  ;; TODO must be mooved to constants/constants.lisp
-  PHASE_ECADD_RESULT      iota  ;; TODO must be mooved to constants/constants.lisp
-  PHASE_ECMUL_DATA        iota  ;; TODO must be mooved to constants/constants.lisp
-  PHASE_ECMUL_RESULT      iota  ;; TODO must be mooved to constants/constants.lisp
-  PHASE_PAIRING_DATA      iota  ;; TODO must be mooved to constants/constants.lisp
-  PHASE_PAIRING_RESULT    iota) ;; TODO must be mooved to constants/constants.lisp
+  PHASE_ECRECOVER_DATA   iota  ;; TODO must be mooved to constants/constants.lisp
+  PHASE_ECRECOVER_RESULT iota  ;; TODO must be mooved to constants/constants.lisp
+  PHASE_SHA2-256_DATA    iota  ;; TODO must be mooved to constants/constants.lisp
+  PHASE_SHA2-256_RESULT  iota  ;; TODO must be mooved to constants/constants.lisp
+  PHASE_ECADD_DATA       iota  ;; TODO must be mooved to constants/constants.lisp
+  PHASE_ECADD_RESULT     iota  ;; TODO must be mooved to constants/constants.lisp
+  PHASE_ECMUL_DATA       iota  ;; TODO must be mooved to constants/constants.lisp
+  PHASE_ECMUL_RESULT     iota  ;; TODO must be mooved to constants/constants.lisp
+  PHASE_PAIRING_DATA     iota  ;; TODO must be mooved to constants/constants.lisp
+  PHASE_PAIRING_RESULT   iota) ;; TODO must be mooved to constants/constants.lisp
 
 
