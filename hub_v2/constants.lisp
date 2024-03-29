48e53ebd2acd5494b73fcecb7cb40b3622ce5214
Francois Bojarski <francois.bojarski@consensys.net>
2024-03-29 10:58:05 +0530

    fix: add TODOs

--- hub_v2/constants.lisp
+++ hub_v2/constants.lisp
@@ -2,17 +2,17 @@
 
 ;; PHASE numbers for lookups
 (defconst 
-  PHASE_ECRECOVER_DATA    iota
-  PHASE_ECRECOVER_RESULT  iota
-  PHASE_SHA2-256_DATA     iota
-  PHASE_SHA2-256_RESULT   iota
-  PHASE_RIPEMD-160_DATA   iota
-  PHASE_RIPEMD-160_RESULT iota
-  PHASE_ECADD_DATA        iota
-  PHASE_ECADD_RESULT      iota
-  PHASE_ECMUL_DATA        iota
-  PHASE_ECMUL_RESULT      iota
-  PHASE_PAIRING_DATA      iota
-  PHASE_PAIRING_RESULT    iota)
+  PHASE_ECRECOVER_DATA    iota  ;; TODO must be mooved to constants/constants.lisp
+  PHASE_ECRECOVER_RESULT  iota  ;; TODO must be mooved to constants/constants.lisp
+  PHASE_SHA2-256_DATA     iota  ;; TODO must be mooved to constants/constants.lisp
+  PHASE_SHA2-256_RESULT   iota  ;; TODO must be mooved to constants/constants.lisp
+  PHASE_RIPEMD-160_DATA   iota  ;; TODO must be mooved to constants/constants.lisp
+  PHASE_RIPEMD-160_RESULT iota  ;; TODO must be mooved to constants/constants.lisp
+  PHASE_ECADD_DATA        iota  ;; TODO must be mooved to constants/constants.lisp
+  PHASE_ECADD_RESULT      iota  ;; TODO must be mooved to constants/constants.lisp
+  PHASE_ECMUL_DATA        iota  ;; TODO must be mooved to constants/constants.lisp
+  PHASE_ECMUL_RESULT      iota  ;; TODO must be mooved to constants/constants.lisp
+  PHASE_PAIRING_DATA      iota  ;; TODO must be mooved to constants/constants.lisp
+  PHASE_PAIRING_RESULT    iota) ;; TODO must be mooved to constants/constants.lisp
 
 
