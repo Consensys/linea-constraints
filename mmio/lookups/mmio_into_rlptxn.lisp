6cd783117232a54bc14735eab21b986895eeb664
Francois Bojarski <francois.bojarski@consensys.net>
2024-04-02 16:36:06 +0530

    feat: target row where limb is constructed for lookup mmio to rlpTxn

--- mmio/lookups/mmio_into_rlptxn.lisp
+++ mmio/lookups/mmio_into_rlptxn.lisp
@@ -3,6 +3,7 @@
   ;reference columns
   (
     rlpTxn.ABS_TX_NUM
+    rlpTxn.LC
     rlpTxn.PHASE_ID
     rlpTxn.INDEX_DATA
     rlpTxn.LIMB
@@ -12,6 +13,7 @@
   ;source columns
   (
     (* mmio.EXO_IS_TXCD mmio.EXO_ID)
+    mmio.EXO_IS_TXCD
     (* mmio.EXO_IS_TXCD mmio.PHASE)
     (* mmio.EXO_IS_TXCD mmio.INDEX_X)
     (* mmio.EXO_IS_TXCD mmio.LIMB)
