484ea0c2e575375a13e37f3c4ce5f008f401038b
OlivierBBB <olivier.begassat.cours@gmail.com>
2024-04-07 01:16:20 +0200

    COPY instruction family WIP

--- hub/constraints/instruction-handling/copy.lisp
+++ hub/constraints/instruction-handling/copy.lisp
@@ -35,6 +35,7 @@
   ROW_OFFSET_EXTCODECOPY_NO_XAHOY_REVERT_ACCOUNT_DOING_ROW   2
   ROW_OFFSET_EXTCODECOPY_NO_XAHOY_REVERT_ACCOUNT_UNDOING_ROW 3
   ROW_OFFSET_EXTCODECOPY_NO_XAHOY_NO_REVERT_ACCOUNT_ROW      2
+  ROW_OFFSET_EXTCODECOPY_NO_XAHOY_ACCOUNT_ROW                2
   )
 
 ;; (defun (copy-inst-instruction)                                  stack/INSTRUCTION    )
@@ -53,21 +54,25 @@
 ;; (defun (copy-inst-raw-address-hi)                               [ stack/STACK_ITEM_VALUE_HI 4 ] )
 ;; (defun (copy-inst-raw-address-lo)                               [ stack/STACK_ITEM_VALUE_LO 4 ] )
 ;; ;;
-;; (defun (copy-inst-OOB-raises-return-data-exception)             ( shift    [ misc/OOB_DATA 7 ]    ROW_OFFSET_COPY_INST_MISCELLANEOUS_ROW )
-;; (defun (copy-inst-MXP-raises-memory-expansion-exception)        ( shift    misc/MXP_MXPX          ROW_OFFSET_COPY_INST_MISCELLANEOUS_ROW )
-;; (defun (copy-inst-MXP-memory-expansion-gas)                     ( shift    misc/MXP_GAS_MXP       ROW_OFFSET_COPY_INST_MISCELLANEOUS_ROW )
+;; (defun (copy-inst-OOB-raises-return-data-exception)             ( shift    [ misc/OOB_DATA 7 ]    ROW_OFFSET_COPY_INST_MISCELLANEOUS_ROW ))
+;; (defun (copy-inst-MXP-raises-memory-expansion-exception)        ( shift    misc/MXP_MXPX          ROW_OFFSET_COPY_INST_MISCELLANEOUS_ROW ))
+;; (defun (copy-inst-MXP-memory-expansion-gas)                     ( shift    misc/MXP_GAS_MXP       ROW_OFFSET_COPY_INST_MISCELLANEOUS_ROW ))
 ;; ;;
-;; (defun (copy-inst-call-data-context)                            ( shift    context/CALL_DATA_CONTEXT_NUMBER    ROW_OFFSET_CALLDATACOPY_CONTEXT_ROW )
-;; (defun (copy-inst-call-data-offset)                             ( shift    context/CALL_DATA_OFFSET            ROW_OFFSET_CALLDATACOPY_CONTEXT_ROW )
-;; (defun (copy-inst-call-data-size)                               ( shift    context/CALL_DATA_SIZE              ROW_OFFSET_CALLDATACOPY_CONTEXT_ROW )
-;; (defun (copy-inst-return-data-context)                          ( shift    context/RETURN_DATA_CONTEXT_NUMBER    ROW_OFFSET_RETURNDATACOPY_CONTEXT_ROW )
-;; (defun (copy-inst-return-data-offset)                           ( shift    context/RETURN_DATA_OFFSET            ROW_OFFSET_RETURNDATACOPY_CONTEXT_ROW )
-;; (defun (copy-inst-return-data-size)                             ( shift    context/RETURN_DATA_SIZE              ROW_OFFSET_RETURNDATACOPY_CONTEXT_ROW )
+;; (defun (copy-inst-call-data-context)                            ( shift    context/CALL_DATA_CONTEXT_NUMBER      ROW_OFFSET_CALLDATACOPY_CONTEXT_ROW ))
+;; (defun (copy-inst-call-data-offset)                             ( shift    context/CALL_DATA_OFFSET              ROW_OFFSET_CALLDATACOPY_CONTEXT_ROW ))
+;; (defun (copy-inst-call-data-size)                               ( shift    context/CALL_DATA_SIZE                ROW_OFFSET_CALLDATACOPY_CONTEXT_ROW ))
 ;; ;;
-;; (defun (copy-inst-address-warmth)                               ( shift    account/WARMTH                 ROW_OFFSET_EXTCODECOPY_ACCOUNT_ROW )
-;; (defun (copy-inst-address-has-code)                             ( shift    account/HAS_CODE               ROW_OFFSET_EXTCODECOPY_ACCOUNT_ROW )
-;; (defun (copy-inst-address-code-size)                            ( shift    account/CODE_SIZE              ROW_OFFSET_EXTCODECOPY_ACCOUNT_ROW )
-;; (defun (copy-inst-address-code-fragment-index)                  ( shift    account/CODE_FRAGMENT_INDEX    ROW_OFFSET_EXTCODECOPY_ACCOUNT_ROW )
+;; (defun (copy-inst-return-data-context)                          ( shift    context/RETURN_DATA_CONTEXT_NUMBER    ROW_OFFSET_RETURNDATACOPY_CONTEXT_ROW ))
+;; (defun (copy-inst-return-data-offset)                           ( shift    context/RETURN_DATA_OFFSET            ROW_OFFSET_RETURNDATACOPY_CONTEXT_ROW ))
+;; (defun (copy-inst-return-data-size)                             ( shift    context/RETURN_DATA_SIZE              ROW_OFFSET_RETURNDATACOPY_CONTEXT_ROW ))
+;; ;;
+;; (defun (copy-inst-current-address-code-fragment-index)          ( shift    account/CODE_FRAGMENT_INDEX           ROW_OFFSET_CODECOPY_NO_XAHOY_ACCOUNT_ROW ))
+;; (defun (copy-inst-current-address-code-size)                    ( shift    account/CODE_SIZE                     ROW_OFFSET_CODECOPY_NO_XAHOY_ACCOUNT_ROW ))
+;; ;;
+;; (defun (copy-inst-exo-address-warmth)                               ( shift    account/WARMTH                 ROW_OFFSET_EXTCODECOPY_NO_XAHOY_ACCOUNT_ROW ))
+;; (defun (copy-inst-exo-address-has-code)                             ( shift    account/HAS_CODE               ROW_OFFSET_EXTCODECOPY_NO_XAHOY_ACCOUNT_ROW ))
+;; (defun (copy-inst-exo-address-code-size)                            ( shift    account/CODE_SIZE              ROW_OFFSET_EXTCODECOPY_NO_XAHOY_ACCOUNT_ROW ))
+;; (defun (copy-inst-exo-address-code-fragment-index)                  ( shift    account/CODE_FRAGMENT_INDEX    ROW_OFFSET_EXTCODECOPY_NO_XAHOY_ACCOUNT_ROW ))
 
 
 (defun (copy-inst-standard-precondition)  (*  PEEK_AT_STACK
@@ -176,9 +181,12 @@
                                                    (copy-inst-return-data-size))))
 
 (defconstraint    copy-misc-row-setting-RDCX                                                 (:guard    (copy-inst-standard-precondition))
-                  (if-not-zero  (shift  misc/OOB_FLAG  ROW_OFFSET_COPY_INST_MISCELLANEOUS_ROW)
-                                (eq!  stack/RDCX
-                                      (copy-inst-OOB-raises-return-data-exception))))
+                  (if-zero  (shift  misc/OOB_FLAG  ROW_OFFSET_COPY_INST_MISCELLANEOUS_ROW)
+                            ;; OOB_FLAG ≡ 0
+                            ;; TODO: zero case is redundant; careful when modifying the constraint (if-zero vs. if-not-zero)
+                            (vanishes!  stack/RDCX)
+                            ;; OOB_FLAG ≡ 1
+                            (eq!  stack/RDCX (copy-inst-OOB-raises-return-data-exception))))
 
 (defconstraint    copy-misc-row-setting-MXP-instruction                                      (:guard    (copy-inst-standard-precondition))
                   (if-not-zero  (shift  misc/MXP_FLAG  ROW_OFFSET_COPY_INST_MISCELLANEOUS_ROW)
@@ -192,63 +200,95 @@
 
 (defconstraint    copy-misc-row-setting-MXPX                                                 (:guard    (copy-inst-standard-precondition))
                   (if-zero  (shift  misc/MXP_FLAG  ROW_OFFSET_COPY_INST_MISCELLANEOUS_ROW)
+                            ;; MXP_FLAG ≡ 0
+                            ;; can only happen for RETURNDATACOPY instruction raising the returnDataCopyException; redundant constraint;
                             (eq!  stack/RDCX  0)
-                            (eq!  stack/RDCX  (copy-inst-MXP-raises-memory-expansion-exception)
+                            ;; MXP_FLAG ≡ 1
+                            (eq!  stack/RDCX  (copy-inst-MXP-raises-memory-expansion-exception))))
 
 
 (defconstraint    copy-misc-row-partially-setting-the-MMU-instruction                        (:guard    (copy-inst-standard-precondition))
-(defconstraint    copy-misc-row-finishing-setting-the-MMU-instruction-CALLDATACOPY-case      (:guard    (copy-inst-standard-precondition))
-(defconstraint    copy-misc-row-finishing-setting-the-MMU-instruction-RETURNDATACOPY-case    (:guard    (copy-inst-standard-precondition))
-(defconstraint    copy-misc-row-finishing-setting-the-MMU-instruction-CODECOPY-case          (:guard    (copy-inst-standard-precondition))
-(defconstraint    copy-misc-row-finishing-setting-the-MMU-instruction-EXTCODECOPY-case       (:guard    (copy-inst-standard-precondition))
+                  (if-not-zero  (shift  misc/MMU_FLAG  ROW_OFFSET_COPY_INST_MISCELLANEOUS_ROW)
+                                (set-MMU-inst-any-to-ram-with-padding  ROW_OFFSET_COPY_INST_MISCELLANEOUS_ROW      ;; offset
+                                                                       (copy-inst-source-id)                       ;; source ID
+                                                                       CONTEXT_NUMBER                              ;; target ID
+                                                                       ;; aux_id                                      ;; auxiliary ID
+                                                                       (copy-inst-source-offset-hi)                ;; source offset high
+                                                                       (copy-inst-source-offset-lo)                ;; source offset low
+                                                                       (copy-inst-target-offset-lo)                ;; target offset low
+                                                                       (copy-inst-size-lo)                         ;; size
+                                                                       (copy-inst-reference-offset)                ;; reference offset
+                                                                       (copy-inst-reference-size)                  ;; reference size
+                                                                       ;; success_bit                                 ;; success bit
+                                                                       ;; limb_1                                      ;; limb 1
+                                                                       ;; limb_2                                      ;; limb 2
+                                                                       (copy-inst-exo-sum)                         ;; weighted exogenous module flag sum
+                                                                       ;; phase                                       ;; phase
+                                                                       )))
 
+(defun  (copy-inst-source-id)  (+  (*  (copy-inst-is-CALLDATACOPY)     (copy-inst-call-data-context))
+                                   (*  (copy-inst-is-RETURNDATACOPY)   (copy-inst-return-data-context))
+                                   (*  (copy-inst-is-CODECOPY)         (copy-inst-current-address-code-fragment-index))
+                                   (*  (copy-inst-is-EXTCODECOPY)      (copy-inst-exo-address-code-fragment-index))))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;                                       ;;
-;;    X.Y.4 Specifics for CALLDATACOPY   ;;
-;;                                       ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defun  (copy-inst-reference-offset)  (+  (*  (copy-inst-is-CALLDATACOPY)     (copy-inst-call-data-offset))
+                                          (*  (copy-inst-is-RETURNDATACOPY)   (copy-inst-return-data-offset))))
 
+(defun  (copy-inst-reference-size)  (+  (*  (copy-inst-is-CALLDATACOPY)     (copy-inst-call-data-size))
+                                        (*  (copy-inst-is-RETURNDATACOPY)   (copy-inst-return-data-size))
+                                        (*  (copy-inst-is-CODECOPY)         (copy-inst-current-address-code-size))
+                                        (*  (copy-inst-is-EXTCODECOPY)      (*  (copy-inst-exo-address-code-fragment-index)
+                                                                                (copy-inst-exo-address-has-code)))))
 
-(defconstraint   copy-setting-the-gas-cost-for-CALLDATACOPY                      (:guard   (*   (copy-inst-standard-precondition)   (copy-inst-is-CALLDATACOPY)))
-(defconstraint   copy-setting-the-context-row-for-CALLDATACOPY                   (:guard   (*   (copy-inst-standard-precondition)   (copy-inst-is-CALLDATACOPY)))
+(defun  (copy-inst-exo-sum)  (*  (+  (copy-inst-is-CODECOPY) (copy-inst-is-EXTCODECOPY))
+                                 EXO_SUM_WEIGHT_ROM))
 
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;                                         ;;
-;;    X.Y.5 Specifics for RETURNDATACOPY   ;;
-;;                                         ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-(defconstraint   copy-setting-the-gas-cost-for-RETURNDATACOPY                   (:guard   (*   (copy-inst-standard-precondition)    (copy-inst-is-RETURNDATACOPY)))
-(defconstraint   copy-setting-the-context-row-for-RETURNDATACOPY                (:guard   (*   (copy-inst-standard-precondition)    (copy-inst-is-RETURNDATACOPY)))
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;                                   ;;
-;;    X.Y.6 Specifics for CODECOPY   ;;
-;;                                   ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-(defconstraint   copy-setting-the-gas-cost-for-CODECOPY                         (:guard   (*   (copy-inst-standard-precondition)    (copy-inst-is-CODECOPY)))
-(defconstraint   copy-setting-the-context-row-for-exceptional-CODECOPY          (:guard   (*   (copy-inst-standard-precondition)    (copy-inst-is-CODECOPY)))
-(defconstraint   copy-setting-the-context-row-for-unexceptional-CODECOPY        (:guard   (*   (copy-inst-standard-precondition)    (copy-inst-is-CODECOPY)))
-(defconstraint   copy-setting-the-account-row-for-unexceptional-CODECOPY        (:guard   (*   (copy-inst-standard-precondition)    (copy-inst-is-CODECOPY)))
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;                                      ;;
-;;    X.Y.7 Specifics for EXTCODECOPY   ;;
-;;                                      ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defun           (copy-inst-trigger-CFI)
-
-(defconstraint   copy-setting-the-gas-cost-for-EXTCODECOPY                                        (:guard   (*   (copy-inst-standard-precondition)    (copy-inst-is-EXTCODECOPY)))
-(defconstraint   copy-the-MXPX-case-for-EXTCODECOPY                                               (:guard   (*   (copy-inst-standard-precondition)    (copy-inst-is-EXTCODECOPY)))
-(defconstraint   copy-the-OOGX-case-for-EXTCODECOPY                                               (:guard   (*   (copy-inst-standard-precondition)    (copy-inst-is-EXTCODECOPY)))
-(defconstraint   copy-inst-unexceptional-reverted-EXTCODECOPY-doing-account-row                   (:guard   (*   (copy-inst-standard-precondition)    (copy-inst-is-EXTCODECOPY)))
-(defconstraint   copy-inst-unexceptional-reverted-EXTCODECOPY-undoing-account-row                 (:guard   (*   (copy-inst-standard-precondition)    (copy-inst-is-EXTCODECOPY)))
-(defconstraint   copy-inst-unexceptional-unreverted-EXTCODECOPY-account-row                       (:guard   (*   (copy-inst-standard-precondition)    (copy-inst-is-EXTCODECOPY)))
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ;;                                       ;;
+;; ;;    X.Y.4 Specifics for CALLDATACOPY   ;;
+;; ;;                                       ;;
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;
+;; (defconstraint   copy-setting-the-gas-cost-for-CALLDATACOPY                      (:guard   (*   (copy-inst-standard-precondition)   (copy-inst-is-CALLDATACOPY)))
+;; (defconstraint   copy-setting-the-context-row-for-CALLDATACOPY                   (:guard   (*   (copy-inst-standard-precondition)   (copy-inst-is-CALLDATACOPY)))
+;;
+;;
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ;;                                         ;;
+;; ;;    X.Y.5 Specifics for RETURNDATACOPY   ;;
+;; ;;                                         ;;
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;
+;; (defconstraint   copy-setting-the-gas-cost-for-RETURNDATACOPY                   (:guard   (*   (copy-inst-standard-precondition)    (copy-inst-is-RETURNDATACOPY)))
+;; (defconstraint   copy-setting-the-context-row-for-RETURNDATACOPY                (:guard   (*   (copy-inst-standard-precondition)    (copy-inst-is-RETURNDATACOPY)))
+;;
+;;
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ;;                                   ;;
+;; ;;    X.Y.6 Specifics for CODECOPY   ;;
+;; ;;                                   ;;
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;
+;; (defconstraint   copy-setting-the-gas-cost-for-CODECOPY                         (:guard   (*   (copy-inst-standard-precondition)    (copy-inst-is-CODECOPY)))
+;; (defconstraint   copy-setting-the-context-row-for-exceptional-CODECOPY          (:guard   (*   (copy-inst-standard-precondition)    (copy-inst-is-CODECOPY)))
+;; (defconstraint   copy-setting-the-context-row-for-unexceptional-CODECOPY        (:guard   (*   (copy-inst-standard-precondition)    (copy-inst-is-CODECOPY)))
+;; (defconstraint   copy-setting-the-account-row-for-unexceptional-CODECOPY        (:guard   (*   (copy-inst-standard-precondition)    (copy-inst-is-CODECOPY)))
+;;
+;;
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ;;                                      ;;
+;; ;;    X.Y.7 Specifics for EXTCODECOPY   ;;
+;; ;;                                      ;;
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; (defun           (copy-inst-trigger-CFI)
+;;
+;; (defconstraint   copy-setting-the-gas-cost-for-EXTCODECOPY                                        (:guard   (*   (copy-inst-standard-precondition)    (copy-inst-is-EXTCODECOPY)))
+;; (defconstraint   copy-the-MXPX-case-for-EXTCODECOPY                                               (:guard   (*   (copy-inst-standard-precondition)    (copy-inst-is-EXTCODECOPY)))
+;; (defconstraint   copy-the-OOGX-case-for-EXTCODECOPY                                               (:guard   (*   (copy-inst-standard-precondition)    (copy-inst-is-EXTCODECOPY)))
+;; (defconstraint   copy-inst-unexceptional-reverted-EXTCODECOPY-doing-account-row                   (:guard   (*   (copy-inst-standard-precondition)    (copy-inst-is-EXTCODECOPY)))
+;; (defconstraint   copy-inst-unexceptional-reverted-EXTCODECOPY-undoing-account-row                 (:guard   (*   (copy-inst-standard-precondition)    (copy-inst-is-EXTCODECOPY)))
+;; (defconstraint   copy-inst-unexceptional-unreverted-EXTCODECOPY-account-row                       (:guard   (*   (copy-inst-standard-precondition)    (copy-inst-is-EXTCODECOPY)))
