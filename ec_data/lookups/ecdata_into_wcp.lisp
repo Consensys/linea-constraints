e8f881b2ce61cdfb27c5ffb04688d8b15e10fed5
Lorenzo Gentile <lorenzogentile404@gmail.com>
2024-04-23 14:36:19 +0200

    ECDATA updated columns, constants and lookups

--- ec_data/lookups/ecdata_into_wcp.lisp
+++ ec_data/lookups/ecdata_into_wcp.lisp
@@ -1,3 +1,6 @@
+(defun (ec_data-into-wcp-activation-flag)
+  ec_data.WCP_FLAG)
+
 (deflookup 
   lookup-ec_data-into-wcp
   ; target columns
@@ -6,17 +9,17 @@
     wcp.ARGUMENT_1_LO
     wcp.ARGUMENT_2_HI
     wcp.ARGUMENT_2_LO
-    wcp.INST
     wcp.RESULT
+    wcp.INST
   )
   ; source columns
   (
-    ec_data.WCP_ARG1_HI
-    ec_data.WCP_ARG1_LO
-    ec_data.WCP_ARG2_HI
-    ec_data.WCP_ARG2_LO
-    ec_data.WCP_INST
-    ec_data.WCP_RES
+    (* ec_data.WCP_ARG1_HI (ec_data-into-wcp-activation-flag))
+    (* ec_data.WCP_ARG1_LO (ec_data-into-wcp-activation-flag))
+    (* ec_data.WCP_ARG2_HI (ec_data-into-wcp-activation-flag))
+    (* ec_data.WCP_ARG2_LO (ec_data-into-wcp-activation-flag))
+    (* ec_data.WCP_RES (ec_data-into-wcp-activation-flag))
+    (* ec_data.WCP_INST (ec_data-into-wcp-activation-flag))
   ))
 
 
