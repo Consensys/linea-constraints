3d7b696abcac630169f026ecb63acd9b72ff518c
Lorenzo Gentile <lorenzogentile404@gmail.com>
2024-04-29 22:29:01 +0200

    feat(ecdata): removed constants

--- ec_data/constants.lisp
+++ ec_data/constants.lisp
@@ -1,10 +1,8 @@
 (module ec_data)
 
 (defconst 
-  P_BN                          0x30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd47
   P_BN_HI                       0x30644e72e131a029b85045b68181585d
   P_BN_LO                       0x97816a916871ca8d3c208c16d87cfd47
-  SECP256K1N                    0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f
   SECP256K1N_HI                 0xffffffffffffffffffffffffffffffff
   SECP256K1N_LO                 0xfffffffffffffffffffffffefffffc2f
   LT                            0x10
