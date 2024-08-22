17ddde419fa37b7866c9d92dbd90340d9e2c826b
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2024-08-22 17:03:25 +0200

    fix: some OOB formatting

--- oob/constraints.lisp
+++ oob/constraints.lisp
@@ -5,77 +5,103 @@
 ;;   2 Constraints     ;;
 ;;                     ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;                     ;;
 ;; 2.1 shorthands and  ;;
 ;;     constants       ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defun (flag-sum-inst)    (+ IS_JUMP IS_JUMPI IS_RDC IS_CDL IS_XCALL IS_CALL IS_CREATE IS_SSTORE IS_DEPLOYMENT))
-(defun (flag-sum-prc-common)  (+ IS_ECRECOVER IS_SHA2 IS_RIPEMD IS_IDENTITY IS_ECADD IS_ECMUL IS_ECPAIRING))
-(defun (flag-sum-prc-blake)  (+ IS_BLAKE2F_CDS IS_BLAKE2F_PARAMS))
-(defun (flag-sum-prc-modexp)  (+ IS_MODEXP_CDS IS_MODEXP_XBS IS_MODEXP_LEAD IS_MODEXP_PRICING IS_MODEXP_EXTRACT))
-(defun (flag-sum-prc)  (+ (flag-sum-prc-common) (flag-sum-prc-blake) (flag-sum-prc-modexp)))
-(defun (flag-sum)  (+ (flag-sum-inst) (flag-sum-prc)))
-(defun (wght-sum-inst)  (+ (* OOB_INST_JUMP IS_JUMP)
-     (* OOB_INST_JUMPI IS_JUMPI)
-     (* OOB_INST_RDC IS_RDC)
-     (* OOB_INST_CDL IS_CDL)
-     (* OOB_INST_XCALL IS_XCALL)
-     (* OOB_INST_CALL IS_CALL)
-     (* OOB_INST_CREATE IS_CREATE)
-     (* OOB_INST_SSTORE IS_SSTORE)
-     (* OOB_INST_DEPLOYMENT IS_DEPLOYMENT)))
-(defun (wght-sum-prc-common)
-  (+ (* OOB_INST_ECRECOVER IS_ECRECOVER)
-     (* OOB_INST_SHA2 IS_SHA2)
-     (* OOB_INST_RIPEMD IS_RIPEMD)
-     (* OOB_INST_IDENTITY IS_IDENTITY)
-     (* OOB_INST_ECADD IS_ECADD)
-     (* OOB_INST_ECMUL IS_ECMUL)
-     (* OOB_INST_ECPAIRING IS_ECPAIRING)))
-(defun (wght-sum-prc-blake)  (+ (* OOB_INST_BLAKE_CDS IS_BLAKE2F_CDS) (* OOB_INST_BLAKE_PARAMS IS_BLAKE2F_PARAMS)))
-(defun (wght-sum-prc-modexp)  (+ (* OOB_INST_MODEXP_CDS IS_MODEXP_CDS)
-     (* OOB_INST_MODEXP_XBS IS_MODEXP_XBS)
-     (* OOB_INST_MODEXP_LEAD IS_MODEXP_LEAD)
-     (* OOB_INST_MODEXP_PRICING IS_MODEXP_PRICING)
-     (* OOB_INST_MODEXP_EXTRACT IS_MODEXP_EXTRACT)))
-(defun (wght-sum-prc)  (+ (wght-sum-prc-common) (wght-sum-prc-blake) (wght-sum-prc-modexp)))
-(defun (wght-sum)  (+ (wght-sum-inst) (wght-sum-prc)))
-(defun (maxct-sum-inst)  (+ (* CT_MAX_JUMP IS_JUMP)
-     (* CT_MAX_JUMPI IS_JUMPI)
-     (* CT_MAX_RDC IS_RDC)
-     (* CT_MAX_CDL IS_CDL)
-     (* CT_MAX_XCALL IS_XCALL)
-     (* CT_MAX_CALL IS_CALL)
-     (* CT_MAX_CREATE IS_CREATE)
-     (* CT_MAX_SSTORE IS_SSTORE)
-     (* CT_MAX_DEPLOYMENT IS_DEPLOYMENT)))
-(defun (maxct-sum-prc-common)    (+ (* CT_MAX_ECRECOVER IS_ECRECOVER)
-     (* CT_MAX_SHA2 IS_SHA2)
-     (* CT_MAX_RIPEMD IS_RIPEMD)
-     (* CT_MAX_IDENTITY IS_IDENTITY)
-     (* CT_MAX_ECADD IS_ECADD)
-     (* CT_MAX_ECMUL IS_ECMUL)
-     (* CT_MAX_ECPAIRING IS_ECPAIRING)))
+(defun (flag-sum-inst)                      (+    IS_JUMP IS_JUMPI
+                                                  IS_RDC
+                                                  IS_CDL
+                                                  IS_XCALL
+                                                  IS_CALL
+                                                  IS_CREATE
+                                                  IS_SSTORE
+                                                  IS_DEPLOYMENT))
+(defun (flag-sum-prc-common)                (+    IS_ECRECOVER
+                                                  IS_SHA2
+                                                  IS_RIPEMD
+                                                  IS_IDENTITY
+                                                  IS_ECADD
+                                                  IS_ECMUL
+                                                  IS_ECPAIRING))
+(defun (flag-sum-prc-blake)                 (+    IS_BLAKE2F_CDS
+                                                  IS_BLAKE2F_PARAMS))
+(defun (flag-sum-prc-modexp)                (+    IS_MODEXP_CDS
+                                                  IS_MODEXP_XBS
+                                                  IS_MODEXP_LEAD
+                                                  IS_MODEXP_PRICING
+                                                  IS_MODEXP_EXTRACT))
+(defun (flag-sum-prc)                       (+    (flag-sum-prc-common)
+                                                  (flag-sum-prc-blake)
+                                                  (flag-sum-prc-modexp)))
+(defun (flag-sum)                           (+    (flag-sum-inst)
+                                                  (flag-sum-prc)))
+(defun (wght-sum-inst)                      (+    (* OOB_INST_JUMP             IS_JUMP)
+                                                  (* OOB_INST_JUMPI            IS_JUMPI)
+                                                  (* OOB_INST_RDC              IS_RDC)
+                                                  (* OOB_INST_CDL              IS_CDL)
+                                                  (* OOB_INST_XCALL            IS_XCALL)
+                                                  (* OOB_INST_CALL             IS_CALL)
+                                                  (* OOB_INST_CREATE           IS_CREATE)
+                                                  (* OOB_INST_SSTORE           IS_SSTORE)
+                                                  (* OOB_INST_DEPLOYMENT       IS_DEPLOYMENT)))
+(defun (wght-sum-prc-common)                (+    (* OOB_INST_ECRECOVER        IS_ECRECOVER)
+                                                  (* OOB_INST_SHA2             IS_SHA2)
+                                                  (* OOB_INST_RIPEMD           IS_RIPEMD)
+                                                  (* OOB_INST_IDENTITY         IS_IDENTITY)
+                                                  (* OOB_INST_ECADD            IS_ECADD)
+                                                  (* OOB_INST_ECMUL            IS_ECMUL)
+                                                  (* OOB_INST_ECPAIRING        IS_ECPAIRING)))
+(defun (wght-sum-prc-blake)                 (+    (* OOB_INST_BLAKE_CDS        IS_BLAKE2F_CDS)
+                                                  (* OOB_INST_BLAKE_PARAMS     IS_BLAKE2F_PARAMS)))
+(defun (wght-sum-prc-modexp)                (+    (* OOB_INST_MODEXP_CDS       IS_MODEXP_CDS)
+                                                  (* OOB_INST_MODEXP_XBS       IS_MODEXP_XBS)
+                                                  (* OOB_INST_MODEXP_LEAD      IS_MODEXP_LEAD)
+                                                  (* OOB_INST_MODEXP_PRICING   IS_MODEXP_PRICING)
+                                                  (* OOB_INST_MODEXP_EXTRACT   IS_MODEXP_EXTRACT)))
+(defun (wght-sum-prc)                       (+    (wght-sum-prc-common)
+                                                  (wght-sum-prc-blake)
+                                                  (wght-sum-prc-modexp)))
+(defun (wght-sum)                           (+    (wght-sum-inst)
+                                                  (wght-sum-prc)))
+(defun (maxct-sum-inst)                     (+    (* CT_MAX_JUMP               IS_JUMP)
+                                                  (* CT_MAX_JUMPI              IS_JUMPI)
+                                                  (* CT_MAX_RDC                IS_RDC)
+                                                  (* CT_MAX_CDL                IS_CDL)
+                                                  (* CT_MAX_XCALL              IS_XCALL)
+                                                  (* CT_MAX_CALL               IS_CALL)
+                                                  (* CT_MAX_CREATE             IS_CREATE)
+                                                  (* CT_MAX_SSTORE             IS_SSTORE)
+                                                  (* CT_MAX_DEPLOYMENT         IS_DEPLOYMENT)))
+(defun (maxct-sum-prc-common)               (+    (* CT_MAX_ECRECOVER          IS_ECRECOVER)
+                                                  (* CT_MAX_SHA2               IS_SHA2)
+                                                  (* CT_MAX_RIPEMD             IS_RIPEMD)
+                                                  (* CT_MAX_IDENTITY           IS_IDENTITY)
+                                                  (* CT_MAX_ECADD              IS_ECADD)
+                                                  (* CT_MAX_ECMUL              IS_ECMUL)
+                                                  (* CT_MAX_ECPAIRING          IS_ECPAIRING)))
 (defun (maxct-sum-prc-blake)  (+ (* CT_MAX_BLAKE2F_CDS IS_BLAKE2F_CDS) (* CT_MAX_BLAKE2F_PARAMS IS_BLAKE2F_PARAMS)))
 (defun (maxct-sum-prc-modexp)  (+ (* CT_MAX_MODEXP_CDS IS_MODEXP_CDS)
-     (* CT_MAX_MODEXP_XBS IS_MODEXP_XBS)
-     (* CT_MAX_MODEXP_LEAD IS_MODEXP_LEAD)
-     (* CT_MAX_MODEXP_PRICING IS_MODEXP_PRICING)
-     (* CT_MAX_MODEXP_EXTRACT IS_MODEXP_EXTRACT)))
+                                  (* CT_MAX_MODEXP_XBS IS_MODEXP_XBS)
+                                  (* CT_MAX_MODEXP_LEAD IS_MODEXP_LEAD)
+                                  (* CT_MAX_MODEXP_PRICING IS_MODEXP_PRICING)
+                                  (* CT_MAX_MODEXP_EXTRACT IS_MODEXP_EXTRACT)))
 (defun (maxct-sum-prc)  (+ (maxct-sum-prc-common) (maxct-sum-prc-blake) (maxct-sum-prc-modexp)))
 (defun (maxct-sum)  (+ (maxct-sum-inst) (maxct-sum-prc)))
 (defun (lookup-sum k)  (+ (shift ADD_FLAG k) (shift MOD_FLAG k) (shift WCP_FLAG k)))
 (defun (wght-lookup-sum k)  (+ (* 1 (shift ADD_FLAG k))
-     (* 2 (shift MOD_FLAG k))
-     (* 3 (shift WCP_FLAG k))))
+                               (* 2 (shift MOD_FLAG k))
+                               (* 3 (shift WCP_FLAG k))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;                             ;;
 ;;    2.2 binary constraints   ;;
 ;;                             ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defconstraint binary-constraints ()
   (begin (is-binary ADD_FLAG)
          (is-binary MOD_FLAG)
@@ -112,6 +138,7 @@
 ;;    2.3 instruction decoding   ;;
 ;;                               ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defconstraint flag-sum-vanishes ()
   (if-zero STAMP
            (vanishes! (flag-sum))))
@@ -140,6 +167,7 @@
 ;;    2.5 heartbeat    ;;
 ;;                     ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defconstraint first-row (:domain {0})
   (vanishes! STAMP))
 
@@ -171,6 +199,7 @@
 ;;    2.6 Constraint systems   ;;
 ;;    for populating lookups   ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defun (call-to-ADD k arg_1_hi arg_1_lo arg_2_hi arg_2_lo)
   (begin (eq! (wght-lookup-sum k) 1)
          (eq! (shift OUTGOING_INST k) EVM_INST_ADD)
@@ -243,6 +272,7 @@
 ;;    3.3 For JUMP       ;;
 ;;                       ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defun (jump-hypothesis)
   IS_JUMP)
 
@@ -279,6 +309,7 @@
 ;;    3.4 For JUMPI      ;;
 ;;                       ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defun (jumpi-hypothesis)
   IS_JUMPI)
 
@@ -427,6 +458,7 @@
 ;; 3.7 For               ;;
 ;; SSTORE                ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defun (sstore-hypothesis)
   IS_SSTORE)
 
@@ -560,6 +592,7 @@
 ;; 3.11 For              ;;
 ;; CREATE's              ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defun (create-hypothesis)
   IS_CREATE)
 
@@ -631,6 +664,7 @@
 ;;   5 Populating common         ;;
 ;;   precompiles                 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;                       ;;
 ;; 5.1 Common            ;;
@@ -650,19 +684,19 @@
 (defun (prc---empty-call-data)   [DATA 7])
 (defun (prc---r@c-nonzero)       [DATA 8])
 (defun (prc---cds-is-zero)       OUTGOING_RES_LO)
-(defun (prc----is-zero)          (next OUTGOING_RES_LO))
+(defun (prc---r@c-is-zero)          (next OUTGOING_RES_LO))
 
 (defconstraint prc---check-cds-is-zero (:guard (* (standing-hypothesis) (prc-hypothesis) (prc-common-hypothesis)))
   (call-to-ISZERO 0 0 (prc---cds)))
 
-(defconstraint prc---check--is-zero (:guard (* (standing-hypothesis) (prc-hypothesis) (prc-common-hypothesis)))
-  (call-to-ISZERO 1 0 (prc---)))
+(defconstraint prc---check-r@c-is-zero (:guard (* (standing-hypothesis) (prc-hypothesis) (prc-common-hypothesis)))
+  (call-to-ISZERO 1 0 (prc---r@c)))
 
 (defconstraint prc---justify-hub-predictions (:guard (* (standing-hypothesis) (prc-hypothesis) (prc-common-hypothesis)))
   (begin (eq! (prc---extract-call-data)
               (* (prc---hub-success) (- 1 (prc---cds-is-zero))))
          (eq! (prc---empty-call-data) (* (prc---hub-success) (prc---cds-is-zero)))
-         (eq! (prc----nonzero) (- 1 (prc----is-zero)))))
+         (eq! (prc---r@c-nonzero) (- 1 (prc---r@c-is-zero)))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;                       ;;
@@ -754,6 +788,7 @@
 ;;   6 Populating MODEXP   ;;
 ;;   precompiles           ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;                         ;;
 ;;   6.1 For MODEXP - cds  ;;
@@ -891,7 +926,7 @@
                                                                200))
 
 (defconstraint prc-modexp-pricing---check--is-zero (:guard (* (standing-hypothesis) (prc-hypothesis) (prc-modexp-pricing-hypothesis)))
-  (call-to-ISZERO 0 0 (prc---)))
+  (call-to-ISZERO 0 0 (prc---r@c)))
 
 (defconstraint prc-modexp-pricing---check-exponent-log-is-zero (:guard (* (standing-hypothesis) (prc-hypothesis) (prc-modexp-pricing-hypothesis)))
   (call-to-ISZERO 1 0 (prc-modexp-pricing---exponent-log)))
@@ -918,13 +953,14 @@
          (if-zero (prc---ram-success)
                   (vanishes! (prc---return-gas))
                   (eq! (prc---return-gas) (- (prc---call-gas) (prc-modexp-pricing---precompile-cost))))
-         (eq! (prc----nonzero) (- 1 OUTGOING_RES_LO))))
+         (eq! (prc---r@c-nonzero) (- 1 OUTGOING_RES_LO))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;                         ;;
 ;;   6.5 For MODEXP        ;;
 ;;   - extract             ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defun (prc-modexp-extract-hypothesis)                              IS_MODEXP_EXTRACT)
 (defun (prc-modexp-extract---bbs)                                   [DATA 3])
 (defun (prc-modexp-extract---ebs)                                   [DATA 4])
@@ -960,30 +996,33 @@
 ;;   7 Populating BLAKE2F  ;;
 ;;   precompiles           ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;                       ;;
 ;; 7.1 For BLAKE2F_cds   ;;
 ;;                       ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defun (prc-blake-cds-hypothesis)   IS_BLAKE2F_CDS)
 (defun (prc-blake-cds---valid-cds)  OUTGOING_RES_LO)
-(defun (prc-blake-cds----is-zero)   (next OUTGOING_RES_LO))
+(defun (prc-blake-cds---r@c-is-zero)   (next OUTGOING_RES_LO))
 
 (defconstraint prc-blake-cds---compare-cds-and-213 (:guard (* (standing-hypothesis) (prc-hypothesis) (prc-blake-cds-hypothesis)))
   (call-to-EQ 0 0 (prc---cds) 0 213))
 
 (defconstraint prc-blake-cds---check--is-zero (:guard (* (standing-hypothesis) (prc-hypothesis) (prc-blake-cds-hypothesis)))
-  (call-to-ISZERO 1 0 (prc---)))
+  (call-to-ISZERO 1 0 (prc---r@c)))
 
 (defconstraint blake2f-a---justify-hub-predictions (:guard (* (standing-hypothesis) (prc-hypothesis) (prc-blake-cds-hypothesis)))
   (begin (eq! (prc---hub-success) (prc-blake-cds---valid-cds))
-         (eq! (prc----nonzero) (- 1 (prc-blake-cds----is-zero)))))
+         (eq! (prc---r@c-nonzero) (- 1 (prc-blake-cds---r@c-is-zero)))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;::;;
 ;;                         ;;
 ;; 7.2 For BLAKE2F_params  ;;
 ;;                         ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defun (prc-blake-params-hypothesis)          IS_BLAKE2F_PARAMS)
 (defun (prc-blake-params---blake-r)           [DATA 6])
 (defun (prc-blake-params---blake-f)           [DATA 7])
