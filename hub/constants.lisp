a97f26a2b00c0fd2b59e72dadaa40f1b2d671f6f
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2024-09-07 21:36:32 +0200

    fix: update to constants

--- hub/constants.lisp
+++ hub/constants.lisp
@@ -2,7 +2,7 @@
 
 (defconst
   MULTIPLIER___STACK_HEIGHT                8      ;; for height increments
-  MULTIPLIER___DOM_SUB_STAMPS             16      ;; for dom/sub stamp business
-  DOM_SUB_STAMP_OFFSET___REVERT            8
-  DOM_SUB_STAMP_OFFSET___SELFDESTRUCT     12
+  MULTIPLIER___DOM_SUB_STAMPS              8      ;; for dom/sub stamp business
+  DOM_SUB_STAMP_OFFSET___REVERT            6
+  DOM_SUB_STAMP_OFFSET___SELFDESTRUCT      7
   )
