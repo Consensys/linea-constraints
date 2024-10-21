f9774e802deaf12e6715ff092966d4edb64440a1
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2024-10-21 10:56:31 +0200

    fix: added "account-fresh-new-deployment-number-and-status" constraint system

--- hub/constraints/account-rows/specialized.lisp
+++ hub/constraints/account-rows/specialized.lisp
@@ -117,15 +117,18 @@
                                       (account-same-balance kappa)))
 
 ;; never used in practice
-(defun (account-deletion kappa) (begin (vanishes!             (shift account/NONCE_NEW             kappa))
-                                       (vanishes!             (shift account/BALANCE_NEW           kappa))
-                                       (vanishes!             (shift account/CODE_SIZE_NEW         kappa))
-                                       (vanishes!             (shift account/HAS_CODE_NEW          kappa))
-                                       (debug      (eq!       (shift account/CODE_HASH_HI_NEW      kappa)   EMPTY_KECCAK_HI))
-                                       (debug      (eq!       (shift account/CODE_HASH_LO_NEW      kappa)   EMPTY_KECCAK_LO))
-                                       (account-increment-deployment-number                        kappa)
-                                       (vanishes!             (shift account/DEPLOYMENT_STATUS_NEW kappa))
-                                       (debug      (vanishes! (shift account/DEPLOYMENT_STATUS     kappa)))))
+(defun (account-deletion kappa) (begin (vanishes!        (shift account/NONCE_NEW           kappa))
+                                       (vanishes!        (shift account/BALANCE_NEW         kappa))
+                                       (vanishes!        (shift account/CODE_SIZE_NEW       kappa))
+                                       (vanishes!        (shift account/HAS_CODE_NEW        kappa))
+                                       (debug     (eq!   (shift account/CODE_HASH_HI_NEW    kappa)   EMPTY_KECCAK_HI))
+                                       (debug     (eq!   (shift account/CODE_HASH_LO_NEW    kappa)   EMPTY_KECCAK_LO))
+                                       (account-fresh-new-deployment-number-and-status      kappa)))
+
+(defun (account-fresh-new-deployment-number-and-status    kappa) (begin  (account-increment-deployment-number                        kappa)
+                                                                         (vanishes!             (shift account/DEPLOYMENT_STATUS_NEW kappa))
+                                                                         (debug      (vanishes! (shift account/DEPLOYMENT_STATUS     kappa)))))
+
 
 (defun (account-same-address-as  undoAt
                                  doneAt)
