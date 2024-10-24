267db58eae9023bbce329e65c9f7e3d9db2a71b8
F Bojarski <ceciestunepoubelle@protonmail.ch>
2024-10-24 08:52:18 +0200

    missing lookup from MMIO
    Signed-off-by: F Bojarski <ceciestunepoubelle@protonmail.ch>

--- mmio/lookups/mmio_into_ecdata.lisp
+++ mmio/lookups/mmio_into_ecdata.lisp
@@ -1,3 +1,24 @@
-;;TODO
+(deflookup 
+  mmio-into-ecdata
+  ;reference columns
+  (
+    ecdata.ID
+    ecdata.PHASE
+    ecdata.INDEX
+    ecdata.LIMB
+    ;; ecdata.nBYTES
+    ecdata.TOTAL_SIZE
+    ecdata.SUCCESS_BIT
+  )
+  ;source columns
+  (
+    (* mmio.EXO_IS_ECDATA mmio.EXO_ID)
+    (* mmio.EXO_IS_ECDATA mmio.PHASE)
+    (* mmio.EXO_IS_ECDATA mmio.INDEX_X)
+    (* mmio.EXO_IS_ECDATA mmio.LIMB)
+    ;; (* mmio.EXO_IS_ECDATA mmio.SIZE)
+    (* mmio.EXO_IS_ECDATA mmio.TOTAL_SIZE)
+    (* mmio.EXO_IS_ECDATA mmio.SUCCESS_BIT)
+  ))
 
 
