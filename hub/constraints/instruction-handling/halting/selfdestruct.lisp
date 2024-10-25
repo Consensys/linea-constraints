ca36bab3ea61f00673481617914c2e82f87bf03f
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2024-10-25 19:28:37 +0200

    fix: back propagating SELFDESTRUCT scenario

--- hub/constraints/instruction-handling/halting/selfdestruct.lisp
+++ hub/constraints/instruction-handling/halting/selfdestruct.lisp
@@ -16,15 +16,21 @@
 ;;                           ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; TODO: uncomment
 (defconstraint    setting-selfdestruct-scenario-sum ()
                   (if-not-zero PEEK_AT_STACK
                                (if-not-zero stack/HALT_FLAG
                                             (if-not-zero (halting-instruction---is-SELFDESTRUCT)
                                                          (if-not-zero (- 1 stack/SUX stack/SOX)
                                                                       (begin
-                                                                        (will-eq! PEEK_AT_SCENARIO                        1)
+                                                                        (will-eq! PEEK_AT_SCENARIO                            1)
                                                                         (will-eq! (scenario-shorthand---SELFDESTRUCT---sum)   1)))))))
+
+(defconstraint    selfdestruct-scenario-back-propagation (:guard (selfdestruct-scenario-precondition))
+                  (begin
+                    (eq!    (shift  PEEK_AT_STACK                            ROFF_SELFDESTRUCT___STACK_ROW)    1)
+                    (eq!    (shift  stack/HALT_FLAG                          ROFF_SELFDESTRUCT___STACK_ROW)    1)
+                    (eq!    (shift  (halting-instruction---is-SELFDESTRUCT)  ROFF_SELFDESTRUCT___STACK_ROW)    1)
+                    (eq!    (shift  (- 1 stack/SUX stack/SOX)                ROFF_SELFDESTRUCT___STACK_ROW)    1)))
 
 ;; ""
 
