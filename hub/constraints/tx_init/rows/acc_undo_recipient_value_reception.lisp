d9fa89a1da57c174846e984e4e6fc495effa03c7
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2024-12-01 20:19:20 +0700

    feat: update of initialization phase

---
+++ hub/constraints/tx_init/rows/acc_undo_recipient_value_reception.lisp
@@ -0,0 +1,24 @@
+(module hub)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                                                    ;;
+;;   X     TX_INIT phase                              ;;
+;;   X.Y   Common constraints                         ;;
+;;   X.Y.Z Undoing recipient account value reception   ;;
+;;                                                    ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defconstraint   tx-init---account-row---recipient-value-reception
+                 (:guard (tx-init---standard-precondition))
+                 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+                 (begin
+                   (account-same-address-as                       tx-init---row-offset---ACC---recipient-value-reception---undoing   tx-init---row-offset---ACC---recipient-value-reception)
+                   (account-undo-balance-update                   tx-init---row-offset---ACC---recipient-value-reception---undoing   tx-init---row-offset---ACC---recipient-value-reception)
+                   (account-undo-nonce-update                     tx-init---row-offset---ACC---recipient-value-reception---undoing   tx-init---row-offset---ACC---recipient-value-reception)
+                   (account-undo-code-update                      tx-init---row-offset---ACC---recipient-value-reception---undoing   tx-init---row-offset---ACC---recipient-value-reception)
+                   (account-same-warmth                           tx-init---row-offset---ACC---recipient-value-reception---undoing)
+                   (account-undo-deployment-status-update         tx-init---row-offset---ACC---recipient-value-reception---undoing   tx-init---row-offset---ACC---recipient-value-reception)
+                   (account-same-marked-for-selfdestruct          tx-init---row-offset---ACC---recipient-value-reception---undoing)
+                   (DOM-SUB-stamps---revert-with-child            tx-init---row-offset---ACC---recipient-value-reception---undoing
+                                                                  4
+                                                                  (tx-init---transaction-end-stamp))))
---
+++ hub/constraints/tx_init/rows/acc_undo_sender_value_transfer.lisp
@@ -0,0 +1,27 @@
+(module hub)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                                                 ;;
+;;   X     TX_INIT phase                           ;;
+;;   X.Y   Common constraints                      ;;
+;;   X.Y.Z Undoing sender account value transfer   ;;
+;;                                                 ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defconstraint   tx-init---account-row---sender-value-transfer
+                 (:guard (tx-init---standard-precondition))
+                 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+                 (begin
+                   (account-same-address-as                       tx-init---row-offset---ACC---sender-value-transfer---undoing
+                                                                  tx-init---row-offset---ACC---sender-value-transfer)
+                   (account-undo-balance-update                   tx-init---row-offset---ACC---sender-value-transfer---undoing
+                                                                  tx-init---row-offset---ACC---sender-value-transfer)
+                   (account-same-nonce                            tx-init---row-offset---ACC---sender-value-transfer---undoing)
+                   (account-same-code                             tx-init---row-offset---ACC---sender-value-transfer---undoing)
+                   (account-same-deployment-number-and-status     tx-init---row-offset---ACC---sender-value-transfer---undoing)
+                   (account-same-warmth                           tx-init---row-offset---ACC---sender-value-transfer---undoing)
+                   (account-same-marked-for-selfdestruct          tx-init---row-offset---ACC---sender-value-transfer---undoing)
+                   (account-isnt-precompile                       tx-init---row-offset---ACC---sender-value-transfer---undoing)
+                   (DOM-SUB-stamps---revert-with-child            tx-init---row-offset---ACC---sender-value-transfer---undoing
+                                                                  3
+                                                                  (tx-init---transaction-end-stamp))))
