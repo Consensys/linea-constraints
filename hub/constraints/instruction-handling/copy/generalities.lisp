03365615d56f4a6f9289d7c9e67826633d3e2e24
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2024-12-16 13:26:19 +0700

    ras: making (exo-sum) definition more straightforward

--- hub/constraints/instruction-handling/copy/generalities.lisp
+++ hub/constraints/instruction-handling/copy/generalities.lisp
@@ -226,4 +226,5 @@
                                                    (* (copy-instruction---is-CODECOPY)          (copy-instruction---bytecode-address-code-size))
                                                    (* (copy-instruction---is-EXTCODECOPY)       (copy-instruction---foreign-address-code-size) (copy-instruction---foreign-address-has-code))))
 
-(defun (copy-instruction---exo-sum)           (*   (+ (copy-instruction---is-CODECOPY) (copy-instruction---is-EXTCODECOPY)) EXO_SUM_WEIGHT_ROM))
+(defun (copy-instruction---exo-sum)           (+   (* (copy-instruction---is-CODECOPY)          EXO_SUM_WEIGHT_ROM)
+                                                   (* (copy-instruction---is-EXTCODECOPY)       EXO_SUM_WEIGHT_ROM)))
