155416f62c883d98f25e2c040965351293161076
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2024-12-21 15:03:28 +0100

    fix: typo (sender-pay-for-gas  ->  recipient-value-reception)

--- hub/constraints/tx_init/rows/first_execution_row_parameters.lisp
+++ hub/constraints/tx_init/rows/first_execution_row_parameters.lisp
@@ -13,10 +13,10 @@
                   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
                   (if-not-zero    (tx-init---transaction-failure-prediction)
                                   (first-row-of-new-context
-                                    tx-init---row-offset---first-execution-phase-row---failure                                        ;; row offset
-                                    0                                                                                                 ;; next caller context number
-                                    (shift   account/CODE_FRAGMENT_INDEX           tx-init---row-offset---ACC---sender-pay-for-gas)   ;; next CFI
-                                    (shift   transaction/GAS_INITIALLY_AVAILABLE   tx-init---row-offset---TXN)                        ;; initially available gas
+                                    tx-init---row-offset---first-execution-phase-row---failure                                             ;; row offset
+                                    0                                                                                                      ;; next caller context number
+                                    (shift   account/CODE_FRAGMENT_INDEX           tx-init---row-offset---ACC---recipient-value-reception) ;; next CFI
+                                    (shift   transaction/GAS_INITIALLY_AVAILABLE   tx-init---row-offset---TXN)                             ;; initially available gas
                                     )))
 
 (defconstraint    tx-init---setting-parameters-on-the-first-row-of-new-context---success
@@ -24,8 +24,8 @@
                   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
                   (if-not-zero    (tx-init---transaction-success-prediction)
                                   (first-row-of-new-context
-                                    tx-init---row-offset---first-execution-phase-row---success                                        ;; row offset
-                                    0                                                                                                 ;; next caller context number
-                                    (shift   account/CODE_FRAGMENT_INDEX           tx-init---row-offset---ACC---sender-pay-for-gas)   ;; next CFI
-                                    (shift   transaction/GAS_INITIALLY_AVAILABLE   tx-init---row-offset---TXN)                        ;; initially available gas
+                                    tx-init---row-offset---first-execution-phase-row---success                                             ;; row offset
+                                    0                                                                                                      ;; next caller context number
+                                    (shift   account/CODE_FRAGMENT_INDEX           tx-init---row-offset---ACC---recipient-value-reception) ;; next CFI
+                                    (shift   transaction/GAS_INITIALLY_AVAILABLE   tx-init---row-offset---TXN)                             ;; initially available gas
                                     )))
