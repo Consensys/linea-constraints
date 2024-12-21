369e81c2c2c57bcbe80cee6318918cf3324acd04
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2024-12-21 15:04:39 +0100

    fix: TX_INIT undoing constraints conditional to transaction failure

--- hub/constraints/tx_init/rows/acc_undo_recipient_value_reception.lisp
+++ hub/constraints/tx_init/rows/acc_undo_recipient_value_reception.lisp
@@ -11,14 +11,15 @@
 (defconstraint   tx-init---account-row---recipient-value-reception---undoing-row
                  (:guard (tx-init---standard-precondition))
                  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-                 (begin
-                   (account-same-address-as                       tx-init---row-offset---ACC---recipient-value-reception---undoing   tx-init---row-offset---ACC---recipient-value-reception)
-                   (account-undo-balance-update                   tx-init---row-offset---ACC---recipient-value-reception---undoing   tx-init---row-offset---ACC---recipient-value-reception)
-                   (account-undo-nonce-update                     tx-init---row-offset---ACC---recipient-value-reception---undoing   tx-init---row-offset---ACC---recipient-value-reception)
-                   (account-undo-code-update                      tx-init---row-offset---ACC---recipient-value-reception---undoing   tx-init---row-offset---ACC---recipient-value-reception)
-                   (account-same-warmth                           tx-init---row-offset---ACC---recipient-value-reception---undoing)
-                   (account-undo-deployment-status-update         tx-init---row-offset---ACC---recipient-value-reception---undoing   tx-init---row-offset---ACC---recipient-value-reception)
-                   (account-same-marked-for-selfdestruct          tx-init---row-offset---ACC---recipient-value-reception---undoing)
-                   (DOM-SUB-stamps---revert-with-child            tx-init---row-offset---ACC---recipient-value-reception---undoing
-                                                                  4
-                                                                  (tx-init---transaction-end-stamp))))
+                 (if-not-zero   (tx-init---transaction-failure-prediction)
+                                (begin
+                                  (account-same-address-as                       tx-init---row-offset---ACC---recipient-value-reception---undoing   tx-init---row-offset---ACC---recipient-value-reception)
+                                  (account-undo-balance-update                   tx-init---row-offset---ACC---recipient-value-reception---undoing   tx-init---row-offset---ACC---recipient-value-reception)
+                                  (account-undo-nonce-update                     tx-init---row-offset---ACC---recipient-value-reception---undoing   tx-init---row-offset---ACC---recipient-value-reception)
+                                  (account-undo-code-update                      tx-init---row-offset---ACC---recipient-value-reception---undoing   tx-init---row-offset---ACC---recipient-value-reception)
+                                  (account-same-warmth                           tx-init---row-offset---ACC---recipient-value-reception---undoing)
+                                  (account-undo-deployment-status-update         tx-init---row-offset---ACC---recipient-value-reception---undoing   tx-init---row-offset---ACC---recipient-value-reception)
+                                  (account-same-marked-for-selfdestruct          tx-init---row-offset---ACC---recipient-value-reception---undoing)
+                                  (DOM-SUB-stamps---revert-with-child            tx-init---row-offset---ACC---recipient-value-reception---undoing
+                                                                                 4
+                                                                                 (tx-init---transaction-end-stamp)))))
