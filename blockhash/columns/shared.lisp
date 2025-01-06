96cbea83581042e0a23c0323987c14b1e07b4bf2
Lorenzo Gentile <lorenzogentile404@gmail.com>
2025-01-06 17:48:51 +0100

    respected naming and folders standard

--- blockhash/columns/shared.lisp
+++
@@ -1,8 +0,0 @@
-(module blockhash)
-
-(defcolumns
-  (IOMF              :binary@prove)
-  (MACRO             :binary@prove)
-  (PRPRC             :binary@prove)
-  (CT                :i8)
-  (CT_MAX            :i8))
