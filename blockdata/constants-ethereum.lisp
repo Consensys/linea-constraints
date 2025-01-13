8b516b20a9bedf6ec530c8ad22017b152363d90d
amkCha <29160563+amkCha@users.noreply.github.com>
2025-01-13 13:34:57 +0100

    feat(blockdata): unique name for gaslimit ethereum and linea

---
+++ blockdata/constants-ethereum.lisp
@@ -0,0 +1,5 @@
+(module blockdata)
+
+(defconst GAS_LIMIT_MINIMUM 5000)
+
+(defconst GAS_LIMIT_MAXIMUM 0xffffffffffffffff)
---
+++ blockdata/constants-linea.lisp
@@ -0,0 +1,5 @@
+(module blockdata)
+
+(defconst GAS_LIMIT_MINIMUM 61000000)
+
+(defconst GAS_LIMIT_MAXIMUM 2000000000)
