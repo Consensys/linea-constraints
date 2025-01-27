47389bbcfb9a8980a9a68dd67bde5985a20e7521
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2025-01-27 20:58:21 +0100

    ras: formatting

--- hub/constraints/consistency/account/constraints.lisp
+++ hub/constraints/consistency/account/constraints.lisp
@@ -170,12 +170,16 @@
 ;;                              ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defconstraint    account-consistency---other---monotony-of-deployment-number (:guard    acp_PEEK_AT_ACCOUNT)
+(defconstraint    account-consistency---other---monotony-of-deployment-number
+                  (:guard    acp_PEEK_AT_ACCOUNT)
+                  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
                   (any!    (eq!   acp_DEPLOYMENT_NUMBER_NEW    acp_DEPLOYMENT_NUMBER)
                            (eq!   acp_DEPLOYMENT_NUMBER_NEW    (+    1    acp_DEPLOYMENT_NUMBER))))
 
 
-(defconstraint    account-consistency---other---vanishing-constraints-upon-trivial-deployments       (:guard    acp_PEEK_AT_ACCOUNT)
+(defconstraint    account-consistency---other---vanishing-constraints-upon-trivial-deployments
+                  (:guard    acp_PEEK_AT_ACCOUNT)
+                  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
                   (if-not-zero    (-    acp_DEPLOYMENT_NUMBER_NEW    acp_DEPLOYMENT_NUMBER)
                                   (if-zero   acp_DEPLOYMENT_NUMBER_NEW
                                              (begin
@@ -186,7 +190,9 @@
                                                (eq!         acp_CODE_HASH_HI_NEW    EMPTY_KECCAK_HI)
                                                (eq!         acp_CODE_HASH_LO_NEW    EMPTY_KECCAK_LO)))))
 
-(defconstraint    account-consistency---other---vanishing-constraints-upon-nontrivial-deployments    (:guard    acp_PEEK_AT_ACCOUNT)
+(defconstraint    account-consistency---other---vanishing-constraints-upon-nontrivial-deployments
+                  (:guard    acp_PEEK_AT_ACCOUNT)
+                  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
                   (if-not-zero    (-    acp_DEPLOYMENT_NUMBER_NEW    acp_DEPLOYMENT_NUMBER)
                                   (if-not-zero   acp_DEPLOYMENT_NUMBER_NEW
                                                  (begin
