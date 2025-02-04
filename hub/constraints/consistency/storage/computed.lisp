ab8f6217f878e2d777d12c6836ecb48c429fad3b
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2025-02-04 22:59:29 +0100

    fix: re-adding scp_ computed columns
    First usage of
    
      (map-if ...)
    
    formerly "dictionary-based-value-setting" or so

--- hub/constraints/consistency/storage/computed.lisp
+++ hub/constraints/consistency/storage/computed.lisp
@@ -107,55 +107,60 @@
 ;;                                         ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; (defcomputed
-;;   (scp_DEPLOYMENT_NUMBER_FIRST_IN_BLOCK)
-;;   (dictionary-based-value-setting
-;;     ;; target perspective and key columns
-;;     (
-;;      ;; perspective column
-;;      scp_PEEK_AT_STORAGE
-;;      ;; target key columns
-;;      scp_ADDRESS_HI
-;;      scp_ADDRESS_LO
-;;      scp_REL_BLK_NUM
-;;      )
-;;     ;; source perspective and key/value columns
-;;     (
-;;      ;; perspective column
-;;      acp_PEEK_AT_ACCOUNT
-;;      ;; source key columns
-;;      acp_ADDRESS_HI
-;;      acp_ADDRESS_LO
-;;      acp_REL_BLK_NUM
-;;      ;; source value column
-;;      acp_DEPLOYMENT_NUMBER_FIRST_IN_BLOCK
-;;      )
-;;     )
-;;
-;; (defcomputed
-;;   (scp_DEPLOYMENT_NUMBER_FINAL_IN_BLOCK)
-;;   (dictionary-based-value-setting
-;;     ;; target perspective and key columns
-;;     (
-;;      ;; perspective column
-;;      scp_PEEK_AT_STORAGE
-;;      ;; target key columns
-;;      scp_ADDRESS_HI
-;;      scp_ADDRESS_LO
-;;      scp_REL_BLK_NUM
-;;      )
-;;     ;; source perspective and key/value columns
-;;     (
-;;      ;; perspective column
-;;      acp_PEEK_AT_ACCOUNT
-;;      ;; source key columns
-;;      acp_ADDRESS_HI
-;;      acp_ADDRESS_LO
-;;      acp_REL_BLK_NUM
-;;      ;; source value column
-;;      acp_DEPLOYMENT_NUMBER_FINAL_IN_BLOCK
-;;      )
-;;     )
+(defcomputed
+  (scp_DEPLOYMENT_NUMBER_FIRST_IN_BLOCK)
+  (map-if
+
+    ;; target perspective and key columns
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    ;; target selector
+    scp_PEEK_AT_STORAGE
+    ;; target key columns
+    scp_ADDRESS_HI
+    scp_ADDRESS_LO
+    scp_REL_BLK_NUM
+
+    ;; source perspective and key/value columns
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    ;; source selector
+    acp_PEEK_AT_ACCOUNT
+    ;; source key columns
+    acp_ADDRESS_HI
+    acp_ADDRESS_LO
+    acp_REL_BLK_NUM
+    ;; source value column
+    acp_DEPLOYMENT_NUMBER_FIRST_IN_BLOCK
+    )
+  )
+
+(defcomputed
+  (scp_DEPLOYMENT_NUMBER_FINAL_IN_BLOCK)
+  (map-if
+    ;; target perspective and key columns
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    ;; perspective column
+    scp_PEEK_AT_STORAGE
+    ;; target key columns
+    scp_ADDRESS_HI
+    scp_ADDRESS_LO
+    scp_REL_BLK_NUM
+
+    ;; source perspective and key/value columns
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    ;; perspective column
+    acp_PEEK_AT_ACCOUNT
+    ;; source key columns
+    acp_ADDRESS_HI
+    acp_ADDRESS_LO
+    acp_REL_BLK_NUM
+    ;; source value column
+    acp_DEPLOYMENT_NUMBER_FINAL_IN_BLOCK
+    )
+  )
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;
