6d4445bfa064fa4b3788eab49413d92154fd5281
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2025-03-22 01:02:50 +0100

    feat: scenario columns + shorthands

--- mxp_v3/columns/scenario/shorthands.lisp
+++ mxp_v3/columns/scenario/shorthands.lisp
@@ -1,2 +1,33 @@
 (module mxp_v3)
 
+
+(defun  (mxp-scenario-shorthand---scenario-sum)
+  (+    (mxp-scenario-shorthand---no-state-update)
+	(mxp-scenario-shorthand---state-update)
+	))
+
+(defun  (mxp-scenario-shorthand---no-state-update)
+  (+    MSIZE
+	TRIVIAL
+	MXPX
+	;; STATE_UPDATE_WORD_PRICING
+	;; STATE_UPDATE_BYTE_PRICING
+	))
+
+(defun  (mxp-scenario-shorthand---state-update)
+  (+    ;; MSIZE
+        ;; TRIVIAL
+        ;; MXPX
+        STATE_UPDATE_WORD_PRICING
+        STATE_UPDATE_BYTE_PRICING
+        ))
+
+(defun  (mxp-scenario-shorthand---not-msize-nor-trivial)
+  (+    (mxp-scenario-shorthand---state-update)
+	MXPX
+	))
+
+(defun  (mxp-scenario-shorthand---not-msize)
+  (+    (mxp-scenario-shorthand---not-msize-nor-trivial)
+	  TRIVIAL
+	  ))
