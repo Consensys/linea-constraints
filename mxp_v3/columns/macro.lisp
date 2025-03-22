9394141497d412a5b4275a964da10952b9e452b0
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2025-03-22 14:20:21 +0100

    feat: make it compile

--- mxp_v3/columns/macro.lisp
+++ mxp_v3/columns/macro.lisp
@@ -1,25 +1,25 @@
 (module mxp_v3)
 
-(defperspective macro
+(defperspective    macro
                 ;; selector
                 MACRO
                 ;; macro-instruction fields
                 (
-                 ( INST            :byte    )
-                 ( DEPLOYING       :boolean )
-                 ( OFFSET_1_HI     :i128    )
-                 ( OFFSET_1_LO     :i128    )
-                 ( SIZE_1_HI       :i128    )
-                 ( SIZE_1_LO       :i128    )
-                 ( OFFSET_2_HI     :i128    )
-                 ( OFFSET_2_LO     :i128    )
-                 ( SIZE_2_HI       :i128    )
-                 ( SIZE_2_LO       :i128    )
-                 ( RES             :i32     )
-                 ( MXPX            :boolean )
-                 ( GAS_MXPX        :i64     )
-                 ( MAY_TRIGGER_MMU :boolean )
-                 ( S1NZNOMXPX      :boolean )
-                 ( S2NZNOMXPX      :boolean )
+                 ( INST            :byte   )
+                 ( DEPLOYING       :binary )
+                 ( OFFSET_1_HI     :i128   )
+                 ( OFFSET_1_LO     :i128   )
+                 ( SIZE_1_HI       :i128   )
+                 ( SIZE_1_LO       :i128   )
+                 ( OFFSET_2_HI     :i128   )
+                 ( OFFSET_2_LO     :i128   )
+                 ( SIZE_2_HI       :i128   )
+                 ( SIZE_2_LO       :i128   )
+                 ( RES             :i32    )
+                 ( MXPX            :binary )
+                 ( GAS_MXPX        :i64    )
+                 ( MAY_TRIGGER_MMU :binary )
+                 ( S1NZNOMXPX      :binary )
+                 ( S2NZNOMXPX      :binary )
                  )
                 )
