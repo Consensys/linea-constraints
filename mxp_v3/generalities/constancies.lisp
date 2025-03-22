6d5f793c8643c17dcfb77ef2d2f2ddc9619df13d
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2025-03-22 01:57:48 +0100

    feat: some generalities/

--- mxp_v3/generalities/constancies.lisp
+++ mxp_v3/generalities/constancies.lisp
@@ -1,2 +1,7 @@
 (module mxp_v3)
 
+(defconstraint    counter-constancies   ()
+		  (begin
+		    (counter-constancy  CT  CT_MAX                     )
+		    (counter-constancy  CT  (mxp-perspective-wght-sum) )
+		    ))
