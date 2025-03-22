785adf287e710337d4b52c9a720279b06c5c417e
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2025-03-22 01:02:37 +0100

    feat: macro columns

--- mxp_v3/columns/macro.lisp
+++ mxp_v3/columns/macro.lisp
@@ -1,2 +1,25 @@
 (module mxp_v3)
 
+(defperspective macro
+                ;; selector
+                MACRO
+                ;; macro-instruction fields
+                (
+                 ( INST            :byte    )
+                 ( DEPLOYING       :boolean )
+                 ( OFFSET_1_HI     :i128    )
+                 ( OFFSET_1_LO     :i128    )
+                 ( SIZE_1_HI       :i128    )
+                 ( SIZE_1_LO       :i128    )
+                 ( OFFSET_2_HI     :i128    )
+                 ( OFFSET_2_LO     :i128    )
+                 ( SIZE_2_HI       :i128    )
+                 ( SIZE_2_LO       :i128    )
+                 ( RES             :i32     )
+                 ( MXPX            :boolean )
+                 ( GAS_MXPX        :i64     )
+                 ( MAY_TRIGGER_MMU :boolean )
+                 ( S1NZNOMXPX      :boolean )
+                 ( S2NZNOMXPX      :boolean )
+                 )
+                )
