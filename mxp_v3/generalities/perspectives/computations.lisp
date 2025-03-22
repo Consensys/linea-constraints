60f5df0bfb7055063444d929976dc3b91987324f
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2025-03-22 14:01:14 +0100

    feat: generalities/perspectives constraints

--- mxp_v3/generalities/perspectives/computations.lisp
+++ mxp_v3/generalities/perspectives/computations.lisp
@@ -1,2 +1,4 @@
 (module mxp_v3)
 
+(defconstraint  generalies---perspectives---computation---lookup-flag-exclusivity   (:guard COMPUTATION)
+		(vanishes!   (*   computation/WCP_FLAG   computation/EUC_FLAG)))
