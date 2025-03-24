354a01853ae19583c39cb214e3feb8e5e637aa44
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2025-03-24 13:57:01 +0100

    ras

--- mxp_v3/columns/decoder.lisp
+++ mxp_v3/columns/decoder.lisp
@@ -7,10 +7,10 @@
 		(
 		 ( INST                 :byte   )
 		 ( IS_MSIZE             :binary )
-		 ( IS_FIXED_SIZE_32     :binary )
-		 ( IS_FIXED_SIZE_1      :binary )
 		 ( IS_RETURN            :binary )
 		 ( IS_MCOPY             :binary )
+		 ( IS_FIXED_SIZE_32     :binary )
+		 ( IS_FIXED_SIZE_1      :binary )
 		 ( IS_SINGLE_MAX_OFFSET :binary )
 		 ( IS_DOUBLE_MAX_OFFSET :binary )
 		 ( IS_WORD_PRICING      :binary )
