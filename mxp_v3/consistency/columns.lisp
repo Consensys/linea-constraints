5ff1078c9e0c24ffca589808b76d606f482a915c
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2025-03-24 13:08:40 +0100

    feat: consistency arguments

--- mxp_v3/consistency/columns.lisp
+++ mxp_v3/consistency/columns.lisp
@@ -1,2 +1,26 @@
 (module mxp_v3)
 
+(defpermutation
+  ;; permuted columns
+  ;; mscp ≡ MXP state consistency permutation
+  (
+   mscp_SCENARIO
+   mscp_CN
+   mscp_MXP_STAMP
+   mscp_C_MEM
+   mscp_C_MEM_NEW
+   mscp_WORDS
+   mscp_WORDS_NEW
+   )
+  ;;
+  ;; inputs
+  (
+   (+ SCENARIO)
+   (+ CN)
+   (+ MXP_STAMP)
+   scenario/C_MEM
+   scenario/C_MEM_NEW
+   scenario/WORDS
+   scenario/WORDS_NEW
+   )
+  )
