86b2a550922aaead0d6ff4f8c983ed90bf385f56
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2025-03-24 13:08:40 +0100

    feat: consistency arguments

---
+++ mxp_v3/consistency/constraints.lisp
@@ -0,0 +1,37 @@
+(module mxp_v3)
+
+;; mscp_SCENARIO
+;; mscp_CN
+;; mscp_STAMP
+;; mscp_C_MEM
+;; mscp_C_MEM_NEW
+;; mscp_WORDS
+;; mscp_WORDS_NEW
+
+(defconstraint  consistency---initialization---first-scenario-row ()
+                (if-zero  (prev  mscp_SCENARIO)
+                          (if-not-zero   mscp_SCENARIO
+                                         (begin
+                                           (vanishes!  mscp_WORDS)
+                                           (vanishes!  mscp_C_MEM)
+                                           ))))
+
+
+(defconstraint  consistency---initialization---first-encounter-with-context ()
+                (if-not-zero  (prev  mscp_SCENARIO)
+                              (if-not-zero   mscp_SCENARIO
+                                             (if-not-zero   (remained-constant!   mscp_CN)
+                                                            (begin
+                                                              (vanishes!  mscp_WORDS)
+                                                              (vanishes!  mscp_C_MEM)
+                                                              )))))
+
+
+(defconstraint  consistency---linking-constraints ()
+                (if-not-zero  (prev  mscp_SCENARIO)
+                              (if-not-zero   mscp_SCENARIO
+                                             (if-zero   (remained-constant!   mscp_CN)
+                                                        (begin
+                                                          (eq!  mscp_WORDS  (prev  mscp_WORDS_NEW))
+                                                          (eq!  mscp_C_MEM  (prev  mscp_C_MEM_NEW))
+                                                          )))))
