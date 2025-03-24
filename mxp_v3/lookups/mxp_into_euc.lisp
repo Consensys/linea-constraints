2b5f9a5742afcd079f36bd545c4da5ac10f3bb34
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2025-03-24 13:57:14 +0100

    feat: MXP into EUC lookup

--- mxp_v3/lookups/mxp_into_euc.lisp
+++ mxp_v3/lookups/mxp_into_euc.lisp
@@ -1,2 +1,23 @@
-(module mxp_v3)
+(defun
+  (mxp-to-euc-selector)
+  (* mxp_v3.COMPUTATION mxp_v3.computation/EUC_FLAG)
+  )
 
+(deflookup
+  mxp-into-euc
+  ;reference columns
+  (
+    euc.DIVIDEND
+    euc.DIVISOR
+    euc.QUOTIENT
+    euc.CEIL
+    euc.DONE
+  )
+  ;source columns
+  (
+    (* mxp_v3.computation/ARG_1_LO (mxp-to-euc-selector))
+    (* mxp_v3.computation/ARG_2_LO (mxp-to-euc-selector))
+    (* mxp_v3.computation/RES_A    (mxp-to-euc-selector))
+    (* mxp_v3.computation/RES_B    (mxp-to-euc-selector))
+    (mxp-to-euc-selector)
+  ))
