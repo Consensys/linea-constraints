af27c27fd63f52b2a39f06585d79ec0ef15d071b
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2025-03-29 13:25:23 +0100

    fix: make compile + noop in MSIZE case is debug

--- mxp_v3/columns/computation/calls.lisp
+++ mxp_v3/columns/computation/calls.lisp
@@ -34,7 +34,7 @@
     (eq!  (shift  computation/ARG_1_LO  relof)  arg_1_lo     )
     (eq!  (shift  computation/ARG_2_HI  relof)  arg_2_hi     )
     (eq!  (shift  computation/ARG_2_LO  relof)  arg_2_lo     )
-    (eq!  (shift  computation/EXO_INST  relof)  EVM_INST_LEQ )))
+    (eq!  (shift  computation/EXO_INST  relof)  WCP_INST_LEQ )))
 
 
 (defun  (wcp-call-to-ISZERO  relof
