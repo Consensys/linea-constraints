963de9241b47f7c94246fdb82efa7f9d91a54a25
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2025-03-29 12:35:36 +0100

    feat: added `(wcp-call-to-LEQ ...)` to MXP module

--- mxp_v3/columns/computation/calls.lisp
+++ mxp_v3/columns/computation/calls.lisp
@@ -23,6 +23,20 @@
     (eq!  (shift  computation/EXO_INST  relof)  EVM_INST_LT )))
 
 
+(defun  (wcp-call-to-LEQ  relof
+			  arg_1_hi
+			  arg_1_lo
+			  arg_2_hi
+			  arg_2_lo)
+  (begin
+    (eq!  (shift  computation/WCP_FLAG  relof)  1            )
+    (eq!  (shift  computation/ARG_1_HI  relof)  arg_1_hi     )
+    (eq!  (shift  computation/ARG_1_LO  relof)  arg_1_lo     )
+    (eq!  (shift  computation/ARG_2_HI  relof)  arg_2_hi     )
+    (eq!  (shift  computation/ARG_2_LO  relof)  arg_2_lo     )
+    (eq!  (shift  computation/EXO_INST  relof)  EVM_INST_LEQ )))
+
+
 (defun  (wcp-call-to-ISZERO  relof
 			     arg_1_hi
 			     arg_1_lo)
