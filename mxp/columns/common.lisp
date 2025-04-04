6d501924d47bcb9444f8a40581e79f8e74536d09
Lorenzo Gentile <lorenzogentile404@gmail.com>
2025-04-05 00:09:00 +0200

    renamed mxp_v3 to mxp and old mxp to mxp_v2 and substituted force-bool with force-bin

---
+++ mxp/columns/common.lisp
@@ -0,0 +1,18 @@
+(module mxp)
+
+(defcolumns
+  ( MXP_STAMP   :i16          )
+  ( CN          :i16          )
+  ( DECODER     :binary@prove )
+  ( MACRO       :binary@prove )
+  ( SCENARIO    :binary@prove )
+  ( COMPUTATION :binary@prove )
+  ( CT          :i4           )
+  ( CT_MAX      :i4           )
+  )
+
+(defalias
+  DECDR  DECODER
+  SCNRI  SCENARIO
+  CMPTN  COMPUTATION
+  )
