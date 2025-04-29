28df989df2470abf1bef6fa58848640c67c09c6e
Olivier Bégassat <38285177+OlivierBBB@users.noreply.github.com>
2025-04-29 16:47:31 +0200

    EIP-3860: Limit and meter initcode (#644)
    Signed-off-by: F Bojarski <ceciestunepoubelle@protonmail.ch>
    Co-authored-by: F Bojarski <ceciestunepoubelle@protonmail.ch>
    Co-authored-by: François Bojarski <54240434+letypequividelespoubelles@users.noreply.github.com>

--- hub/constraints/miscellaneous-rows/oob/opcodes/oob_inst_create.lisp
+++ hub/constraints/miscellaneous-rows/oob/opcodes/oob_inst_create.lisp
@@ -9,7 +9,7 @@
                                         has_code           ;; callee's HAS_CODE
                                         call_stack_depth   ;; current call stack depth
                                         creator_nonce      ;; creator account nonce
-                                        ;; init_code_size     ;; init code size (it's necessarily small at this point, so only low part required)
+                                        init_code_size     ;; init code size (it's necessarily small at this point, so only low part required)
                                         ) (begin
                                         (eq!    (shift     misc/OOB_INST       kappa)   OOB_INST_CREATE  )
                                         (eq!    (shift    (misc_oob_data_1)    kappa)   value_hi         )
@@ -21,6 +21,5 @@
                                         ;; (eq!    (shift    (misc_oob_data_7)    kappa) )    ;; value_is_nonzero
                                         ;; (eq!    (shift    (misc_oob_data_8)    kappa) )    ;; aborting condition
                                         (eq!    (shift    (misc_oob_data_9)    kappa)   creator_nonce    )
-                                        ;; (eq!    (shift    (misc_oob_data_10)   kappa)   init_code_size   ) ;; XXXXXX
+                                        (eq!    (shift    (misc_oob_data_10)   kappa)   init_code_size   )
                                         ))
-
