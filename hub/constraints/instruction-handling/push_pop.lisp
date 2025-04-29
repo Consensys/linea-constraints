7d2f2bacaddca13da057fdc78a7af451cd686f03
François Bojarski <54240434+letypequividelespoubelles@users.noreply.github.com>
2025-04-29 15:16:48 +0200

    EIP-3855: PUSH0 (#640)
    Signed-off-by: F Bojarski <ceciestunepoubelle@protonmail.ch>
    Signed-off-by: François Bojarski <54240434+letypequividelespoubelles@users.noreply.github.com>
    Co-authored-by: Olivier Bégassat <38285177+OlivierBBB@users.noreply.github.com>
    Co-authored-by: David Pearce <david.pearce@consensys.net>
    Co-authored-by: DavePearce <dave01001110@gmail.com>

--- hub/constraints/instruction-handling/push_pop.lisp
+++ hub/constraints/instruction-handling/push_pop.lisp
@@ -4,7 +4,8 @@
 (defun    (push-pop-instruction---result-hi)             [ stack/STACK_ITEM_VALUE_HI  4 ])
 (defun    (push-pop-instruction---result-lo)             [ stack/STACK_ITEM_VALUE_LO  4 ])
 (defun    (push-pop-instruction---is-POP)                [ stack/DEC_FLAG             1 ])
-(defun    (push-pop-instruction---is-PUSH)               [ stack/DEC_FLAG             2 ]) ;; ""
+(defun    (push-pop-instruction---is-PUSH)               [ stack/DEC_FLAG             2 ])
+(defun    (push-pop-instruction---is-PUSH-ZERO)          [ stack/DEC_FLAG             3 ]) ;; ""
 
 (defconstraint    push-pop-instruction---setting-the-stack-pattern---POP-case
                   (:guard (push-pop-instruction---standard-hypothesis))
@@ -13,7 +14,7 @@
 
 (defconstraint    push-pop-instruction---setting-the-stack-pattern---PUSH-case
                   (:guard (push-pop-instruction---standard-hypothesis))
-                  (if-not-zero (push-pop-instruction---is-PUSH)
+                  (if-not-zero (+ (push-pop-instruction---is-PUSH) (push-pop-instruction---is-PUSH-ZERO) )
                                (stack-pattern-0-1)))
 
 (defconstraint    push-pop-instruction---setting-NSR
@@ -36,6 +37,12 @@
                                (begin (eq! (push-pop-instruction---result-hi) stack/PUSH_VALUE_HI)
                                       (eq! (push-pop-instruction---result-lo) stack/PUSH_VALUE_LO))))
 
+(defconstraint    push-pop-instruction---setting-stack-values---PUSH0-case
+                  (:guard (push-pop-instruction---standard-hypothesis))
+                  (if-not-zero (push-pop-instruction---is-PUSH-ZERO)
+                               (begin (vanishes! (push-pop-instruction---result-hi))
+                                      (vanishes! (push-pop-instruction---result-lo)))))
+
 (defconstraint    push-pop-instruction---setting-PC_NEW---POP-case
                   (:guard (push-pop-instruction---standard-hypothesis))
                   (if-not-zero (push-pop-instruction---is-POP)
@@ -43,5 +50,5 @@
 
 (defconstraint    push-pop-instruction---setting-PC_NEW---PUSH-case
                   (:guard (push-pop-instruction---standard-hypothesis))
-                  (if-not-zero (push-pop-instruction---is-PUSH)
-                               (eq! PC_NEW (+ 1 PC 1 (- stack/INSTRUCTION EVM_INST_PUSH1)))))
+                  (if-not-zero (force-bin (+ (push-pop-instruction---is-PUSH) (push-pop-instruction---is-PUSH-ZERO)))
+                               (eq! PC_NEW (+ 1 PC (- stack/INSTRUCTION EVM_INST_PUSH0)))))
