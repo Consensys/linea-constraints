70901249020f1da27c4a77ad9c390fea6a08284a
Lorenzo Gentile <lorenzogentile404@gmail.com>
2025-05-19 21:40:18 +0200

    added constants

---
+++ bls/constants.lisp
@@ -0,0 +1,47 @@
+(module bls)
+
+(defconst
+  POINT_EVALUATION_PRIME_HI                      0x73eda753299d7d483339d80809a1d805        
+  POINT_EVALUATION_PRIME_LO                      0x1b4d2c3f4b6a0c7e8f3f5a0e9d1b2c3f
+  BLS_PRIME_3                                    0x00000000000000000000000000000000
+  BLS_PRIME_2                                    0x1a0111ea397fe69a4b1ba7b6434bacd7
+  BLS_PRIME_1                                    0x64774b84f38512bf6730d2a0f6b0f624
+  BLS_PRIME_0                                    0x1eabfffeb153ffffb9feffffffffaaab
+  INDEX_MAX_POINT_EVALUATION_DATA                11
+  INDEX_MAX_POINT_EVALUATION_RESULT              3
+  INDEX_MAX_G1_ADD_DATA                          15
+  INDEX_MAX_G1_ADD_RESULT                        7
+  INDEX_MAX_G1_MSM_DATA_MIN                      9
+  INDEX_MAX_G1_MSM_RESULT                        7
+  INDEX_MAX_G2_ADD_DATA                          31
+  INDEX_MAX_G2_ADD_RESULT                        15
+  INDEX_MAX_G2_MSM_DATA_MIN                      17
+  INDEX_MAX_G2_MSM_RESULT                        15
+  INDEX_MAX_PAIRING_CHECK_DATA_MIN               23
+  INDEX_MAX_PAIRING_CHECK_RESULT                 1
+  INDEX_MAX_MAP_FP_TO_G1_DATA                    3
+  INDEX_MAX_MAP_FP_TO_G1_RESULT                  7
+  INDEX_MAX_MAP_FP2_TO_G2_DATA                   7
+  INDEX_MAX_MAP_FP2_TO_G2_RESULT                 15
+  TOTAL_SIZE_POINT_EVALUATION_DATA               192
+  TOTAL_SIZE_POINT_EVALUATION_RESULT             64
+  TOTAL_SIZE_G1_ADD_DATA                         256
+  TOTAL_SIZE_G1_ADD_RESULT                       128
+  TOTAL_SIZE_G1_MSM_DATA_MIN                     160
+  TOTAL_SIZE_G1_MSM_RESULT                       128
+  TOTAL_SIZE_G2_ADD_DATA                         512
+  TOTAL_SIZE_G2_ADD_RESULT                       256
+  TOTAL_SIZE_G2_MSM_DATA_MIN                     288
+  TOTAL_SIZE_G2_MSM_RESULT                       256
+  TOTAL_SIZE_PAIRING_CHECK_DATA_MIN              384
+  TOTAL_SIZE_PAIRING_CHECK_RESULT                32
+  TOTAL_SIZE_MAP_FP_TO_G1_DATA                   64
+  TOTAL_SIZE_MAP_FP_TO_G1_RESULT                 128
+  TOTAL_SIZE_MAP_FP2_TO_G2_DATA                  128
+  TOTAL_SIZE_MAP_FP2_TO_G2_RESULT                256
+  CT_MAX_SMALL_POINT                             7
+  CT_MAX_LARGE_POINT                             15
+  CT_MAX_SCALAR                                  1
+)
+
+
