e84bd439b517ba8563c84448c4a1c3cdc5dc142a
amkCha <29160563+amkCha@users.noreply.github.com>
2025-05-20 11:04:38 +0200

    move oob to oobshan

---
+++ oob/oobshan/binarities.lisp
@@ -0,0 +1,13 @@
+(module oobshan)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                             ;;
+;;    2.2 binary constraints   ;;
+;;                             ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defconstraint wcp-add-mod-are-exclusive ()
+  (is-binary (lookup-sum 0)))
+
+;; others are done with binary@prove in columns.lisp
