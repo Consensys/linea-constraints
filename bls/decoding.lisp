daa8482ec5497be5160f86628761ead53045a0c0
Lorenzo Gentile <lorenzogentile404@gmail.com>
2025-06-02 12:14:33 +0200

    make constraints compile again

--- bls/decoding.lisp
+++ bls/decoding.lisp
@@ -7,16 +7,16 @@
     (+ IS_G1_ADD_DATA IS_G1_ADD_RESULT))
 
 (defun (is_g1_msm)
-    (+ IS_G1_MSM_DATA_MIN IS_G1_MSM_RESULT))
+    (+ IS_G1_MSM_DATA IS_G1_MSM_RESULT))
 
 (defun (is_g2_add)
     (+ IS_G2_ADD_DATA IS_G2_ADD_RESULT))
 
 (defun (is_g2_msm)
-    (+ IS_G2_MSM_DATA_MIN IS_G2_MSM_RESULT))
+    (+ IS_G2_MSM_DATA IS_G2_MSM_RESULT))
 
 (defun (is_pairing_check)
-    (+ IS_PAIRING_CHECK_DATA_MIN IS_PAIRING_CHECK_RESULT))
+    (+ IS_PAIRING_CHECK_DATA IS_PAIRING_CHECK_RESULT))
 
 (defun (is_map_fp_to_g1)
     (+ IS_MAP_FP_TO_G1_DATA IS_MAP_FP_TO_G1_RESULT))
