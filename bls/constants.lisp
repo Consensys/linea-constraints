904b6f37c06664e3de5296692499c56079e78286
Lorenzo Gentile <lorenzogentile404@gmail.com>
2025-06-02 13:54:54 +0200

    updated aliases

--- bls/constants.lisp
+++ bls/constants.lisp
@@ -7,6 +7,7 @@
   BLS_PRIME_2                                    0x1a0111ea397fe69a4b1ba7b6434bacd7
   BLS_PRIME_1                                    0x64774b84f38512bf6730d2a0f6b0f624
   BLS_PRIME_0                                    0x1eabfffeb153ffffb9feffffffffaaab
+  
   INDEX_MAX_POINT_EVALUATION_DATA                11
   INDEX_MAX_POINT_EVALUATION_RESULT              3
   INDEX_MAX_G1_ADD_DATA                          15
@@ -23,25 +24,13 @@
   INDEX_MAX_MAP_FP_TO_G1_RESULT                  7
   INDEX_MAX_MAP_FP2_TO_G2_DATA                   7
   INDEX_MAX_MAP_FP2_TO_G2_RESULT                 15
-  TOTAL_SIZE_POINT_EVALUATION_DATA               192
-  TOTAL_SIZE_POINT_EVALUATION_RESULT             64
-  TOTAL_SIZE_G1_ADD_DATA                         256
-  TOTAL_SIZE_G1_ADD_RESULT                       128
-  TOTAL_SIZE_G1_MSM_DATA_MIN                     160
-  TOTAL_SIZE_G1_MSM_RESULT                       128
-  TOTAL_SIZE_G2_ADD_DATA                         512
-  TOTAL_SIZE_G2_ADD_RESULT                       256
-  TOTAL_SIZE_G2_MSM_DATA_MIN                     288
-  TOTAL_SIZE_G2_MSM_RESULT                       256
-  TOTAL_SIZE_PAIRING_CHECK_DATA_MIN              384
-  TOTAL_SIZE_PAIRING_CHECK_RESULT                32
-  TOTAL_SIZE_MAP_FP_TO_G1_DATA                   64
-  TOTAL_SIZE_MAP_FP_TO_G1_RESULT                 128
-  TOTAL_SIZE_MAP_FP2_TO_G2_DATA                  128
-  TOTAL_SIZE_MAP_FP2_TO_G2_RESULT                256
+
+  CT_MAX_POINT_EVALUATION                        11
   CT_MAX_SMALL_POINT                             7
   CT_MAX_LARGE_POINT                             15
   CT_MAX_SCALAR                                  1
+  CT_MAX_FP_TO_G1                                3
+  CT_MAX_MAP_FP2_TO_G2                           7
 )
 
 
