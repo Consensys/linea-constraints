517488fa99efbb046f35b68302e624b99f9ef188
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2025-07-22 23:35:03 +0200

    feat: transaction phase flags

--- hub/cancun/constraints/system/phase_flags/binarities.lisp
+++ hub/cancun/constraints/system/phase_flags/binarities.lisp
@@ -1,2 +1,11 @@
 (module hub)
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                                 ;;
+;;   X.Y Transaction phase flags   ;;
+;;   X.Y.Z Binary constraints      ;;
+;;                                 ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; no constraints required due to binary@prove annotations
