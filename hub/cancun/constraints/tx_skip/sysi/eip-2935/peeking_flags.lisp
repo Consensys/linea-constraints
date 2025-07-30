a15713f973a7600a37175c9ab962968652f2388d
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2025-07-30 11:15:12 +0200

    fix

--- hub/cancun/constraints/tx_skip/sysi/eip-2935/peeking_flags.lisp
+++ hub/cancun/constraints/tx_skip/sysi/eip-2935/peeking_flags.lisp
@@ -15,8 +15,8 @@
 (defconstraint    tx-skip---SYSI-2935---setting-the-peeking-flags
                   (:guard (tx-skip---precondition---SYSI-2935))
                   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-                  (eq!    (+    (shift    PEEK_AT_TRANSACTION    tx-skip---SYSI-2935---row-offset---TXN                                   )
-                                (shift    PEEK_AT_ACCOUNT        tx-skip---SYSI-2935---row-offset---ACC---loading-the-beacon-root-account )
-                                (shift    PEEK_AT_STORAGE        tx-skip---SYSI-2935---row-offset---STO---storing-the-time-stamp          )
-                                (shift    PEEK_AT_CONTEXT        tx-skip---SYSI-2935---row-offset---CON---final-zero-context              ))
+                  (eq!    (+    (shift    PEEK_AT_TRANSACTION    tx-skip---SYSI-2935---row-offset---TXN                                          )
+                                (shift    PEEK_AT_ACCOUNT        tx-skip---SYSI-2935---row-offset---ACC---loading-the-block-hash-history-account )
+                                (shift    PEEK_AT_STORAGE        tx-skip---SYSI-2935---row-offset---STO---storing-the-previous-block-hash        )
+                                (shift    PEEK_AT_CONTEXT        tx-skip---SYSI-2935---row-offset---CON---final-zero-context                     ))
                           tx-skip---SYSI-2935---NSR))                                               
