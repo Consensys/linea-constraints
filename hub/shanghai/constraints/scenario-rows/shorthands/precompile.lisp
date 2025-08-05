c14af67e82bb4fafc19ef4a4569e056f5502af5c
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2025-08-06 00:55:50 +0200

    ras: added some more (force-bin ...)'s for Cancun

--- hub/shanghai/constraints/scenario-rows/shorthands/precompile.lisp
+++ hub/shanghai/constraints/scenario-rows/shorthands/precompile.lisp
@@ -68,82 +68,83 @@
                  ;; scenario/PRC_SUCCESS_CALLER_WONT_REVERT
                  ;; scenario/PRC_FAILURE_KNOWN_TO_HUB
                  ;; scenario/PRC_FAILURE_KNOWN_TO_RAM
-                 ))
+                 )))
 
-  ;;  PRC/weighted_sum
-  (defun (scenario-shorthand---PRC---weighted-address-bit-sum)
-    (+
-      (*  1  scenario/PRC_ECRECOVER  )
-      (*  2  scenario/PRC_SHA2-256   )
-      (*  3  scenario/PRC_RIPEMD-160 )
-      (*  4  scenario/PRC_IDENTITY   )
-      (*  5  scenario/PRC_MODEXP     )
-      (*  6  scenario/PRC_ECADD      )
-      (*  7  scenario/PRC_ECMUL      )
-      (*  8  scenario/PRC_ECPAIRING  )
-      (*  9  scenario/PRC_BLAKE2f    )
-      ;; scenario/PRC_SUCCESS_CALLER_WILL_REVERT
-      ;; scenario/PRC_SUCCESS_CALLER_WONT_REVERT
-      ;; scenario/PRC_FAILURE_KNOWN_TO_HUB
-      ;; scenario/PRC_FAILURE_KNOWN_TO_RAM
-      ))
+;;  PRC/weighted_sum
+(defun (scenario-shorthand---PRC---weighted-address-bit-sum)
+  (+
+    (*  1  scenario/PRC_ECRECOVER  )
+    (*  2  scenario/PRC_SHA2-256   )
+    (*  3  scenario/PRC_RIPEMD-160 )
+    (*  4  scenario/PRC_IDENTITY   )
+    (*  5  scenario/PRC_MODEXP     )
+    (*  6  scenario/PRC_ECADD      )
+    (*  7  scenario/PRC_ECMUL      )
+    (*  8  scenario/PRC_ECPAIRING  )
+    (*  9  scenario/PRC_BLAKE2f    )
+    ;; scenario/PRC_SUCCESS_CALLER_WILL_REVERT
+    ;; scenario/PRC_SUCCESS_CALLER_WONT_REVERT
+    ;; scenario/PRC_FAILURE_KNOWN_TO_HUB
+    ;; scenario/PRC_FAILURE_KNOWN_TO_RAM
+    ))
 
-  ;;  PRC/failure
-  (defun (scenario-shorthand---PRC---failure)
-    (force-bin   (+
-                   ;; scenario/PRC_ECRECOVER
-                   ;; scenario/PRC_SHA2-256
-                   ;; scenario/PRC_RIPEMD-160
-                   ;; scenario/PRC_IDENTITY
-                   ;; scenario/PRC_MODEXP
-                   ;; scenario/PRC_ECADD
-                   ;; scenario/PRC_ECMUL
-                   ;; scenario/PRC_ECPAIRING
-                   ;; scenario/PRC_BLAKE2f
-                   ;; scenario/PRC_SUCCESS_CALLER_WILL_REVERT
-                   ;; scenario/PRC_SUCCESS_CALLER_WONT_REVERT
-                   scenario/PRC_FAILURE_KNOWN_TO_HUB
-                   scenario/PRC_FAILURE_KNOWN_TO_RAM
-                   )))
+;;  PRC/failure
+(defun (scenario-shorthand---PRC---failure)
+  (force-bin   (+
+                 ;; scenario/PRC_ECRECOVER
+                 ;; scenario/PRC_SHA2-256
+                 ;; scenario/PRC_RIPEMD-160
+                 ;; scenario/PRC_IDENTITY
+                 ;; scenario/PRC_MODEXP
+                 ;; scenario/PRC_ECADD
+                 ;; scenario/PRC_ECMUL
+                 ;; scenario/PRC_ECPAIRING
+                 ;; scenario/PRC_BLAKE2f
+                 ;; scenario/PRC_SUCCESS_CALLER_WILL_REVERT
+                 ;; scenario/PRC_SUCCESS_CALLER_WONT_REVERT
+                 scenario/PRC_FAILURE_KNOWN_TO_HUB
+                 scenario/PRC_FAILURE_KNOWN_TO_RAM
+                 )))
 
-  ;;  PRC/success
-  (defun (scenario-shorthand---PRC---success)
-    (force-bin   (+
-                   ;; scenario/PRC_ECRECOVER
-                   ;; scenario/PRC_SHA2-256
-                   ;; scenario/PRC_RIPEMD-160
-                   ;; scenario/PRC_IDENTITY
-                   ;; scenario/PRC_MODEXP
-                   ;; scenario/PRC_ECADD
-                   ;; scenario/PRC_ECMUL
-                   ;; scenario/PRC_ECPAIRING
-                   ;; scenario/PRC_BLAKE2f
-                   scenario/PRC_SUCCESS_CALLER_WILL_REVERT
-                   scenario/PRC_SUCCESS_CALLER_WONT_REVERT
-                   ;; scenario/PRC_FAILURE_KNOWN_TO_HUB
-                   ;; scenario/PRC_FAILURE_KNOWN_TO_RAM
-                   )))
+;;  PRC/success
+(defun (scenario-shorthand---PRC---success)
+  (force-bin   (+
+                 ;; scenario/PRC_ECRECOVER
+                 ;; scenario/PRC_SHA2-256
+                 ;; scenario/PRC_RIPEMD-160
+                 ;; scenario/PRC_IDENTITY
+                 ;; scenario/PRC_MODEXP
+                 ;; scenario/PRC_ECADD
+                 ;; scenario/PRC_ECMUL
+                 ;; scenario/PRC_ECPAIRING
+                 ;; scenario/PRC_BLAKE2f
+                 scenario/PRC_SUCCESS_CALLER_WILL_REVERT
+                 scenario/PRC_SUCCESS_CALLER_WONT_REVERT
+                 ;; scenario/PRC_FAILURE_KNOWN_TO_HUB
+                 ;; scenario/PRC_FAILURE_KNOWN_TO_RAM
+                 )))
 
-  ;;  PRC/scenario_sum
-  (defun (scenario-shorthand---PRC---sum)
-    (force-bin   (+   (scenario-shorthand---PRC---failure)
-                      (scenario-shorthand---PRC---success)
-                      )))
+;;  PRC/scenario_sum
+(defun (scenario-shorthand---PRC---sum)
+  (force-bin   (+   (scenario-shorthand---PRC---failure)
+                    (scenario-shorthand---PRC---success)
+                    )))
 
-  ;; ;;  PRC/
-  ;; (defun (scenario-shorthand---PRC---)
-  ;;   (+
-  ;;     scenario/PRC_ECRECOVER
-  ;;     scenario/PRC_SHA2-256
-  ;;     scenario/PRC_RIPEMD-160
-  ;;     scenario/PRC_IDENTITY
-  ;;     scenario/PRC_MODEXP
-  ;;     scenario/PRC_ECADD
-  ;;     scenario/PRC_ECMUL
-  ;;     scenario/PRC_ECPAIRING
-  ;;     scenario/PRC_BLAKE2f
-  ;;     scenario/PRC_SUCCESS_CALLER_WILL_REVERT
-  ;;     scenario/PRC_SUCCESS_CALLER_WONT_REVERT
-  ;;     scenario/PRC_FAILURE_KNOWN_TO_HUB
-  ;;     scenario/PRC_FAILURE_KNOWN_TO_RAM
-  ;;     ))
+;; ;;  PRC/
+;; (defun (scenario-shorthand---PRC---)
+;;   (+
+;;     scenario/PRC_ECRECOVER
+;;     scenario/PRC_SHA2-256
+;;     scenario/PRC_RIPEMD-160
+;;     scenario/PRC_IDENTITY
+;;     scenario/PRC_MODEXP
+;;     scenario/PRC_ECADD
+;;     scenario/PRC_ECMUL
+;;     scenario/PRC_ECPAIRING
+;;     scenario/PRC_BLAKE2f
+;;     scenario/PRC_SUCCESS_CALLER_WILL_REVERT
+;;     scenario/PRC_SUCCESS_CALLER_WONT_REVERT
+;;     scenario/PRC_FAILURE_KNOWN_TO_HUB
+;;     scenario/PRC_FAILURE_KNOWN_TO_RAM
+;;     ))
+
