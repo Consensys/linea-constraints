58cc9c956aa17d689aa2fdf047f5c942e0348bce
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2025-08-05 20:17:27 +0200

    feat: new scenario/ shorthands
    this implements
    
    - hub/scenario/shorthands/precompile.tex
    
    and specifies
    
    - specifies \scenPrecompileCommonBls
    - updates   \scenPrecompileCommonNotIdentity
    - updates   \scenPrecompileWeightedSum

--- hub/cancun/constraints/scenario-rows/shorthands/precompile.lisp
+++ hub/cancun/constraints/scenario-rows/shorthands/precompile.lisp
@@ -22,6 +22,41 @@
     ;; scenario/PRC_SUCCESS_CALLER_WONT_REVERT
     ;; scenario/PRC_FAILURE_KNOWN_TO_HUB
     ;; scenario/PRC_FAILURE_KNOWN_TO_RAM
+    ;; scenario/PRC_POINT_EVALUATION
+    ;; scenario/PRC_BLS_G1_ADD
+    ;; scenario/PRC_BLS_G1_MSM
+    ;; scenario/PRC_BLS_G2_ADD
+    ;; scenario/PRC_BLS_G2_MSM
+    ;; scenario/PRC_BLS_PAIRING_CHECK
+    ;; scenario/PRC_BLS_MAP_FP_TO_G1
+    ;; scenario/PRC_BLS_MAP_FP2_TO_G2
+    (scenario-shorthand---PRC---common-BLS-address-bit-sum)
+    ))
+
+;;  PRC/sum
+(defun (scenario-shorthand---PRC---common-BLS-address-bit-sum)
+  (+
+    ;; scenario/PRC_ECRECOVER
+    ;; scenario/PRC_SHA2-256
+    ;; scenario/PRC_RIPEMD-160
+    ;; scenario/PRC_IDENTITY
+    ;; scenario/PRC_MODEXP
+    ;; scenario/PRC_ECADD
+    ;; scenario/PRC_ECMUL
+    ;; scenario/PRC_ECPAIRING
+    ;; scenario/PRC_BLAKE2f
+    ;; scenario/PRC_SUCCESS_CALLER_WILL_REVERT
+    ;; scenario/PRC_SUCCESS_CALLER_WONT_REVERT
+    ;; scenario/PRC_FAILURE_KNOWN_TO_HUB
+    ;; scenario/PRC_FAILURE_KNOWN_TO_RAM
+    scenario/PRC_POINT_EVALUATION
+    scenario/PRC_BLS_G1_ADD
+    scenario/PRC_BLS_G1_MSM
+    scenario/PRC_BLS_G2_ADD
+    scenario/PRC_BLS_G2_MSM
+    scenario/PRC_BLS_PAIRING_CHECK
+    scenario/PRC_BLS_MAP_FP_TO_G1
+    scenario/PRC_BLS_MAP_FP2_TO_G2
     ))
 
 (defun (scenario-shorthand---PRC---common-address-bit-sum)
@@ -75,15 +110,23 @@
 ;;  PRC/weighted_sum
 (defun (scenario-shorthand---PRC---weighted-address-bit-sum)
   (+
-    (*  1  scenario/PRC_ECRECOVER  )
-    (*  2  scenario/PRC_SHA2-256   )
-    (*  3  scenario/PRC_RIPEMD-160 )
-    (*  4  scenario/PRC_IDENTITY   )
-    (*  5  scenario/PRC_MODEXP     )
-    (*  6  scenario/PRC_ECADD      )
-    (*  7  scenario/PRC_ECMUL      )
-    (*  8  scenario/PRC_ECPAIRING  )
-    (*  9  scenario/PRC_BLAKE2f    )
+    (*    1   scenario/PRC_ECRECOVER          )
+    (*    2   scenario/PRC_SHA2-256           )
+    (*    3   scenario/PRC_RIPEMD-160         )
+    (*    4   scenario/PRC_IDENTITY           )
+    (*    5   scenario/PRC_MODEXP             )
+    (*    6   scenario/PRC_ECADD              )
+    (*    7   scenario/PRC_ECMUL              )
+    (*    8   scenario/PRC_ECPAIRING          )
+    (*    9   scenario/PRC_BLAKE2f            )
+    (*   10   scenario/PRC_POINT_EVALUATION   )
+    (*   11   scenario/PRC_BLS_G1_ADD         )
+    (*   12   scenario/PRC_BLS_G1_MSM         )
+    (*   13   scenario/PRC_BLS_G2_ADD         )
+    (*   14   scenario/PRC_BLS_G2_MSM         )
+    (*   15   scenario/PRC_BLS_PAIRING_CHECK  )
+    (*   16   scenario/PRC_BLS_MAP_FP_TO_G1   )
+    (*   17   scenario/PRC_BLS_MAP_FP2_TO_G2  )
     ;; scenario/PRC_SUCCESS_CALLER_WILL_REVERT
     ;; scenario/PRC_SUCCESS_CALLER_WONT_REVERT
     ;; scenario/PRC_FAILURE_KNOWN_TO_HUB
