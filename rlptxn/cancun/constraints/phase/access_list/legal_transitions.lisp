68504d7a1ed337b0d42490253369594f47d1ea7d
F Bojarski <ceciestunepoubelle@protonmail.ch>
2025-08-07 16:57:34 +0530

    some fixes in access list
    Signed-off-by: F Bojarski <ceciestunepoubelle@protonmail.ch>

--- rlptxn/cancun/constraints/phase/access_list/legal_transitions.lisp
+++ rlptxn/cancun/constraints/phase/access_list/legal_transitions.lisp
@@ -19,7 +19,7 @@
 (defconstraint setting-flag-after-storage-list-list-rlp ()
     (if-not-zero IS_PREFIX_OF_STORAGE_KEY_LIST
         (begin 
-        (eq! (+ IS_PREFIX_OF_STORAGE_KEY_LIST IS_ACCESS_LIST_STORAGE_KEY IS_PREFIX_OF_ACCESS_LIST_ITEM)       (next IS_ACCESS_LIST))
+        (eq! (+ (next IS_PREFIX_OF_STORAGE_KEY_LIST) (next IS_ACCESS_LIST_STORAGE_KEY) (next IS_PREFIX_OF_ACCESS_LIST_ITEM))       (next IS_ACCESS_LIST))
         (eq! (+ (next IS_ACCESS_LIST_STORAGE_KEY) (next IS_PREFIX_OF_ACCESS_LIST_ITEM))                       (* (next IS_ACCESS_LIST) DONE)))))
 
 (defconstraint setting-flag-after-storage-key ()
--- rlptxn/cancun/constraints/phase/access_list/sub_phases/rlp_storage_keys.lisp
+++ rlptxn/cancun/constraints/phase/access_list/sub_phases/rlp_storage_keys.lisp
@@ -5,10 +5,10 @@
         (begin 
         (rlp-compound-bytes32    0 (rlptxn---storage-hi) (rlptxn---storage-lo))
         (if-not-zero (storage-key-list-countdown)
-            (eq! (next IS_ACCESS_LIST_STORAGE_KEY) 1)
+            (eq! (shift IS_ACCESS_LIST_STORAGE_KEY         3) 1)
             (if-not-zero (AL-item-countdown)
-                 (eq! (next IS_PREFIX_OF_ACCESS_LIST_ITEM) 1)
-                 (eq! PHASE_END 1)))
+                 (eq! (shift IS_PREFIX_OF_ACCESS_LIST_ITEM 3) 1)
+                 (eq! (shift PHASE_END                     2) 1)))
         )))
 
 (defun (rlptxn---storage-hi) cmp/EXO_DATA_1)
