5cbac617aaab91209af93f3820f1199d1cbc8fed
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2025-08-11 18:38:56 +0200

    ras: TO phase

--- rlptxn/cancun/constraints/phase/to.lisp
+++ rlptxn/cancun/constraints/phase/to.lisp
@@ -1,17 +1,28 @@
 (module rlptxn)
 
-(defconstraint phase-to ()
-    (if-not-zero (* IS_TO TXN)
-        (if-zero IS_DEPLOYMENT
-            ;; message call case
-            (begin
-            (rlp-compound-constraint---ADDRESS    1 txn/TO_HI txn/TO_LO)
-            (eq! (shift PHASE_END 3) 1)
-            )
+(defun    (is-first-row-of-TO-phase)    (* IS_TO TXN))
 
-            ;; deployment case
-            (begin
-            (set-limb                1  (*  RLP_PREFIX_INT_SHORT (^ 256 LLARGEMO)) 1)
-            (eq! (shift PHASE_END 1) 1)
-            )
-            )))
+(defconstraint TO-phase-constraints ()
+               (if-not-zero (is-first-row-of-TO-phase)
+                            (if-not-zero IS_DEPLOYMENT
+                                         ;; deployment transaction case
+                                         ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+                                         (begin
+                                           ;; setting CT_MAX
+                                           (debug  (vanishes!  (shift   CT_MAX   1)))
+                                           ;; writing the RLP prefix to the RLP string
+                                           (set-limb   1
+                                                       (*  RLP_PREFIX_INT_SHORT (^ 256 LLARGEMO)) ;; ""
+                                                       1)
+                                           ;; setting PHASE_END
+                                           (eq!   (shift PHASE_END 1)   1))
+                                         ;; message call transaction case
+                                         ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+                                         (begin
+                                           ;; calling the RLP_UTILS instruction
+                                           (rlp-compound-constraint---ADDRESS    1
+                                                                                 txn/TO_HI
+                                                                                 txn/TO_LO)
+                                           ;; setting PHASE_END
+                                           (eq! (shift PHASE_END 3) 1))
+                                         )))
