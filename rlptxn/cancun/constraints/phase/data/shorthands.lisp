3cc55b8d56026876b131c4e404e5b1b8575ef15c
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2025-08-11 19:06:27 +0200

    ras: DATA phase

--- rlptxn/cancun/constraints/phase/data/shorthands.lisp
+++ rlptxn/cancun/constraints/phase/data/shorthands.lisp
@@ -3,5 +3,5 @@
 (defun (zeros-countdown)                        cmp/AUX_1)
 (defun (nonzs-countdown)                        cmp/AUX_2)
 (defun (is-payload-size-analysis-row)           (* (prev TXN)     IS_DATA))
-;;define outside of the module to be accessible by lookup files
+;; defined outside of the module to be accessible in lookups
 ;; (defun (is-limb-content-analysis-row)           (* (prev CMP) CMP IS_DATA))
