5598a26598caf066880f5cd30769e4c0bfdc7a79
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2025-08-12 13:30:10 +0200

    ras: finalization

--- rlptxn/cancun/constraints/phase/access_list/sub_phases/finalization.lisp
+++ rlptxn/cancun/constraints/phase/access_list/sub_phases/finalization.lisp
@@ -1,6 +1,6 @@
 (module rlptxn)
 
-(defconstraint al-finalization ()
+(defconstraint   access-list---finalization-constraints ()
     (if-not-zero (* IS_ACCESS_LIST PHASE_END)
         (begin
         (vanishes! (rlptxn---access-list---access-list-item-countdown))
@@ -9,3 +9,10 @@
         (vanishes! (rlptxn---access-list---AL-RLP-length-countdown))
         (vanishes! (rlptxn---access-list---AL-item-RLP-length-countdown))
         )))
+
+
+(defproperty   access-list---finish-either-on-prefix-or-storage-key-list-or-storage-key
+               (eq!   (+   (is-access-list-prefix)
+                           IS_PREFIX_OF_STORAGE_KEY_LIST
+                           IS_ACCESS_LIST_STORAGE_KEY)
+                      1))
