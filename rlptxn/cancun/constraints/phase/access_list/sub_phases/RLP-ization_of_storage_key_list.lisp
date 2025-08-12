86ec4fb0bc48b2ffb2f4046f060dc88262caca1a
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2025-08-12 13:26:03 +0200

    ras: storage keys

--- rlptxn/cancun/constraints/phase/access_list/sub_phases/RLP-ization_of_storage_key_list.lisp
+++ rlptxn/cancun/constraints/phase/access_list/sub_phases/RLP-ization_of_storage_key_list.lisp
@@ -1,7 +1,8 @@
 (module rlptxn)
 
 
-(defun    (access-list---first-row-of-storage-key-list-processing)    (* IS_PREFIX_OF_STORAGE_KEY_LIST (prev DONE)))
+(defun    (access-list---first-row-of-storage-key-list-processing)    (* IS_PREFIX_OF_STORAGE_KEY_LIST
+                                                                         (prev DONE)))
 
 (defconstraint    access-list---RLP-prefix-for-storage-key-lists---calling-RLP_UTILS
                   (:guard    (access-list---first-row-of-storage-key-list-processing))
@@ -11,7 +12,7 @@
                                                                                0))
 
 (defun (length-of-concatenated-storage-key-RLPs)    (* 33 (rlptxn---access-list---storage-key-list-countdown)))
-(defun (storage-key-list-is-nonempty)               cmp/EXO_DATA_4)
+(defun (storage-key-list-is-nonempty)               (force-bin cmp/EXO_DATA_4))
 (defun (storage-key-list-is-empty)                  (force-bin (- 1 (storage-key-list-is-nonempty)))) ;; ""
 
 (defconstraint    access-list---RLP-prefix-for-storage-key-lists---setting-the-next-step-I
