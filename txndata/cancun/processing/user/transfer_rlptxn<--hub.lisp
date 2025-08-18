45534270dd38acf4dcbd7cce82a15f312c586268
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2025-08-18 23:53:38 +0200

    feat: data transfer RLP <== HUB

--- txndata/cancun/processing/user/transfer_rlptxn<--hub.lisp
+++ txndata/cancun/processing/user/transfer_rlptxn<--hub.lisp
@@ -8,15 +8,14 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
-(defconstraint   USER-transaction---data-transfer---RLP<==HUB---
+(defconstraint   USER-transaction---data-transfer---RLP<==HUB---passing-the-CFI-along
 		 (:guard   (first-row-of-USER-transaction))
 		 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+		 (eq!   (shift   rlp/CFI   ROFF___USER___RLP_ROW)
+			(shift   hub/CFI   ROFF___USER___HUB_ROW)
+			))
 
-(defconstraint   USER-transaction---data-transfer---RLP<==HUB---
-		 (:guard   (first-row-of-USER-transaction))
-		 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defconstraint   USER-transaction---data-transfer---RLP<==HUB---
+(defconstraint   USER-transaction---data-transfer---RLP<==HUB---passing-the-REQUIRES_EVM_EXECUTION-bit-along
 		 (:guard   (first-row-of-USER-transaction))
 		 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
