d265193044c57d9bf62931e6bce0a73f4ce36989
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2025-08-18 23:50:12 +0200

    feat: data transfer HUB <== RLP

---
+++ txndata/cancun/processing/user/transfer_rlptxn<--hub.lisp
@@ -0,0 +1,22 @@
+(module txndata)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                                      ;;
+;;    X. USER transaction processing    ;;
+;;    X.Y Data transfer RLP <-- HUB     ;;
+;;                                      ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(defconstraint   USER-transaction---data-transfer---RLP<==HUB---
+		 (:guard   (first-row-of-USER-transaction))
+		 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defconstraint   USER-transaction---data-transfer---RLP<==HUB---
+		 (:guard   (first-row-of-USER-transaction))
+		 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defconstraint   USER-transaction---data-transfer---RLP<==HUB---
+		 (:guard   (first-row-of-USER-transaction))
+		 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
