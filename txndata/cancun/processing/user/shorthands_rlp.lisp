f79249e6f78638f7b034def0712fb20036f637de
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2025-08-18 23:02:04 +0200

    feat: USER shorthands and ROFF/nROWS constants

---
+++ txndata/cancun/processing/user/shorthands_rlp.lisp
@@ -0,0 +1,32 @@
+(module txndata)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                                      ;;
+;;    X. USER transaction processing    ;;
+;;    X.Y Prelude                       ;;
+;;                                      ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun   (USER-transaction---RLP---tx-type)                           (shift   rlp/TX_TYPE                              ROFF___USER___RLP_ROW))
+(defun   (USER-transaction---RLP---to-address-hi-or-zero)             (shift   rlp/TO_ADDRESS_HI                        ROFF___USER___RLP_ROW))
+(defun   (USER-transaction---RLP---to-address-lo-or-zero)             (shift   rlp/TO_ADDRESS_LO                        ROFF___USER___RLP_ROW))
+(defun   (USER-transaction---RLP---is-deployment)                     (shift   rlp/IS_DEPLOYMENT                        ROFF___USER___RLP_ROW))
+(defun   (USER-transaction---RLP---nonce)                             (shift   rlp/NONCE                                ROFF___USER___RLP_ROW))
+(defun   (USER-transaction---RLP---value)                             (shift   rlp/VALUE                                ROFF___USER___RLP_ROW))
+(defun   (USER-transaction---RLP---number-of-zero-bytes)              (shift   rlp/NUMBER_OF_ZERO_BYTES                 ROFF___USER___RLP_ROW))
+(defun   (USER-transaction---RLP---number-of-nonzero-bytes)           (shift   rlp/NUMBER_OF_NONZERO_BYTES              ROFF___USER___RLP_ROW))
+(defun   (USER-transaction---RLP---gas-limit)                         (shift   rlp/GAS_LIMIT                            ROFF___USER___RLP_ROW))
+(defun   (USER-transaction---RLP---gas-price)                         (shift   rlp/GAS_PRICE                            ROFF___USER___RLP_ROW))
+(defun   (USER-transaction---RLP---max-priority-fee)                  (shift   rlp/MAX_PRIORITY_FEE_PER_GAS             ROFF___USER___RLP_ROW))
+(defun   (USER-transaction---RLP---max-fee)                           (shift   rlp/MAX_FEE_PER_GAS                      ROFF___USER___RLP_ROW))
+(defun   (USER-transaction---RLP---number-of-access-list-keys)        (shift   rlp/NUMBER_OF_ACCESS_LIST_STORAGE_KEYS   ROFF___USER___RLP_ROW))
+(defun   (USER-transaction---RLP---number-of-access-list-addresses)   (shift   rlp/NUMBER_OF_ACCESS_LIST_ADDRESSES      ROFF___USER___RLP_ROW))
+
+(defun   (USER-transaction---RLP---is-message-call)    (-   1    (USER-transaction---RLP---is-deployment)))
+(defun   (USER-transaction---data-size)                (+   (USER-transaction---RLP---number-of-zero-bytes)
+                                                            (USER-transaction---RLP---number-of-nonzero-bytes)))
+(defun   (USER-transaction---weighted-byte-count)      (+   (USER-transaction---RLP---number-of-zero-bytes)
+                                                            (USER-transaction---RLP---number-of-nonzero-bytes)))
+(defun   (USER-transaction---data-cost)                (*   (USER-transaction---weighted-byte-count)))
+(defun   (USER-transaction---data-floor-cost)          (*   (USER-transaction---weighted-byte-count)))
+
