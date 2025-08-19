96e05b51564a4c7fb8e121139240fe7316858c10
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2025-08-19 03:22:46 +0200

    feat: it never stops, does it ?

--- txndata/cancun/processing/user/rows/refunds_effective.lisp
+++ txndata/cancun/processing/user/rows/refunds_effective.lisp
@@ -9,3 +9,17 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
+(defconstraint    USER-transaction-processing---common-computations---computing-effective-refund
+                  (:guard    (first-row-of-USER-transaction))
+                  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+                  (small-call-to-LT    ROFF___USER___CMPTN_ROW___EFFECTIVE_GAS_REFUND_COMPUTATION
+                                       (USER-transaction---HUB---refund-counter-final)
+                                       (USER-transaction---refund-limit)))
+
+(defun    (USER-transaction---accrued-refunds-are-LT-than-refund-limit)    (shift      computation/RES   ROFF___USER___CMPTN_ROW___EFFECTIVE_GAS_REFUND_COMPUTATION))
+(defun    (USER-transaction---leftover-gas-plus-refunds)                   (if-zero   (force-bin   (USER-transaction---accrued-refunds-are-LT-than-refund-limit))
+                                                                                      (+   (USER-transaction---HUB---gas-leftover)    (USER-transaction---refund-limit))
+                                                                                      (+   (USER-transaction---HUB---gas-leftover)    (USER-transaction---HUB---refund-counter-final))
+                                                                                      ))
+(defun    (USER-transaction---consumed-gas-after-refunds)    (-   (USER-transaction---RLP---gas-limit)
+                                                                  (USER-transaction---leftover-gas-plus-refunds)))
