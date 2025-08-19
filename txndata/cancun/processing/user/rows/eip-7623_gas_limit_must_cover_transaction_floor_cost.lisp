670e11ca86fda384d7c937c73ce31c3430d3a5fd
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2025-08-19 02:37:43 +0200

    feat: more computations still ...

--- txndata/cancun/processing/user/rows/eip-7623_gas_limit_must_cover_transaction_floor_cost.lisp
+++ txndata/cancun/processing/user/rows/eip-7623_gas_limit_must_cover_transaction_floor_cost.lisp
@@ -1,29 +1,26 @@
 (module txndata)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;                                                       ;;
-;;    X. USER transaction processing                     ;;
-;;    X.Y Common computations                            ;;
-;;    X.Y.Z Gas limit must cover the upfront gas cost    ;;
-;;                                                       ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                                                             ;;
+;;    X. USER transaction processing                           ;;
+;;    X.Y Common computations                                  ;;
+;;    X.Y.Z Gas limit must cover the transaction floor cost    ;;
+;;                                                             ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
-(defconstraint    USER-transaction---common-computations---gas-limit-must-cover-the-upgront-gas-cost
+(defconstraint    USER-transaction---common-computations---gas-limit-and-transaction-floor-cost-comparison
                   (:guard   (first-row-of-USER-transaction))
                   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-                  (begin
-                    (small-call-to-LEQ    ROFF___USER___CMPTN_ROW___GAS_LIMIT_MUST_COVER_THE_UPFRONT_GAS_COST
-                                          (USER-transaction---upfront-gas-cost)
-                                          (USER-transaction---RLP---gas-limit))
-                    (result-must-be-true    ROFF___USER___CMPTN_ROW___GAS_LIMIT_MUST_COVER_THE_UPFRONT_GAS_COST)
-                    ))
+                  (small-call-to-LEQ    ROFF___USER___CMPTN_ROW___GAS_LIMIT_MUST_COVER_THE_TRANSACTION_FLOOR_COST
+                                        (USER-transaction---data-floor-cost)
+                                        (USER-transaction---RLP---gas-limit))
+                  )
 
-(defun    (USER-transaction---upfront-gas-cost)
-  (+    (USER-transaction---data-cost)
-        (USER-transaction---init-code-cost)
-        GAS_CONST_G_TRANSACTION
-        (*  (USER-transaction---RLP---is-deployment)  GAS_CONST_G_TX_CREATE)
-        (*  (USER-transaction---RLP---number-of-access-list-addresses)   GAS_CONST_G_ACCESS_LIST_ADRESS)
-        (*  (USER-transaction---RLP---number-of-access-list-keys)        GAS_CONST_G_ACCESS_LIST_STORAGE)
-        ))
+;; TODO: this must be uncommented for Prague
+(defconstraint    USER-transaction---common-computations---PRAGUE---gas-limit-must-cover-transaction-floor-cost
+                  (:guard   (first-row-of-USER-transaction))
+                  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+                  (result-must-be-true    ROFF___USER___CMPTN_ROW___GAS_LIMIT_MUST_COVER_THE_TRANSACTION_FLOOR_COST)
+                  )
+
---
+++ txndata/cancun/processing/user/rows/refunds_effective.lisp
@@ -0,0 +1,11 @@
+(module txndata)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                                          ;;
+;;    X. USER transaction processing        ;;
+;;    X.Y Common computations               ;;
+;;    X.Y.Z Effective refund computation    ;;
+;;                                          ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
