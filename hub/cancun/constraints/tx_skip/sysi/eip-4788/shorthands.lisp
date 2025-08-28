108bfaab2abb5166b082e4bcd13a8a43d44ef919
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2025-08-29 00:03:34 +0200

    feat: EIP-4788 transactions

--- hub/cancun/constraints/tx_skip/sysi/eip-4788/shorthands.lisp
+++ hub/cancun/constraints/tx_skip/sysi/eip-4788/shorthands.lisp
@@ -14,19 +14,32 @@
 
 
 (defconst
-  tx-skip---SYSI-4788---row-offset---TXN                                     0
-  tx-skip---SYSI-4788---row-offset---ACC---loading-the-beacon-root-account   1
-  tx-skip---SYSI-4788---row-offset---STO---storing-the-time-stamp            2
-  tx-skip---SYSI-4788---row-offset---STO---storing-the-beacon-root           3
-  tx-skip---SYSI-4788---row-offset---CON---final-zero-context                4
-  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-  tx-skip---SYSI-4788---NSR                                                  5
+  tx-skip---SYSI-4788---row-offset---TXN                                        0
+  tx-skip---SYSI-4788---row-offset---ACC---loading-the-beacon-root-account      1
+  tx-skip---SYSI-4788---row-offset---STO---storing-the-time-stamp               2
+  tx-skip---SYSI-4788---row-offset---STO---storing-the-beacon-root              3
+  tx-skip---SYSI-4788---row-offset---CON---final-zero-context---nontrivial-case 4
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  tx-skip---SYSI-4788---row-offset---CON---final-zero-context---trivial-case    2
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  tx-skip---SYSI-4788---NSR---nontrivial-case                                   5
+  tx-skip---SYSI-4788---NSR---trivial-case                                      3
+  tx-skip---SYSI-4788---NSR---unconditional                                     3
   )
 
-(defun (tx-skip---SYSI-4788---timestamp)            (shift   [ transaction/SYST_TXN_DATA   1 ]   tx-skip---SYSI-4788---row-offset---TXN)) ;; ""
-(defun (tx-skip---SYSI-4788---timestamp-mod-8191)   (shift   [ transaction/SYST_TXN_DATA   2 ]   tx-skip---SYSI-4788---row-offset---TXN)) ;; ""
-(defun (tx-skip---SYSI-4788---beacon-root-hi)       (shift   [ transaction/SYST_TXN_DATA   3 ]   tx-skip---SYSI-4788---row-offset---TXN)) ;; ""
-(defun (tx-skip---SYSI-4788---beacon-root-lo)       (shift   [ transaction/SYST_TXN_DATA   4 ]   tx-skip---SYSI-4788---row-offset---TXN)) ;; ""
+(defun (tx-skip---SYSI-4788---sys-smc-exists)               (shift     account/EXISTS                    tx-skip---SYSI-4788---row-offset---ACC)) ;; ""
+(defun (tx-skip---SYSI-4788---timestamp)                    (shift   [ transaction/SYST_TXN_DATA   1 ]   tx-skip---SYSI-4788---row-offset---TXN)) ;; ""
+(defun (tx-skip---SYSI-4788---timestamp-mod-8191)           (shift   [ transaction/SYST_TXN_DATA   2 ]   tx-skip---SYSI-4788---row-offset---TXN)) ;; ""
+(defun (tx-skip---SYSI-4788---beacon-root-hi)               (shift   [ transaction/SYST_TXN_DATA   3 ]   tx-skip---SYSI-4788---row-offset---TXN)) ;; ""
+(defun (tx-skip---SYSI-4788---beacon-root-lo)               (shift   [ transaction/SYST_TXN_DATA   4 ]   tx-skip---SYSI-4788---row-offset---TXN)) ;; ""
+(defun (tx-skip---SYSI-4788---current-block-is-genesis)     (shift   [ transaction/SYST_TXN_DATA   5 ]   tx-skip---SYSI-4788---row-offset---TXN)) ;; ""
+(defun (tx-skip---SYSI-4788---current-block-isnt-genesis)   (-  1  (tx-skip---SYSI-4788---current-block-is-genesis)))
+
+(defun (tx-skip---SYSI-4788---sys-txn-is-nontrivial)      (*  (tx-skip---SYSI-4788---sys-smc-exists)
+                                                              (tx-skip---SYSI-4788---current-block-isnt-genesis)))
+(defun (tx-skip---SYSI-4788---sys-txn-is-trivial)         (-  1   (tx-skip---SYSI-4788---sys-txn-is-nontrivial)))
+
+
 
 
 (defun    (tx-skip---precondition---SYSI-4788)    (*   (-  TOTL_TXN_NUMBER  (prev  TOTL_TXN_NUMBER))
