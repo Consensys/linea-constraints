a49a412af11249acaa33b514a0ca9d5f2920e624
F Bojarski <ceciestunepoubelle@protonmail.ch>
2025-09-04 09:44:06 +0530

    spec issue: non trivial is hascode, not exist
    Signed-off-by: F Bojarski <ceciestunepoubelle@protonmail.ch>

--- hub/cancun/constraints/tx_skip/sysi/eip-2935/shorthands.lisp
+++ hub/cancun/constraints/tx_skip/sysi/eip-2935/shorthands.lisp
@@ -27,22 +27,22 @@
   )
 
 
-(defun (tx-skip---SYSI-2935---sys-smc-exists)               (shift     account/EXISTS                    tx-skip---SYSI-2935---row-offset---ACC---loading-the-beacon-root-account)) ;; ""
+(defun (tx-skip---SYSI-2935---sys-smc-exists)               (shift   account/HAS_CODE              tx-skip---SYSI-2935---row-offset---ACC---loading-the-beacon-root-account)) ;; ""
 ;; we don't need "prev-block-number" itself
 (defun (tx-skip---SYSI-2935---prev-block-number-mod-8191)   (shift   transaction/SYST_TXN_DATA_2   tx-skip---SYSI-2935---row-offset---TXN)) ;; ""
 (defun (tx-skip---SYSI-2935---prev-block-hash-hi)           (shift   transaction/SYST_TXN_DATA_3   tx-skip---SYSI-2935---row-offset---TXN)) ;; ""
 (defun (tx-skip---SYSI-2935---prev-block-hash-lo)           (shift   transaction/SYST_TXN_DATA_4   tx-skip---SYSI-2935---row-offset---TXN)) ;; ""
 (defun (tx-skip---SYSI-2935---current-block-is-genesis)     (shift   transaction/SYST_TXN_DATA_5   tx-skip---SYSI-2935---row-offset---TXN)) ;; ""
-(defun (tx-skip---SYSI-2935---current-block-isnt-genesis)   (-  1  (tx-skip---SYSI-2935---current-block-is-genesis)))
+(defun (tx-skip---SYSI-2935---current-block-isnt-genesis)   (force-bin (-  1  (tx-skip---SYSI-2935---current-block-is-genesis))))
 
 
-(defun (tx-skip---SYSI-2935---sys-txn-is-nontrivial)      (*  (tx-skip---SYSI-2935---sys-smc-exists)
-                                                              (tx-skip---SYSI-2935---current-block-isnt-genesis)))
-(defun (tx-skip---SYSI-2935---sys-txn-is-trivial)         (-  1   (tx-skip---SYSI-2935---sys-txn-is-nontrivial)))
+(defun (tx-skip---SYSI-2935---sys-txn-is-nontrivial)      (force-bin (*  (tx-skip---SYSI-2935---sys-smc-exists)
+                                                                         (tx-skip---SYSI-2935---current-block-isnt-genesis))))
+(defun (tx-skip---SYSI-2935---sys-txn-is-trivial)         (force-bin (-  1   (tx-skip---SYSI-2935---sys-txn-is-nontrivial))))
 
 
-(defun    (tx-skip---precondition---SYSI-2935)    (*   (-    TOTL_TXN_NUMBER    (prev    TOTL_TXN_NUMBER))
-                                                       SYSI
-                                                       TX_SKIP
-                                                       (shift    transaction/EIP_2935    tx-skip---SYSI-2935---row-offset---TXN)
-                                                       ))
+(defun    (tx-skip---precondition---SYSI-2935)    (force-bin (*   (-    TOTL_TXN_NUMBER    (prev    TOTL_TXN_NUMBER))
+                                                                  SYSI
+                                                                  TX_SKIP
+                                                                  (shift    transaction/EIP_2935    tx-skip---SYSI-2935---row-offset---TXN)
+                                                       )))
--- hub/cancun/constraints/tx_skip/sysi/eip-4788/shorthands.lisp
+++ hub/cancun/constraints/tx_skip/sysi/eip-4788/shorthands.lisp
@@ -27,22 +27,22 @@
   tx-skip---SYSI-4788---NSR---nontrivial-case                                   5
   )
 
-(defun (tx-skip---SYSI-4788---sys-smc-exists)               (shift     account/EXISTS                    tx-skip---SYSI-4788---row-offset---ACC---loading-the-beacon-root-account)) ;; ""
+(defun (tx-skip---SYSI-4788---sys-smc-exists)               (shift   account/HAS_CODE              tx-skip---SYSI-4788---row-offset---ACC---loading-the-beacon-root-account)) ;; ""
 (defun (tx-skip---SYSI-4788---timestamp)                    (shift   transaction/SYST_TXN_DATA_1   tx-skip---SYSI-4788---row-offset---TXN)) ;; ""
 (defun (tx-skip---SYSI-4788---timestamp-mod-8191)           (shift   transaction/SYST_TXN_DATA_2   tx-skip---SYSI-4788---row-offset---TXN)) ;; ""
 (defun (tx-skip---SYSI-4788---beacon-root-hi)               (shift   transaction/SYST_TXN_DATA_3   tx-skip---SYSI-4788---row-offset---TXN)) ;; ""
 (defun (tx-skip---SYSI-4788---beacon-root-lo)               (shift   transaction/SYST_TXN_DATA_4   tx-skip---SYSI-4788---row-offset---TXN)) ;; ""
 (defun (tx-skip---SYSI-4788---current-block-is-genesis)     (shift   transaction/SYST_TXN_DATA_5   tx-skip---SYSI-4788---row-offset---TXN)) ;; ""
-(defun (tx-skip---SYSI-4788---current-block-isnt-genesis)   (-  1  (tx-skip---SYSI-4788---current-block-is-genesis)))
+(defun (tx-skip---SYSI-4788---current-block-isnt-genesis)   (force-bin (-  1  (tx-skip---SYSI-4788---current-block-is-genesis))))
 
 
-(defun (tx-skip---SYSI-4788---sys-txn-is-nontrivial)      (*  (tx-skip---SYSI-4788---sys-smc-exists)
-                                                              (tx-skip---SYSI-4788---current-block-isnt-genesis)))
-(defun (tx-skip---SYSI-4788---sys-txn-is-trivial)         (-  1   (tx-skip---SYSI-4788---sys-txn-is-nontrivial)))
+(defun (tx-skip---SYSI-4788---sys-txn-is-nontrivial)      (force-bin (*  (tx-skip---SYSI-4788---sys-smc-exists)
+                                                                         (tx-skip---SYSI-4788---current-block-isnt-genesis))))
+(defun (tx-skip---SYSI-4788---sys-txn-is-trivial)         (force-bin (-  1   (tx-skip---SYSI-4788---sys-txn-is-nontrivial))))
 
 
-(defun    (tx-skip---precondition---SYSI-4788)    (*   (-  TOTL_TXN_NUMBER  (prev  TOTL_TXN_NUMBER))
-                                                       SYSI
-                                                       TX_SKIP
-                                                       (shift    transaction/EIP_4788    tx-skip---SYSI-4788---row-offset---TXN)
-                                                       ))
+(defun    (tx-skip---precondition---SYSI-4788)    (force-bin (*   (-  TOTL_TXN_NUMBER  (prev  TOTL_TXN_NUMBER))
+                                                                  SYSI
+                                                                  TX_SKIP
+                                                                  (shift    transaction/EIP_4788    tx-skip---SYSI-4788---row-offset---TXN)
+                                                       )))
