624c83ce85b59abc281aaba9fa78f106f081fcd7
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2025-09-08 18:26:31 +0200

    fix: individual SYST_TXN_DATA_k columns + typing

--- txndata/cancun/processing/sysi/eip-2935/shorthands.lisp
+++ txndata/cancun/processing/sysi/eip-2935/shorthands.lisp
@@ -8,13 +8,13 @@
 
 
 
-(defun   (EIP-2935---prev-block-number)                (shift  [ hub/SYST_TXN_DATA 1 ]  ROFF___SYSI___HUB_ROW)) ;; ""
-(defun   (EIP-2935---prev-block-number-mod-8191)       (shift  [ hub/SYST_TXN_DATA 2 ]  ROFF___SYSI___HUB_ROW)) ;; ""
-(defun   (EIP-2935---prev-block-hash-hi)               (shift  [ hub/SYST_TXN_DATA 3 ]  ROFF___SYSI___HUB_ROW)) ;; ""
-(defun   (EIP-2935---prev-block-hash-lo)               (shift  [ hub/SYST_TXN_DATA 4 ]  ROFF___SYSI___HUB_ROW)) ;; ""
-(defun   (EIP-2935---current-block-is-genesis-block)   (shift  [ hub/SYST_TXN_DATA 5 ]  ROFF___SYSI___HUB_ROW)) ;; ""
-(defun   (EIP-2935---block-number)                     (shift    hub/btc_BLOCK_NUMBER   ROFF___SYSI___HUB_ROW)) ;; ""
-(defun   (EIP-2935---timestamp)                        (shift    hub/btc_TIMESTAMP      ROFF___SYSI___HUB_ROW)) ;; ""
+(defun   (EIP-2935---prev-block-number)                (shift    hub/SYST_TXN_DATA_1    ROFF___SYSI___HUB_ROW))
+(defun   (EIP-2935---prev-block-number-mod-8191)       (shift    hub/SYST_TXN_DATA_2    ROFF___SYSI___HUB_ROW))
+(defun   (EIP-2935---prev-block-hash-hi)               (shift    hub/SYST_TXN_DATA_3    ROFF___SYSI___HUB_ROW))
+(defun   (EIP-2935---prev-block-hash-lo)               (shift    hub/SYST_TXN_DATA_4    ROFF___SYSI___HUB_ROW))
+(defun   (EIP-2935---current-block-is-genesis-block)   (shift    hub/SYST_TXN_DATA_5    ROFF___SYSI___HUB_ROW))
+(defun   (EIP-2935---block-number)                     (shift    hub/btc_BLOCK_NUMBER   ROFF___SYSI___HUB_ROW))
+(defun   (EIP-2935---timestamp)                        (shift    hub/btc_TIMESTAMP      ROFF___SYSI___HUB_ROW))
 
 (defun   (first-row-of-EIP-2935-transaction)   (*   (first-row-of-SYSI-transaction)   (shift   hub/EIP_2935   ROFF___SYSI___HUB_ROW)))
 
