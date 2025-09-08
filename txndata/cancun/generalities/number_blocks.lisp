c7532ce65f20e1c5f9d0c9517f0b2b14d27c297e
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2025-09-08 19:13:35 +0200

    fix: typo

--- txndata/cancun/generalities/number_blocks.lisp
+++ txndata/cancun/generalities/number_blocks.lisp
@@ -12,8 +12,8 @@
 (defproperty      block-number-constraints---zero-one-increments                                    (has-0-1-increments    BLK_NUMBER))
 ;; (defconstraint    block-number-constraints---increments                             ()              (will-inc!             BLK_NUMBER    (*    (- 1  SYSI)  (next SYSI))))
 (defcomputedcolumn   (BLK_NUMBER :i16 :fwd)     (+   (prev BLK_NUMBER)
-						     (*    (-  1   SYSI)
-							   (next   SYSI))))
+						     (*    (-  1   (prev SYSI))
+							   SYSI)))
 (defconstraint    block-number-constraints---BLK_NUMBER-is-pegged-to-txn-flag-sum   ()              (if-zero               BLK_NUMBER
 															    (eq!    (txn-flag-sum)    0)  ;; BLK = 0
 															    (eq!    (txn-flag-sum)    1)  ;; BLK ≠ 0
