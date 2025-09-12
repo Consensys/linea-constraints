055ec5fb579b37a90af4a76906805755864760ff
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2025-09-12 13:33:10 +0200

    ras: renaming of row offset + non stack row constants for EIP-2935

--- hub/cancun/constraints/tx_skip/sysi/eip-2935/peeking_flags.lisp
+++ hub/cancun/constraints/tx_skip/sysi/eip-2935/peeking_flags.lisp
@@ -15,8 +15,8 @@
 (defconstraint    tx-skip---SYSI-2935---setting-the-peeking-flags
                   (:guard (tx-skip---precondition---SYSI-2935))
                   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-                  (eq!    (+    (shift    PEEK_AT_TRANSACTION    tx-skip---SYSI-2935---row-offset---TXN                                          )
-                                (shift    PEEK_AT_ACCOUNT        tx-skip---SYSI-2935---row-offset---ACC---loading-the-block-hash-history-account )
-                                (shift    PEEK_AT_STORAGE        tx-skip---SYSI-2935---row-offset---STO---storing-the-previous-block-hash        )
-                                (shift    PEEK_AT_CONTEXT        tx-skip---SYSI-2935---row-offset---CON---final-zero-context                     ))
-                          tx-skip---SYSI-2935---NSR))                                               
+                  (eq!    (+    (shift    PEEK_AT_TRANSACTION    ROFF---tx-skip---SYSI-2935---TXN                                          )
+                                (shift    PEEK_AT_ACCOUNT        ROFF---tx-skip---SYSI-2935---ACC---loading-the-block-hash-history-account )
+                                (shift    PEEK_AT_STORAGE        ROFF---tx-skip---SYSI-2935---STO---storing-the-previous-block-hash        )
+                                (shift    PEEK_AT_CONTEXT        ROFF---tx-skip---SYSI-2935---CON---final-zero-context                     ))
+                          NSR---tx-skip---SYSI-2935))                                               
--- hub/cancun/constraints/tx_skip/sysi/eip-2935/shorthands.lisp
+++ hub/cancun/constraints/tx_skip/sysi/eip-2935/shorthands.lisp
@@ -13,21 +13,21 @@
 
 
 (defconst
-  tx-skip---SYSI-2935---row-offset---TXN                                          0
-  tx-skip---SYSI-2935---row-offset---ACC---loading-the-block-hash-history-account 1
-  tx-skip---SYSI-2935---row-offset---STO---storing-the-previous-block-hash        2
-  tx-skip---SYSI-2935---row-offset---CON---final-zero-context                     3
-  tx-skip---SYSI-2935---NSR                                                       4
+  ROFF---tx-skip---SYSI-2935---TXN                                          0
+  ROFF---tx-skip---SYSI-2935---ACC---loading-the-block-hash-history-account 1
+  ROFF---tx-skip---SYSI-2935---STO---storing-the-previous-block-hash        2
+  ROFF---tx-skip---SYSI-2935---CON---final-zero-context                     3
+  NSR---tx-skip---SYSI-2935                                                 4
   )
 
 
-(defun (tx-skip---SYSI-2935---prev-block-number-mod-8191)   (shift   [ transaction/SYST_TXN_DATA   1 ]   tx-skip---SYSI-2935---row-offset---TXN)) ;; ""
-(defun (tx-skip---SYSI-2935---prev-block-hash-hi)           (shift   [ transaction/SYST_TXN_DATA   2 ]   tx-skip---SYSI-2935---row-offset---TXN)) ;; ""
-(defun (tx-skip---SYSI-2935---prev-block-hash-lo)           (shift   [ transaction/SYST_TXN_DATA   3 ]   tx-skip---SYSI-2935---row-offset---TXN)) ;; ""
+(defun (tx-skip---SYSI-2935---prev-block-number-mod-8191)   (shift   [ transaction/SYST_TXN_DATA   1 ]   ROFF---tx-skip---SYSI-2935---TXN)) ;; ""
+(defun (tx-skip---SYSI-2935---prev-block-hash-hi)           (shift   [ transaction/SYST_TXN_DATA   2 ]   ROFF---tx-skip---SYSI-2935---TXN)) ;; ""
+(defun (tx-skip---SYSI-2935---prev-block-hash-lo)           (shift   [ transaction/SYST_TXN_DATA   3 ]   ROFF---tx-skip---SYSI-2935---TXN)) ;; ""
 
 
 (defun    (tx-skip---precondition---SYSI-2935)    (*   (-    TOTL_TXN_NUMBER    (prev    TOTL_TXN_NUMBER))
                                                        SYSI
                                                        TX_SKIP
-                                                       (shift    transaction/EIP_2935    tx-skip---SYSI-2935---row-offset---TXN)
+                                                       (shift    transaction/EIP_2935    ROFF---tx-skip---SYSI-2935---TXN)
                                                        ))
