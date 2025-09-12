8141d6b266158b3ec2ae0ea8567b65105e3edd17
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2025-09-12 13:34:26 +0200

    ras: renaming of row offset + non stack row constants for EIP-4788

--- hub/cancun/constraints/tx_skip/sysi/eip-4788/peeking_flags.lisp
+++ hub/cancun/constraints/tx_skip/sysi/eip-4788/peeking_flags.lisp
@@ -15,9 +15,9 @@
 (defconstraint    tx-skip---SYSI-4788---setting-the-peeking-flags
                   (:guard (tx-skip---precondition---SYSI-4788))
                   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-                  (eq!    (+    (shift    PEEK_AT_TRANSACTION    tx-skip---SYSI-4788---row-offset---TXN                                    )
-                                (shift    PEEK_AT_ACCOUNT        tx-skip---SYSI-4788---row-offset---ACC---loading-the-beacon-root-account  )
-                                (shift    PEEK_AT_STORAGE        tx-skip---SYSI-4788---row-offset---STO---storing-the-time-stamp           )
-                                (shift    PEEK_AT_STORAGE        tx-skip---SYSI-4788---row-offset---STO---storing-the-beacon-root          )
-                                (shift    PEEK_AT_CONTEXT        tx-skip---SYSI-4788---row-offset---CON---final-zero-context               ))
-                          tx-skip---SYSI-4788---NSR))
+                  (eq!    (+    (shift    PEEK_AT_TRANSACTION    ROFF---tx-skip---SYSI-4788---TXN                                    )
+                                (shift    PEEK_AT_ACCOUNT        ROFF---tx-skip---SYSI-4788---ACC---loading-the-beacon-root-account  )
+                                (shift    PEEK_AT_STORAGE        ROFF---tx-skip---SYSI-4788---STO---storing-the-time-stamp           )
+                                (shift    PEEK_AT_STORAGE        ROFF---tx-skip---SYSI-4788---STO---storing-the-beacon-root          )
+                                (shift    PEEK_AT_CONTEXT        ROFF---tx-skip---SYSI-4788---CON---final-zero-context               ))
+                          NSR---tx-skip---SYSI-4788))
--- hub/cancun/constraints/tx_skip/sysi/eip-4788/shorthands.lisp
+++ hub/cancun/constraints/tx_skip/sysi/eip-4788/shorthands.lisp
@@ -14,23 +14,23 @@
 
 
 (defconst
-  tx-skip---SYSI-4788---row-offset---TXN                                     0
-  tx-skip---SYSI-4788---row-offset---ACC---loading-the-beacon-root-account   1
-  tx-skip---SYSI-4788---row-offset---STO---storing-the-time-stamp            2
-  tx-skip---SYSI-4788---row-offset---STO---storing-the-beacon-root           3
-  tx-skip---SYSI-4788---row-offset---CON---final-zero-context                4
-  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-  tx-skip---SYSI-4788---NSR                                                  5
+  ROFF---tx-skip---SYSI-4788---TXN                                     0
+  ROFF---tx-skip---SYSI-4788---ACC---loading-the-beacon-root-account   1
+  ROFF---tx-skip---SYSI-4788---STO---storing-the-time-stamp            2
+  ROFF---tx-skip---SYSI-4788---STO---storing-the-beacon-root           3
+  ROFF---tx-skip---SYSI-4788---CON---final-zero-context                4
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  NSR---tx-skip---SYSI-4788                                            5
   )
 
-(defun (tx-skip---SYSI-4788---timestamp)            (shift   [ transaction/SYST_TXN_DATA   1 ]   tx-skip---SYSI-4788---row-offset---TXN)) ;; ""
-(defun (tx-skip---SYSI-4788---timestamp-mod-8191)   (shift   [ transaction/SYST_TXN_DATA   2 ]   tx-skip---SYSI-4788---row-offset---TXN)) ;; ""
-(defun (tx-skip---SYSI-4788---beacon-root-hi)       (shift   [ transaction/SYST_TXN_DATA   3 ]   tx-skip---SYSI-4788---row-offset---TXN)) ;; ""
-(defun (tx-skip---SYSI-4788---beacon-root-lo)       (shift   [ transaction/SYST_TXN_DATA   4 ]   tx-skip---SYSI-4788---row-offset---TXN)) ;; ""
+(defun (tx-skip---SYSI-4788---timestamp)            (shift   [ transaction/SYST_TXN_DATA   1 ]   ROFF---tx-skip---SYSI-4788---TXN)) ;; ""
+(defun (tx-skip---SYSI-4788---timestamp-mod-8191)   (shift   [ transaction/SYST_TXN_DATA   2 ]   ROFF---tx-skip---SYSI-4788---TXN)) ;; ""
+(defun (tx-skip---SYSI-4788---beacon-root-hi)       (shift   [ transaction/SYST_TXN_DATA   3 ]   ROFF---tx-skip---SYSI-4788---TXN)) ;; ""
+(defun (tx-skip---SYSI-4788---beacon-root-lo)       (shift   [ transaction/SYST_TXN_DATA   4 ]   ROFF---tx-skip---SYSI-4788---TXN)) ;; ""
 
 
 (defun    (tx-skip---precondition---SYSI-4788)    (*   (-  TOTL_TXN_NUMBER  (prev  TOTL_TXN_NUMBER))
                                                        SYSI
                                                        TX_SKIP
-                                                       (shift    transaction/EIP_4788    tx-skip---SYSI-4788---row-offset---TXN)
+                                                       (shift    transaction/EIP_4788    ROFF---tx-skip---SYSI-4788---TXN)
                                                        ))
