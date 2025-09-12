f6084cd19716da66e61f8705dc99d676f10757bd
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2025-09-12 14:14:05 +0200

    ras: EIP-2935 constants renaming

--- hub/cancun/constraints/tx_skip/sysi/eip-2935/rows/account_load_the_block_hash_smart_contract.lisp
+++ hub/cancun/constraints/tx_skip/sysi/eip-2935/rows/account_load_the_block_hash_smart_contract.lisp
@@ -16,16 +16,16 @@
                   (:guard (tx-skip---precondition---SYSI-2935))
                   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
                   (begin
-                    (account-trim-address   tx-skip---SYSI-2935---row-offset---ACC---loading-the-block-hash-history-account   ;; row offset
+                    (account-trim-address   ROFF---tx-skip---SYSI-2935---ACC---loading-the-block-hash-history-account   ;; row offset
                                             HISTORY_STORAGE_ADDRESS_HI                                                        ;; high part of raw, potentially untrimmed address
                                             HISTORY_STORAGE_ADDRESS_LO)                                                       ;; low  part of raw, potentially untrimmed address
-                    (eq!     (shift account/ADDRESS_HI             tx-skip---SYSI-2935---row-offset---ACC---loading-the-block-hash-history-account)     HISTORY_STORAGE_ADDRESS_HI)
-                    (eq!     (shift account/ADDRESS_LO             tx-skip---SYSI-2935---row-offset---ACC---loading-the-block-hash-history-account)     HISTORY_STORAGE_ADDRESS_LO)
-                    (account-same-balance                          tx-skip---SYSI-2935---row-offset---ACC---loading-the-block-hash-history-account)
-                    (account-same-nonce                            tx-skip---SYSI-2935---row-offset---ACC---loading-the-block-hash-history-account)
-                    (account-same-code                             tx-skip---SYSI-2935---row-offset---ACC---loading-the-block-hash-history-account)
-                    (account-same-deployment-number-and-status     tx-skip---SYSI-2935---row-offset---ACC---loading-the-block-hash-history-account)
-                    (account-same-warmth                           tx-skip---SYSI-2935---row-offset---ACC---loading-the-block-hash-history-account)
-                    (account-same-marked-for-deletion              tx-skip---SYSI-2935---row-offset---ACC---loading-the-block-hash-history-account)
-                    (DOM-SUB-stamps---standard                     tx-skip---SYSI-2935---row-offset---ACC---loading-the-block-hash-history-account
-                                                                   tx-skip---SYSI-2935---row-offset---ACC---loading-the-block-hash-history-account)))
+                    (eq!     (shift account/ADDRESS_HI             ROFF---tx-skip---SYSI-2935---ACC---loading-the-block-hash-history-account)     HISTORY_STORAGE_ADDRESS_HI)
+                    (eq!     (shift account/ADDRESS_LO             ROFF---tx-skip---SYSI-2935---ACC---loading-the-block-hash-history-account)     HISTORY_STORAGE_ADDRESS_LO)
+                    (account-same-balance                          ROFF---tx-skip---SYSI-2935---ACC---loading-the-block-hash-history-account)
+                    (account-same-nonce                            ROFF---tx-skip---SYSI-2935---ACC---loading-the-block-hash-history-account)
+                    (account-same-code                             ROFF---tx-skip---SYSI-2935---ACC---loading-the-block-hash-history-account)
+                    (account-same-deployment-number-and-status     ROFF---tx-skip---SYSI-2935---ACC---loading-the-block-hash-history-account)
+                    (account-same-warmth                           ROFF---tx-skip---SYSI-2935---ACC---loading-the-block-hash-history-account)
+                    (account-same-marked-for-deletion              ROFF---tx-skip---SYSI-2935---ACC---loading-the-block-hash-history-account)
+                    (DOM-SUB-stamps---standard                     ROFF---tx-skip---SYSI-2935---ACC---loading-the-block-hash-history-account
+                                                                   ROFF---tx-skip---SYSI-2935---ACC---loading-the-block-hash-history-account)))
