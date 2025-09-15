072bd5e8e41c603b94168e3404b15c16f7dcba36
Olivier Bégassat <olivier.begassat.cours@gmail.com>
2025-09-16 00:46:23 +0200

    feat: generalities/ct_and_ct_max

---
+++ blockdata/cancun/generalities/ct_and_ct_max.lisp
@@ -0,0 +1,50 @@
+(module blockdata)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                                 ;;
+;;  2.X CT and CT_MAX constraints  ;;
+;;                                 ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(defconstraint   ct-and-ct-max---unconditional-setting-of-CT_MAX ()
+		 (eq! CT_MAX (ct-max-sum))
+		 )
+
+(defconstraint   ct-and-ct-max---CT-vanishes-on-padding-rows-and-on-the-first-non-padding-row ()
+		 (if-zero    IOMF
+			     (begin
+			       (vanishes!         CT)
+			       (vanishes!   (next CT))
+			       )))
+
+(defconstraint   ct-and-ct-max---CT-updates-before-CT_MAX-threshold
+		 (:guard   IOMF)
+		 (if-not-zero   (-  CT_MAX  CT)
+				(will-inc!   CT   1)))
+
+(defproperty     ct-and-ct-max---CT-updates-before-CT_MAX-threshold---sanity-checks
+		 (:guard   IOMF)
+		 (if-not-zero   (-  CT_MAX  CT)
+				(begin
+				  (eq!   (next   (upcoming-legal-phase-transition)   0 ))
+				  (eq!   (next   (upcoming-phase-is-different)       0 ))
+				  (eq!   (next   (upcoming-phase-is-the-same)        1 ))
+				  )))
+
+
+(defconstraint   ct-and-ct-max---CT-updates-at-CT_MAX-threshold
+		 (:guard   IOMF)
+		 (if-zero   (-  CT_MAX  CT)
+			    (vanishes!   (next   CT))))
+
+(defproperty     ct-and-ct-max---CT-updates-at-CT_MAX-threshold---sanity-checks
+		 (:guard   IOMF)
+		 (if-zero   (-  CT_MAX  CT)
+				(begin
+				  (eq!   (next   (upcoming-legal-phase-transition)   1 ))
+				  (eq!   (next   (upcoming-phase-is-different)       1 ))
+				  (eq!   (next   (upcoming-phase-is-the-same)        0 ))
+				  )))
+
