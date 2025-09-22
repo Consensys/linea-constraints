389a33225ae685b7d6d7f9c83de78f1934125f8a
amkCha <29160563+amkCha@users.noreply.github.com>
2025-09-22 12:25:06 +0200

    fix: shanghai traces IS_PREVRANDAO column

---
+++ blockdata/shanghai/lookups/blockdata_into_euc.lisp
@@ -0,0 +1,21 @@
+(defun (blockdata-into-euc-selector) blockdata.EUC_FLAG)
+
+(defclookup 
+  (blockdata-into-euc :unchecked)
+  ;; target columns
+  (
+    euc.IOMF
+    euc.DIVIDEND
+    euc.DIVISOR
+    euc.QUOTIENT
+  )
+  ;; source selector
+  (blockdata-into-euc-selector)
+  ;; source columns
+  (
+    1
+    blockdata.ARG_1_LO
+    blockdata.ARG_2_LO
+    blockdata.RES
+  ))
+
---
+++ blockdata/shanghai/lookups/blockdata_into_wcp.lisp
@@ -0,0 +1,26 @@
+(defun (blockdata-into-wcp-selector)
+  blockdata.WCP_FLAG)
+
+(defclookup 
+  (blockdata-into-wcp :unchecked)
+  ;; target columns
+  (
+    wcp.ARGUMENT_1_HI
+    wcp.ARGUMENT_1_LO
+    wcp.ARGUMENT_2_HI
+    wcp.ARGUMENT_2_LO
+    wcp.RESULT
+    wcp.INST
+  )
+  ;; source selector
+  (blockdata-into-wcp-selector)
+  ;; source columns
+  (
+    blockdata.ARG_1_HI
+    blockdata.ARG_1_LO
+    blockdata.ARG_2_HI
+    blockdata.ARG_2_LO
+    blockdata.RES
+    blockdata.EXO_INST
+  ))
+
