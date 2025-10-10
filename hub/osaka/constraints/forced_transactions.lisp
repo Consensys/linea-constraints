6cebccff8230a8821be99781dbc947094afc89a8
F Bojarski <ceciestunepoubelle@protonmail.ch>
2025-10-10 08:20:17 +0530

    ras
    Signed-off-by: F Bojarski <ceciestunepoubelle@protonmail.ch>

--- hub/osaka/constraints/forced_transactions.lisp
+++ hub/osaka/constraints/forced_transactions.lisp
@@ -7,13 +7,9 @@
     (* USER (+ (prev PROVER_ILLEGAL_TRANSACTION_DETECTED_ACC)
                (illegal-precompiles))))
 
-(defcomputedcolumn (PROVER_ILLEGAL_TRANSACTION_DETECTED :binary :bwd) 
-    (if-not-zero (system-txn-numbers---user-txn-end)
-    ;; finalization constraint
-    (if-not-zero PROVER_ILLEGAL_TRANSACTION_DETECTED_ACC
-        (eq! PROVER_ILLEGAL_TRANSACTION_DETECTED 1)
-        (eq! PROVER_ILLEGAL_TRANSACTION_DETECTED 0))
-    ;; bwd propagation
-    (eq! (* (prev USER) PROVER_ILLEGAL_TRANSACTION_DETECTED)
-         (prev PROVER_ILLEGAL_TRANSACTION_DETECTED))
-    ))
+(defcomputedcolumn (PROVER_ILLEGAL_TRANSACTION_DETECTED :binary :bwd)
+     (if-not-zero (system-txn-numbers---user-txn-end)
+          ;; finalization constraint
+          (~ PROVER_ILLEGAL_TRANSACTION_DETECTED_ACC)
+          ;; bwd propagation
+          (*  USER (next PROVER_ILLEGAL_TRANSACTION_DETECTED))))
