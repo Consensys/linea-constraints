84cb1ab2133a48951e70c74436d9e546aa97a18e
Lorenzo Gentile <lorenzo.gentile@consensys.net>
2025-11-05 10:18:39 +0100

    fixed naming

---
+++ hub/osaka/constraints/instruction-handling/call/precompiles/ec_add_mul_pairing_bls_and_p256_verify/constants.lisp
@@ -0,0 +1,17 @@
+(module hub)
+
+;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;                ;;;;
+;;;;    X.Y CALL    ;;;;
+;;;;                ;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(defconst
+  ;;
+  precompile-processing---ECADD_MUL_PAIRING_BLS_and_P256_VERIFY---misc-row-offset---full-return-data-transfer                   2
+  precompile-processing---ECADD_MUL_PAIRING_BLS_and_P256_VERIFY---misc-row-offset---partial-return-data-copy                    3
+  precompile-processing---ECADD_MUL_PAIRING_BLS_and_P256_VERIFY---context-row-offset---updating-caller-context                  4
+  )
