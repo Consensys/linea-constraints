e23ed8e2985095214a8363eef5ceda2e910ad5b0
Lorenzo Gentile <lorenzo.gentile@consensys.net>
2025-11-21 10:30:21 +0100

    update oob osaka shorthands

--- oob/osaka/shorthands.lisp
+++ oob/osaka/shorthands.lisp
@@ -11,15 +11,35 @@
                                                   IS_SSTORE
                                                   IS_DEPLOYMENT))
 
-(defun (flag-sum-prc-common)                (+    IS_ECRECOVER
+(defun (flag-sum-prc-common)                (+    (flag-sum-london-common-precompiles)
+                                                  (flag-sum-cancun-precompiles)
+                                                  (flag-sum-prague-precompiles)
+                                                  (flag-sum-osaka-precompiles)))
+
+(defun (flag-sum-london-common-precompiles) (+    IS_ECRECOVER
                                                   IS_SHA2
                                                   IS_RIPEMD
                                                   IS_IDENTITY
                                                   IS_ECADD
                                                   IS_ECMUL
-                                                  IS_ECPAIRING
-                                                  (flag-sum-prc-bls)
-                                                  (flag-sum-osaka-precompile)))
+                                                  IS_ECPAIRING))
+
+(defun (flag-sum-cancun-precompiles)     IS_POINT_EVALUATION)
+
+(defun (flag-sum-prague-precompiles)     (+       (flag-sum-prague-precompiles-fixed-size) 
+                                                  (flag-sum-prague-precompiles-variable-size)))
+
+(defun (flag-sum-prague-precompiles-fixed-size)     (+  IS_BLS_G1_ADD                                                             
+                                                        IS_BLS_G2_ADD
+                                                        IS_BLS_MAP_FP_TO_G1
+                                                        IS_BLS_MAP_FP2_TO_G2))
+
+(defun (flag-sum-prague-precompiles-variable-size)  (+  IS_BLS_G1_MSM
+                                                        IS_BLS_G2_ADD                                                          
+                                                        IS_BLS_MAP_FP_TO_G1
+                                                        IS_BLS_MAP_FP2_TO_G2))
+
+(defun (flag-sum-osaka-precompiles)      IS_P256_VERIFY)
 
 (defun (flag-sum-prc-blake)                 (+    IS_BLAKE2F_CDS
                                                   IS_BLAKE2F_PARAMS))
@@ -30,27 +50,12 @@
                                                   IS_MODEXP_PRICING
                                                   IS_MODEXP_EXTRACT))
 
-(defun (flag-sum-cancun-precompiles)     IS_POINT_EVALUATION)
-
-(defun (flag-sum-prague-precompiles)     (+       IS_BLS_G1_ADD
-                                                  IS_BLS_G1_MSM
-                                                  IS_BLS_G2_ADD
-                                                  IS_BLS_G2_MSM
-                                                  IS_BLS_PAIRING_CHECK
-                                                  IS_BLS_MAP_FP_TO_G1
-                                                  IS_BLS_MAP_FP2_TO_G2))
-
-(defun (flag-sum-prc-bls)                   (+    (flag-sum-cancun-precompiles)
-                                                  (flag-sum-prague-precompiles)))
-
-(defun (flag-sum-osaka-precompile)      IS_P256_VERIFY)
-
 (defun (flag-sum-prc)                       (+    (flag-sum-prc-common)
                                                   (flag-sum-prc-blake)
                                                   (flag-sum-prc-modexp)))
 
 (defun (flag-sum)                           (+    (flag-sum-inst)
-                                                  (flag-sum-prc)))
+                                                  (flag-sum-prc)))                                              
 
 (defun (wght-sum-inst)                      (+    (* OOB_INST_JUMP             IS_JUMP)
                                                   (* OOB_INST_JUMPI            IS_JUMPI)
