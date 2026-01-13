d24e1c010ba05b9af92d9b395967f2df1e56318b
Lorenzo Gentile <lorenzo.gentile@consensys.net>
2026-01-13 22:02:05 +0100

    added lookup into txndata

---
+++ rlpauth/lookups/rlpauth_into_txndata.lisp
@@ -0,0 +1,26 @@
+(defun (rlp-auth-into-txndata-activation-flag) rlpauth.macro * rlpauth.authority_ecrecover_success)
+
+(defclookup
+    (rlp-auth-into-txndata :unchecked)
+    ;; target columns
+    (
+        txndata.BLK_NUMBER
+        txndata.USER_TXN_NUMBER
+        txndata.USER
+        txndata.HUB
+        (prev txndata.rlp/TYPE_4)
+        (:: txndata.FROM_ADDR_HI txndata.FROM_ADDR_LO)
+        txndata.AUTHORITY_IS_SENDER_TOT ;; TODO
+    )
+    ;; source selector
+    (rlp-auth-into-txndata-activation-flag)
+    ;; source columns
+    (
+        rlpauth.blk_number
+        rlpauth.user_txn_number
+        1
+        1
+        1
+        rlpauth.txn_from_address
+        rlpauth.authority_is_sender_tot          
+    ))
